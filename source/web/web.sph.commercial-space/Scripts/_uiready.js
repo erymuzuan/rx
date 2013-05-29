74d3918667bb62c40999e759142f8cc9da391083
erymuzuan <erymuzuan@hotmail.com>
2013-05-29 17:58:26 +0800

    offer details view

---
+++ source/web/web.sph.commercial-space/Scripts/_uiready.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="bootstrap.js" />
+/// <reference path="google-maps-3-vs-1-0-vsdoc.js" />
+/// <reference path="bootstrap-datepicker.js" />
+/// <reference path="jquery.validate-vsdoc.js" />
+/// <reference path="jquery-1.9.1.intellisense.js" />
+/// <reference path="moment.js" />
+/// <reference path="underscore.js" />
+/// <reference path="modernizr-2.6.2.js" />
+
+
+var _uiready = function () {
+    var init = function (view) {
+        $(view).find('.datepicker').datepicker({ format: 'dd/mm/yyyy' });
+
+        if (!Modernizr.inputtypes.date) {
+            $(function () {
+                $('input[type="date"]')
+                    .css({ "min-width": "100px", "width": "200px" })
+                    .kendoDatePicker({
+                        format: "yyyy-MM-dd"
+                    });
+            });
+        }
+
+        $(view).find('.k-datepicker')
+            .css({ "min-width": "100px", "width": "200px" })
+            .kendoDatePicker({
+                format: "yyyy-MM-dd"
+            });
+        $(view).find('.k-datetimepicker')
+            .css({ "min-width": "100px", "width": "200px" })
+            .kendoDateTimePicker({
+                format: "yyyy-MM-dd HH:mm"
+            });
+
+        $(view).find('.k-timepicker')
+            .css({ "min-width": "100px", "width": "200px" })
+            .kendoTimePicker({});
+
+        $(view).find('.k-combobox')
+            .css({ "min-width": "300px!important", "width": "300px" })
+            .kendoComboBox();
+
+        $(view).find('.k-dropdown')
+            .css({ "min-width": "300px!important", "width": "300px" })
+            .kendoDropDownList();
+
+
+    };
+    return {
+        init: init
+    };
+
+}();
