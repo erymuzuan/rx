083fc62c355f724d332965a6447baed8cf1e6141
erymuzuan <erymuzuan@hotmail.com>
2013-06-04 11:34:40 +0800

    whoaaa

--- source/web/web.sph.commercial-space/App/services/Contract.js
+++ source/web/web.sph.commercial-space/App/services/Contract.js
@@ -2,7 +2,9 @@
 /// <reference path="../../Scripts/underscore.js" />
 
 bespoke.sphcommercialspace.domain.Offer.prototype.DepositPaid = function () {
-    return _(this.DepositPaymentCollection).reduce(function (memo, val) {
+    var sum =  _(this.DepositPaymentCollection()).reduce(function (memo, val) {
         return memo + val.Amount;
     }, 0);
+
+    return sum.toFixed(2);
 };
