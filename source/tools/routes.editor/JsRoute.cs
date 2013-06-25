6f20da8f0319ba9f88b0d694f1b60066d9d0c97e
erymuzuan <erymuzuan@hotmail.com>
2013-06-26 07:55:52 +0800

    filter and working on validations

--- source/tools/routes.editor/JsRoute.cs
+++ source/tools/routes.editor/JsRoute.cs
@@ -1,14 +1,57 @@
-﻿namespace routes.editor
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using routes.editor.Annotations;
+
+namespace routes.editor
 {
-    public class JsRoute
+    public class JsRoute : INotifyPropertyChanged, IDataErrorInfo
     {
-        public string role { set; get; }
-        public string url { set; get; }
-        public string moduleId { set; get; }
-        public string name { set; get; }
-        public bool visible { set; get; }
-        public string icon { set; get; }
-        public string caption { set; get; }
-        public JsRouteSetting settings { set; get; }
+
+        public string Role { get; set; }
+
+        public string Url { set; get; }
+        public string ModuleId { set; get; }
+        public string Name { set; get; }
+        public bool Visible { set; get; }
+        public string Icon { set; get; }
+        public string Caption { set; get; }
+        public JsRouteSetting Settings { set; get; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private readonly Dictionary<string, string> m_errors = new Dictionary<string, string>();
+        public void AddErrors(string property, string message)
+        {
+            if (!m_errors.ContainsKey(property))
+                m_errors.Add(property, message);
+            else
+                m_errors[property] = message;
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                return m_errors.ContainsKey(columnName) ? m_errors[columnName] : null;
+            }
+        }
+
+        public string Error
+        {
+            get { return ((m_errors.Count > 0) ? "Route object is in an invalid state" : string.Empty); }
+        }
+
+        public void RemoveErrors(string property)
+        {
+            if (m_errors.ContainsKey(property))
+                m_errors.Remove(property);
+        }
     }
 }
---
+++ source/web/web.sph.commercial-space/Models/JsRouteSetting.cs
@@ -0,0 +1,7 @@
+﻿namespace Bespoke.Sph.Commerspace.Web.Models
+{
+    public class JsRouteSetting
+    {
+        public string Caption { get; set; }
+    }
+}
