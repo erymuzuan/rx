4e7cacce0ec8fecf0008b357167938624f7e399a
Ruzzaima <ruzzaima@hotmail.com>
2013-07-10 17:41:27 +0800

    user editor

--- source/tools/permissions.editor/RoleModel.cs
+++ source/tools/permissions.editor/RoleModel.cs
@@ -2,5 +2,10 @@
 {
     public class RoleModel
     {
+        public string Group { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
     }
 }
--- source/web/web.sph.commercial-space/roles.config.js
+++ source/web/web.sph.commercial-space/roles.config.js
@@ -1,1 +1,23 @@
-﻿
+﻿[
+  {
+    "Group": "admin",
+    "Name": "Dashboard admin",
+    "Role": "admin_dashboard",
+    "Description": "buleh main dashboard",
+    "IsActive": true
+  },
+  {
+    "Group": "admin",
+    "Name": "Admin User",
+    "Role": "admin_user",
+    "Description": "boleh add user",
+    "IsActive": true
+  },
+  {
+    "Group": "petugas",
+    "Name": "Dashboard Petugas",
+    "Role": "maintenance_officer",
+    "Description": "Ditugaskan untuk penyenggaraa",
+    "IsActive": true
+  }
+]
