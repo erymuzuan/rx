b63bc962668c0f98781cc454b00505335dc5850a
erymuzuan <erymuzuan@hotmail.com>
2013-07-12 08:49:10 +0800

    custom fields

--- source/web/web.sph.commercial-space/Controllers/AppController.complaintlist.cs
+++ source/web/web.sph.commercial-space/Controllers/AppController.complaintlist.cs
@@ -1,15 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Bespoke.SphCommercialSpaces.Domain;
 
 namespace Bespoke.Sph.Commerspace.Web.Controllers
 {
     public partial class AppController
     {
+        [ActionName("ComplaintForm.html")]
+        public async Task<ActionResult> ComplaintFormHtml(int id)
+        {
+            var context = new SphDataContext();
+            var template = await context.LoadOneAsync<ComplaintTemplate>(t => t.ComplaintTemplateId == id);
+            return View("ComplaintFormHtml", template);
+        }
 
         public ActionResult ComplaintListHtml()
         {
             return View();
-        } 
-        
+        }
+
         public ActionResult ComplaintListJs()
         {
             return View();
