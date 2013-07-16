f0efcd62ee414eb282c2d6f2aa175e35105ea2dc
erymuzuan <erymuzuan@hotmail.com>
2013-07-16 12:18:03 +0800

    refactor search into seperate files

---
+++ source/web/web.sph.commercial-space/App/viewmodels/search.js
@@ -0,0 +1,60 @@
+﻿define(['durandal/system', 'durandal/plugins/router', 'services/logger', 'config','services/datacontext'],
+    function (system, router, logger, config, context) {
+
+        var isBusy = ko.observable(),
+
+            viewAttached = function (view) {
+                $(view).on('click', '#close-search-result', function (e) {
+                    e.preventDefault();
+                    shell.searchResults.removeAll();
+                });
+
+            },
+            search = function () {
+                var tcs = new $.Deferred();
+                var data = JSON.stringify({ text: shell.searchText() });
+                isBusy(true);
+
+                context.post(data, "/Search")
+                    .then(function (result) {
+                        isBusy(false);
+                        shell.searchResults(result);
+                        tcs.resolve(result);
+                    });
+                return tcs.promise();
+            }
+            ,            
+            navigateSearch = function(sr) {
+                console.log(sr);
+            };
+
+        var shell = {
+            activate: activate,
+            searchCommand: search,
+            searchResults: ko.observableArray(),
+            navigateSearch : navigateSearch, 
+            isBusy: isBusy,
+            searchText: ko.observable(),
+            viewAttached: viewAttached,
+            router: router
+        };
+
+        return shell;
+
+        //#region Internal Methods
+        function activate() {
+            return boot();
+        }
+
+
+        function boot() {
+            router.map(config.routes);
+            log('System Loaded!', null, true);
+            return router.activate(config.startModule);
+        }
+
+        function log(msg, data, showToast) {
+            logger.log(msg, data, system.getModuleId(shell), showToast);
+        }
+        //#endregion
+    });
--- source/web/web.sph.commercial-space/App/viewmodels/shell.js
+++ source/web/web.sph.commercial-space/App/viewmodels/shell.js
@@ -1,40 +1,16 @@
-﻿define(['durandal/system', 'durandal/plugins/router', 'services/logger', 'config','services/datacontext'],
-    function (system, router, logger, config, context) {
+﻿define(['durandal/system', 'durandal/plugins/router', 'services/logger', 'config'],
+    function (system, router, logger, config) {
 
-        var isBusy = ko.observable(),
-
-            viewAttached = function (view) {
+        var viewAttached = function (view) {
                 // NOTE: there's a bug someweher that makes bootstrap data-toggle didn't work
                 $(view).on('click', 'li.dropdown>a.dropdown-toggle', function () {
                     $(this).parent().toggleClass("open");
                 });
 
-            },
-            search = function () {
-                var tcs = new $.Deferred();
-                var data = JSON.stringify({ text: shell.searchText() });
-                isBusy(true);
-
-                context.post(data, "/Search")
-                    .then(function (result) {
-                        isBusy(false);
-                        shell.searchResults(result);
-                        tcs.resolve(result);
-                    });
-                return tcs.promise();
-            }
-            ,            
-            navigateSearch = function(sr) {
-                console.log(sr);
             };
 
         var shell = {
             activate: activate,
-            searchCommand: search,
-            searchResults: ko.observableArray(),
-            navigateSearch : navigateSearch, 
-            isBusy: isBusy,
-            searchText: ko.observable(),
             viewAttached: viewAttached,
             router: router
         };
