adfebd40d5a36c89732cab54ddf0f705c252bb19
erymuzuan <erymuzuan@hotmail.com>
2013-07-16 08:49:14 +0800

    messaging with xml and operation metadata

---
+++ source/subscribers/subscriber.lucene.index/BuildingIndexerSubscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bespoke.Sph.SubscribersInfrastructure;
+using Bespoke.SphCommercialSpaces.Domain;
+
+namespace Bespoke.Sph.Messaging
+{
+    public class LuceneIndexSubscriber : Subscriber<Building>
+    {
+        public override string QueueName
+        {
+            get { return "building_lucene"; }
+        }
+
+        public override string[] RoutingKeys
+        {
+            get { return new[] {"Building.*"}; }
+        }
+
+        protected async override Task ProcessMessage(string operation, Building item)
+        {
+            await Task.Delay(500);
+            Console.WriteLine("{0}:{1}", item.Name, operation);
+        }
+    }
+}
