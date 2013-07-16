d5cc96bd3507f691fe3200a50787b9df3ccaad15
erymuzuan <erymuzuan@hotmail.com>
2013-07-16 12:44:12 +0800

    indexe copied from CPD

--- source/subscribers/subscriber.lucene.index/BuildingIndexerSubscriber.cs
+++ source/subscribers/subscriber.lucene.index/BuildingIndexerSubscriber.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bespoke.Sph.SubscribersInfrastructure;
 using Bespoke.SphCommercialSpaces.Domain;
@@ -21,6 +22,33 @@ namespace Bespoke.Sph.Messaging
         {
             await Task.Delay(500);
             Console.WriteLine("{0}:{1}", item.Name, operation);
+            var metada = new BuildingMetadata
+                {
+                    Title = item.Name,
+                    Text = item.ToString(),
+                    Created = item.CreatedDate,
+                    OwnerCode = item.CreatedBy,
+                    Summary = item.ToString(),
+                    Code = item.LotNo,
+                    Type = typeof(Building).Name
+                };
+            var indexer = new LuceneIndexer(@"g:\temp\index");
+            indexer.AddDocuments(metada);
+
         }
+    }
+
+    class BuildingMetadata : ISearchable
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Summary { get; set; }
+        public string Text { get; set; }
+        public DateTime Created { get; set; }
+        public string Title { get; set; }
+        public Dictionary<string, object> CustomFields { get; set; }
+        public string Code { get; set; }
+        public string Status { get; set; }
+        public string OwnerCode { get; set; }
     }
 }
