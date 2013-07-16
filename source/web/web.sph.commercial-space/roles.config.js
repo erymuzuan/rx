b0ffe0bf63c4304f7fd6c587b7b88640ff07feae
Ruzzaima <ruzzaima@hotmail.com>
2013-07-16 18:02:47 +0800

    roles and routes setting in progress

--- source/web/web.sph.commercial-space/roles.config.js
+++ source/web/web.sph.commercial-space/roles.config.js
@@ -1,44 +1,86 @@
 [
-  {
-    "Group": "admin",
-    "Name": "Dashboard admin",
-    "Role": "admin_dashboard",
-    "Description": "Boleh pantau status permohonan",
-    "IsActive": true
-  },
-  {
-    "Group": "admin",
-    "Name": "Admin User",
-    "Role": "admin_user",
-    "Description": "boleh add user",
-    "IsActive": true
-  },
-  {
-    "Group": "petugas",
-    "Name": "Dashboard Petugas",
-    "Role": "maintenance_officer",
-    "Description": "Ditugaskan untuk penyenggaraa",
-    "IsActive": true
-  },
-  {
-    "Group": "tenant",
-    "Name": "Penyewa",
-    "Role": "can_view_tenant_details",
-    "Description": "penyewa",
-    "IsActive": true
-  },
-  {
-    "Group": "supervisor",
-    "Name": "Penyelia Senggara",
-    "Role": "can_inspect",
-    "Description": "Boleh buat pemeriksaan",
-    "IsActive": true
-  },
-  {
-    "Group": "admin",
-    "Name": "Penetapan ",
-    "Role": "can_edit_setting",
-    "Description": null,
-    "IsActive": true
-  }
+ {
+     "Group": "admin",
+     "Name": "Dashboard admin",
+     "Role": "admin_dashboard",
+     "Description": "Boleh pantau status permohonan",
+     "IsActive": true
+ },
+ {
+     "Group": "admin",
+     "Name": "Admin User",
+     "Role": "admin_user",
+     "Description": "boleh add user",
+     "IsActive": true
+ },
+ {
+     "Group": "petugas",
+     "Name": "Dashboard Petugas",
+     "Role": "maintenance_officer",
+     "Description": "Ditugaskan untuk penyenggaraa",
+     "IsActive": true
+ },
+ {
+     "Group": "tenant",
+     "Name": "Penyewa",
+     "Role": "can_view_tenant_details",
+     "Description": "penyewa",
+     "IsActive": true
+ },
+ {
+     "Group": "maintenance",
+     "Name": "Petugas Senggara",
+     "Role": "can_inspect",
+     "Description": "Ditugaskan untuk membat pemeriksaan terhadap aduan",
+     "IsActive": true
+ },
+ {
+     "Group": "admin",
+     "Name": "Penetapan ",
+     "Role": "can_edit_setting",
+     "Description": null,
+     "IsActive": true
+ },
+ {
+     "Group": "admin",
+     "Name": "Kerani Kontrak",
+     "Role": "can_view_contract_details",
+     "Description": "Boleh melihat dan mengemaskini kontrak",
+     "IsActive": true
+ },
+ {
+     "Group": "complaint",
+     "Name": "Admin Aduan",
+     "Role": "can_edit_complaint_template",
+     "Description": "Boleh menambah dan mengemaskini jenis-jenis aduan",
+     "IsActive": true
+ },
+ {
+     "Group": "complaint",
+     "Name": "PRO Aduan",
+     "Role": "can_assign_complaint",
+     "Description": "Mengagihkan aduan kepada unit yang berkenaan dan memantau status aduan",
+     "IsActive": true
+ },
+ {
+     "Group": "maintenance",
+     "Name": "PRO Senggara",
+     "Role": "can_assign_maintenance_officer",
+     "Description": "Memantau dan menugaskan petugas untuk memeriksa kes aduan",
+     "IsActive": true
+ },
+ {
+     "Group": "finance",
+     "Name": "Kerani Kewangan",
+     "Role": "can_edit_payment",
+     "Description": "Menyemak dan mengemaskini pembayaran sewa dan deposit oleh penyewa",
+     "IsActive": true
+ },
+ {
+     "Group": "contract",
+     "Name": "Admin Kontrak",
+     "Role": "can_edit_contract_template",
+     "Description": "Menambah dan mengemaskini jenis-jenis kontrak",
+     "IsActive": true
+ }
 ]
