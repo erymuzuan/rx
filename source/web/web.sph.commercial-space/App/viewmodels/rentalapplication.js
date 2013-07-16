111bf053abe744e011466762a918da5bb67847dc
Ruzzaima <ruzzaima@hotmail.com>
2013-07-16 12:47:57 +0800

    load state from setting on application form

--- source/web/web.sph.commercial-space/App/viewmodels/rentalapplication.js
+++ source/web/web.sph.commercial-space/App/viewmodels/rentalapplication.js
@@ -17,18 +17,22 @@ define(['services/datacontext', 'services/logger', 'durandal/plugins/router'], f
             
             id(routeData.id);
             var tcs = new $.Deferred();
-            context.loadOneAsync('CommercialSpace', 'CommercialSpaceId eq ' + id()).done(function(cs) {
+            var csTask = context.loadOneAsync('CommercialSpace', 'CommercialSpaceId eq ' + id());
+            var stateTask =  context.loadOneAsync("Setting", "Key eq 'State'");
+            $.when(csTask, stateTask).done(function (cs, s) {
+                var states = JSON.parse(ko.mapping.toJS(s.Value));
+                vm.stateOptions(states);
                 vm.commercialSpace(cs);
                 tcs.resolve(true);
             });
-            vm.rentalapplication.CommercialSpaceId(routeData.id);
+            vm.rentalapplication().CommercialSpaceId(routeData.id);
             var bank = {
                 Name: ko.observable(''),
                 Location: ko.observable(''),
                 AccountNo: ko.observable(''),
                 AccountType: ko.observable('')
             };
-            vm.rentalapplication.BankCollection.push(bank);
+            vm.rentalapplication().BankCollection.push(bank);
             return tcs.promise();
         },
         viewAttached = function() {
@@ -57,7 +61,7 @@ define(['services/datacontext', 'services/logger', 'durandal/plugins/router'], f
         
         saveApplication = function () {
             var tcs = new $.Deferred();
-            var data = ko.mapping.toJSON(vm.rentalapplication);
+            var data = ko.mapping.toJSON(vm.rentalapplication());
             isBusy(true);
             context.post(data, "/RentalApplication/Submit").done(function (e) {
                 logger.log("Data has been successfully saved ", e, "rentalapplication", true);
@@ -78,7 +82,7 @@ define(['services/datacontext', 'services/logger', 'durandal/plugins/router'], f
                 AccountNo: ko.observable(''),
                 AccountType: ko.observable('')
             };
-            vm.rentalapplication.BankCollection.push(bank);
+            vm.rentalapplication().BankCollection.push(bank);
         },
         addAttachment = function () {
             var guid = guidGenerator();
@@ -89,7 +93,7 @@ define(['services/datacontext', 'services/logger', 'durandal/plugins/router'], f
                 IsReceived: ko.observable(false),
                 StoreId: ko.observable(guid)
             };
-            vm.rentalapplication.AttachmentCollection.push(attachment);
+            vm.rentalapplication().AttachmentCollection.push(attachment);
         };
 
     var vm = {
@@ -97,40 +101,8 @@ define(['services/datacontext', 'services/logger', 'durandal/plugins/router'], f
         registrationNo: registrationNo,
         viewAttached: viewAttached,
         configureUpload: configureUpload,
-        rentalapplication: {
-            CommercialSpaceId: ko.observable(),
-            CompanyName: ko.observable(''),
-            CompanyRegistrationNo: ko.observable(''),
-            DateStart: ko.observable(),
-            DateEnd: ko.observable(),
-            Purpose: ko.observable(''),
-            CompanyType: ko.observable(''),
-            Experience: ko.observable(''),
-            IsRecordExist: ko.observable(false),
-            PreviousAddress: ko.observable(''),
-            IsCompany: ko.observable(false),
-            Type: ko.observable(''),
-            Address: {
-                Street: ko.observable(),
-                City: ko.observable(),
-                State: ko.observable(),
-                Postcode: ko.observable(),
-            },
-            BankCollection: ko.observableArray([]),
-            AttachmentCollection: ko.observableArray([]),
-            Contact: {
-                Name: ko.observable(''),
-                Title: ko.observable(''),
-                IcNo: ko.observable(''),
-                Role: ko.observable(''),
-                MobileNo: ko.observable(''),
-                OfficeNo: ko.observable(''),
-                Email: ko.observable('')
-            },
-            CurrentYearSales: ko.observable(),
-            LastYearSales: ko.observable(),
-            PreviousYearSales: ko.observable()
-        },
+        stateOptions: ko.observableArray(),
+        rentalapplication: ko.observable(new bespoke.sphcommercialspace.domain.RentalApplication()),
         commercialSpace : ko.observable (new bespoke.sphcommercialspace.domain.CommercialSpace()),
         saveCommand: saveApplication,
         addBankCommand: addBankCollection,
