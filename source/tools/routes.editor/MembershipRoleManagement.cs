40cc57ca2b4abcab3ff30dfa545f84945f190362
erymuzuan <erymuzuan@hotmail.com>
2013-07-19 07:12:01 +0800

    working on membership roles

---
+++ source/tools/routes.editor/MembershipRoleManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace routes.editor
+{
+    internal class MembershipRoleManagement
+    {
+        private readonly string m_config;
+
+        public MembershipRoleManagement(string config)
+        {
+            m_config = config;
+        }
+
+        public void UpdateRoles(IEnumerable<string> roles)
+        {
+
+        }
+    }
+}
