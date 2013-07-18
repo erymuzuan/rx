308dd125252d037e9fe89b0230e68fb16d5f9ed7
unknown <izzati@WSDELL01.bespoke.local>
2013-07-18 12:20:03 +0800

    Assignment Completed

--- source/web/web.sph.commercial-space/App/viewmodels/maintenance.assignment.js
+++ source/web/web.sph.commercial-space/App/viewmodels/maintenance.assignment.js
@@ -9,40 +9,51 @@
 /// <reference path="../../Scripts/bootstrap.js" />
 
 
-define(['services/datacontext'],
-    function (context) {
+define(['services/datacontext', 'durandal/plugins/router'],
+    function (context,router) {
 
         var isBusy = ko.observable(false),
             id = ko.observable(),
             activate = function (routeData) {
                 id(routeData.id);
-                var query = String.format("ComplaintId eq {0}", id());
+                var maintenanceQuery = String.format("MaintenanceId eq {0}", id());
                 var officerQuery = String.format("Designation eq 'Petugas'");
                 var tcs = new $.Deferred();
-                context.loadOneAsync("Complaint", query)
-                    .done(function(c) {
-                        vm.complaint(c);
-                        context.loadOneAsync("Tenant", "TenantId eq " + c.TenantId())
-                            .done(function(tenant) {
-                                vm.tenant(tenant);
-                                context.getTuplesAsync("UserProfile", officerQuery, 'Username', 'FullName')
-                                    .done(function(o) {
-                                        vm.officerOptions(o);
-                                    });
 
-                                tcs.resolve(true);
-                            });
-                    });
+                var maintenanceTask = context.loadOneAsync("Maintenance", maintenanceQuery);
+                var officerTask = context.getTuplesAsync("UserProfile", officerQuery, 'Username', 'FullName');
+
+                $.when(maintenanceTask, officerTask).then(function (m, o) {
+                    isBusy(false);
+                    vm.maintenance(m);
+                    vm.officerOptions(o);
+                    tcs.resolve(true);
+                });
 
                 return tcs.promise();
             },
-            saveAssignment = function(){};
+
+            saveAssignment = function () {
+                var tcs = new $.Deferred();
+                var data = ko.toJSON({ officer: vm.maintenance().Officer, id: id() });
+                isBusy(true);
+
+                context.post(data, "/Maintenance/Assign")
+                    .then(function (result) {
+                        isBusy(false);
+                        tcs.resolve(result);
+                        $('#success-panel').modal({})
+                         .on('hidden', function () {
+                             var url = '/#/maintenance.list';
+                             router.navigateTo(url);
+                         });
+                    });
+                return tcs.promise();
+            };
 
         var vm = {
             isBusy: isBusy,
             activate: activate,
-            complaint: ko.observable(new bespoke.sphcommercialspace.domain.Complaint()),
-            tenant: ko.observable(new bespoke.sphcommercialspace.domain.Tenant()),
             maintenance: ko.observable(new bespoke.sphcommercialspace.domain.Maintenance()),
             officerOptions: ko.observableArray(),
             saveAssignmentCommand: saveAssignment
