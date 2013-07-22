da7ac3120ec0863c17ccbba3204c1ab83505b0b5
erymuzuan <erymuzuan@hotmail.com>
2013-07-23 05:54:59 +0800

    seperate text fixture for each type of field

---
+++ source/unit.test/domain.test/FunctionFieldTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Bespoke.SphCommercialSpaces.Domain;
+using NUnit.Framework;
+
+namespace domain.test
+{
+    [TestFixture]
+    class FunctionFieldTest
+    {
+
+        [Test]
+        public void FuctionDateTimeValue()
+        {
+            var building = new FuctionField { Script = "DateTime.Today" };
+
+            var result = building.GetValue(new Building());
+            Assert.AreEqual(DateTime.Today, result);
+        }
+
+        [Test]
+        public void DocumentFieldEqFuction()
+        {
+            var building = new Building { BuildingId = 500 };
+            var rule = new Rule
+            {
+                Left = new DocumentField { Path = "//bs:Building/@CreatedDate", Type = typeof(DateTime) },
+                Operator = Operator.Equal,
+                Right = new FuctionField { Script = "DateTime.Today" }
+            };
+
+            var result = rule.Execute(building);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ source/unit.test/domain.test/SerializationTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Bespoke.SphCommercialSpaces.Domain;
+using NUnit.Framework;
+
+namespace domain.test
+{
+    [TestFixture]
+    class SerializationTest
+    {
+        [Test]
+        public void DocumentFieldToXml()
+        {
+            var df = new DocumentField{ Path = "//bs:Building/@BuildingId", Type = typeof(int), NamespacePrefix = "bs"};
+            var xml = XmlSerializerService.ToUtf8EncodedXmlString(df);
+            Console.WriteLine(xml);
+            var ff = XmlSerializerService.DeserializeFromXml<DocumentField>(xml);
+            Assert.IsNotNull(ff);
+            Assert.IsInstanceOf<DocumentField>(ff);
+
+            var df2 = (DocumentField)ff;
+            Assert.IsTrue(typeof(int) == df2.Type);
+
+        }
+         
+    }
+
+}
