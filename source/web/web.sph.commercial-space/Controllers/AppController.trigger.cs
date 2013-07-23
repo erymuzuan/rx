6afb82cca04651b6c45bb4ce050d7c72afe0f4c7
erymuzuan <erymuzuan@hotmail.com>
2013-07-23 13:45:20 +0800

    getting the treeviews

---
+++ source/web/web.sph.commercial-space/Controllers/AppController.trigger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Bespoke.Sph.Commerspace.Web.Controllers
+{
+    public partial class AppController
+    {
+
+        public ActionResult TriggerPathPickerHtml(string id)
+        {
+            var type = Type.GetType(string.Format("Bespoke.SphCommercialSpaces.Domain.{0}, domain.commercialspace, Version=1.0.2.1006, Culture=neutral", id));
+            var vm = new TriggerPathPickerHtmlViewModel
+                {
+                    Type = type,
+                    Path = ""
+                };
+            return View("TriggerPathPickerHtml", vm);
+        }
+
+        public ActionResult TriggerPathPickerJs()
+        {
+            return View();
+        }
+
+    }
+
+    public class TriggerPathPickerHtmlViewModel
+    {
+        public Type Type { get; set; }
+        public string Path { get; set; }
+    }
+}
