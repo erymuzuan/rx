aefdd5d8c2afa2292a34a267e473814ccd9de7f3
erymuzuan <erymuzuan@hotmail.com>
2013-07-23 08:35:48 +0800

    using HostObject to share data

--- source/dependencies/roslyn.scriptengine/RoslynScriptEngine.cs
+++ source/dependencies/roslyn.scriptengine/RoslynScriptEngine.cs
@@ -1,37 +1,30 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 using Bespoke.SphCommercialSpaces.Domain;
 using Roslyn.Compilers;
-using Roslyn.Compilers.CSharp;
 using Roslyn.Scripting.CSharp;
 
 namespace roslyn.scriptengine
 {
+    public class HostObject
+    {
+        public Entity Item { get; set; }
+    }
     public class RoslynScriptEngine : IScriptEngine
     {
-        private ScriptEngine m_scriptEngine;
-
-        public RoslynScriptEngine()
-        {
-            m_scriptEngine = new ScriptEngine();
-            var domain = typeof(Entity).Assembly.FullName;
-            Console.WriteLine(domain);
-            m_scriptEngine.AddReference(MetadataReference.CreateAssemblyReference("System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"));
-        }
 
         public object Evaluate(string script, Entity item)
         {
-            m_scriptEngine = new ScriptEngine();
-            var session = m_scriptEngine.CreateSession();
+            var host = new HostObject
+            {
+                Item = item
+            };
+            var scriptEngine = new ScriptEngine();
+            var session = scriptEngine.CreateSession(host);
             session.AddReference("System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
             session.AddReference("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-
-            var temp = Path.GetTempFileName() + ".xml";
-            using (var stream = new FileStream(temp, FileMode.Create))
-            {
-                XmlSerializerService.ToXmlStream(stream, item);
-            }
+            session.Execute("#r \".\\domain.commercialspace.dll\"");
+            session.Execute("#r \".\\roslyn.scriptengine.dll\"");
 
             var block = script;
             if (!block.EndsWith(";"))
@@ -40,21 +33,17 @@ namespace roslyn.scriptengine
                                      "using Bespoke.SphCommercialSpaces.Domain;" +
                                      "" +
                                      "public object Evaluate()" +
-                                     "{{" +
-                                            "var xml = System.IO.File.ReadAllText(@\"{1}\");\r\n" +
-                                            "var item = XmlSerializerService.DeserializeFromXml<{2}>(xml); \r\n" +
+                                     "{{\r\n" +
+                                        "var item = Item as {1};\r\n"+
 
-                                            "{0}\r\n" +
-                                     "}}", block, temp, item.GetType().Name);
-            session.Execute("#r \".\\domain.commercialspace.dll\"");
+                                        "{0}\r\n" +
+                                     "}}", block, item.GetType().Name);
             session.Execute(code);
 
 
             var result = session.Execute("Evaluate();");
             Console.WriteLine("result :" + result);
 
-            if (File.Exists(temp))
-                File.Delete(temp);
 
             return result;
 
