20cb6b4a1a8a7fef5beea7078667e6fddd6e0499
erymuzuan <erymuzuan@hotmail.com>
2013-08-15 07:38:14 +0800

    toolbar only visible when there's one specified

--- StartWeb.bat
+++ StartWeb.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\IIS Express\iisexpress.exe" /config:%USERPROFILE%\Documents\IISExpress\config\applicationhost.config /site:web.sph.commercial-space
+"C:\Program Files (x86)\IIS Express\iisexpress.exe" /config:%USERPROFILE%\Documents\IISExpress\config\applicationhost.config /site:web.sph.commercial-space /trace:error
