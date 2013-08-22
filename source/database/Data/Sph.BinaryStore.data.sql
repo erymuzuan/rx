08f9ceee4f8f0de96bbcc85efb5491e9a60d674e
Ruzzaima <ruzzaima@hotmail.com>
2013-08-22 15:55:56 +0800

    seed data for templates

---
+++ source/database/Data/Sph.BinaryStore.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [Sph].[BinaryStore] ON
+INSERT INTO [Sph].[BinaryStore] ([BinaryStoreId], [StoreId], [Extension], [FileName], [Tag]) VALUES (46, N'd7090222-2005-4940-9c7c-fd9c40f731bb', N'.docx', N'CommercialSpace.SuratTawaranBaru.docx', NULL)
+INSERT INTO [Sph].[BinaryStore] ([BinaryStoreId], [StoreId], [Extension], [FileName], [Tag]) VALUES (47, N'0bb0b116-cded-4c95-99ac-b2690ce684cd', N'.xlsx', N'ruang.komersil.utility.ledger.xlsx', NULL)
+INSERT INTO [Sph].[BinaryStore] ([BinaryStoreId], [StoreId], [Extension], [FileName], [Tag]) VALUES (52, N'f5860f79-ed22-4a22-9d16-02908fcc103c', N'.doc', N'SURAT TAWARAN KONTRAK BARU.doc', NULL)
+INSERT INTO [Sph].[BinaryStore] ([BinaryStoreId], [StoreId], [Extension], [FileName], [Tag]) VALUES (53, N'f19d2229-282d-4344-882a-8dae0675b7de', N'.docx', N'BSPB/2013/2013000036-20130822.Surat Tawaran.docx', NULL)
+SET IDENTITY_INSERT [Sph].[BinaryStore] OFF
