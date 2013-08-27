f342df491093a8763f9004567a6c715d3a48bc9e
erymuzuan <erymuzuan@hotmail.com>
2013-08-27 08:59:00 +0800

    report execution and parameters

---
+++ source/domain/domain.commercialspace/Reports/ReportColumn.cs
@@ -0,0 +1,10 @@
+﻿namespace Bespoke.SphCommercialSpaces.Domain
+{
+    public partial class ReportColumn : DomainObject
+    {
+        public override string ToString()
+        {
+            return string.Format("{0}", this.Value);
+        }
+    }
+}
