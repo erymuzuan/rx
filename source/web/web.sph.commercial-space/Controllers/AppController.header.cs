310028a06d624759dd4961ae2b7ac30908a49777
Ruzzaima <ruzzaima@hotmail.com>
2013-09-02 10:54:45 +0800

    add dynamic startmodule to header

--- source/web/web.sph.commercial-space/Controllers/AppController.header.cs
+++ source/web/web.sph.commercial-space/Controllers/AppController.header.cs
@@ -1,13 +1,21 @@
 
+﻿using System.Threading.Tasks;
 ﻿using System.Web.Mvc;
+﻿using Bespoke.Sph.Commerspace.Web.ViewModels;
+﻿using Bespoke.SphCommercialSpaces.Domain;
 
 namespace Bespoke.Sph.Commerspace.Web.Controllers
 {
     public partial class AppController
     {
-        public ActionResult HeaderHtml()
+        public async Task<ActionResult>  HeaderHtml()
         {
-            return View();
+            var vm = new HeaderViewModel();
+            var context = new SphDataContext();
+            var user = User.Identity.Name;
+            var profile = await context.LoadOneAsync<UserProfile>(u => u.Username == user);
+            vm.StartModule = profile.StartModule;
+            return View(vm);
         }
     }
 }
