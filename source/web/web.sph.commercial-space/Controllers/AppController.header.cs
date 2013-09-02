cb653fc71dd86a68f158adf805a79be96e1d55d4
Ruzzaima <ruzzaima@hotmail.com>
2013-09-02 11:30:50 +0800

    fix bug on tenant portal

--- source/web/web.sph.commercial-space/Controllers/AppController.header.cs
+++ source/web/web.sph.commercial-space/Controllers/AppController.header.cs
@@ -1,21 +1,12 @@
-
-﻿using System.Threading.Tasks;
-﻿using System.Web.Mvc;
-﻿using Bespoke.Sph.Commerspace.Web.ViewModels;
-﻿using Bespoke.SphCommercialSpaces.Domain;
+using System.Web.Mvc;
 
 namespace Bespoke.Sph.Commerspace.Web.Controllers
 {
     public partial class AppController
     {
-        public async Task<ActionResult>  HeaderHtml()
+        public  ActionResult HeaderHtml()
         {
-            var vm = new HeaderViewModel();
-            var context = new SphDataContext();
-            var user = User.Identity.Name;
-            var profile = await context.LoadOneAsync<UserProfile>(u => u.Username == user);
-            vm.StartModule = profile.StartModule;
-            return View(vm);
+           return View();
         }
     }
 }
