0979c76f9fcac52a962541883ebfe74e829e3c1c
erymuzuan <erymuzuan@hotmail.com>
2013-09-10 14:49:22 +0800

    Custom list collection for building

--- source/web/web.sph.commercial-space/App/partial/Building.js
+++ source/web/web.sph.commercial-space/App/partial/Building.js
@@ -1,4 +1,6 @@
 ﻿/// <reference path="../schemas/sph.domain.g.js" />
+/// <reference path="../durandal/system.js" />
+/// <reference path="../durandal/amd/require.js" />
 /// <reference path="../../Scripts/jquery-2.0.3.intellisense.js" />
 /// <reference path="../../Scripts/underscore.js" />
 
@@ -7,8 +9,8 @@ bespoke.sphcommercialspace = bespoke.sphcommercialspace || {};
 bespoke.sphcommercialspace.domain = bespoke.sphcommercialspace.domain || {};
 
 
-bespoke.sphcommercialspace.domain.BuildingPartial = function (model) {
-    var getCustomField = function(name) {
+bespoke.sphcommercialspace.domain.BuildingPartial = function () {
+    var getCustomField = function (name) {
         var cs = _(this.CustomFieldValueCollection()).find(function (v) {
             return v.Name() === name;
         });
@@ -16,8 +18,40 @@ bespoke.sphcommercialspace.domain.BuildingPartial = function (model) {
             throw "Cannot find custom field for " + name + " in Building";
         }
         return cs.Value;
-    };
+    },
+        getCustomList = function (name) {
+            var cs = _(this.CustomListValueCollection()).find(function (v) {
+                return v.Name() === name;
+            });
+            if (!cs) {
+                throw "Cannot find custom list named " + name + " in Building";
+            }
+            return cs;
+        },
+        addCustomListItem = function (name) {
+
+            return function () {
+                var list = _(this.CustomListValueCollection()).find(function (v) {
+                    return v.Name() === name;
+                });
+                var system = require('durandal/system');
+                var row = new bespoke.sphcommercialspace.domain.CustomListRow(system.guid());
+                var columns = _(list.CustomFieldCollection()).map(function (f) {
+                    var webid = system.guid();
+                    var v = new bespoke.sphcommercialspace.domain.CustomFieldValue(webid);
+                    v.Name(f.Name());
+                    v.Type(f.Type());
+                    return v;
+                });
+
+                row.CustomFieldValueCollection(columns);
+                list.CustomListRowCollection.push(row);
+
+            };
+        };
     return {
-        CustomField : getCustomField
+        CustomField: getCustomField,
+        CustomList: getCustomList,
+        addCustomListItem: addCustomListItem
     };
 };
