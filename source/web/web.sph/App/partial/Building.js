dbca9fde32bf55e96c231e4f58b161a0b83313a8
erymuzuan <erymuzuan@hotmail.com>
2013-09-17 12:07:27 +0800

    block edit details

--- source/web/web.sph/App/partial/Building.js
+++ source/web/web.sph/App/partial/Building.js
@@ -3,6 +3,8 @@
 /// <reference path="../durandal/amd/require.js" />
 /// <reference path="../../Scripts/jquery-2.0.3.intellisense.js" />
 /// <reference path="../../Scripts/underscore.js" />
+/// <reference path="../../Scripts/knockout-2.3.0.debug.js" />
+/// <reference path="../../Scripts/knockout.mapping-latest.debug.js" />
 
 var bespoke = bespoke || {};
 bespoke.sph = bespoke.sph || {};
@@ -12,14 +14,14 @@ bespoke.sph.domain = bespoke.sph.domain || {};
 bespoke.sph.domain.BuildingPartial = function () {
     var system = require('durandal/system'),
         getCustomField = function (name) {
-        var cs = _(this.CustomFieldValueCollection()).find(function (v) {
-            return v.Name() === name;
-        });
-        if (!cs) {
-            throw "Cannot find custom field for " + name + " in Building";
-        }
-        return cs.Value;
-    },
+            var cs = _(this.CustomFieldValueCollection()).find(function (v) {
+                return v.Name() === name;
+            });
+            if (!cs) {
+                throw "Cannot find custom field for " + name + " in Building";
+            }
+            return cs.Value;
+        },
         getCustomList = function (name) {
             var cs = _(this.CustomListValueCollection()).find(function (v) {
                 return v.Name() === name;
@@ -52,19 +54,59 @@ bespoke.sph.domain.BuildingPartial = function () {
         removeCustomListItem = function (name, row) {
             var self = this,
                 list = _(self.CustomListValueCollection()).find(function (v) {
-                return v.Name() === name;
-            });
-            return function() {
+                    return v.Name() === name;
+                });
+            return function () {
                 list.CustomListRowCollection.remove(row);
             };
         },
-        addBlock = function() {
+        addBlock = function () {
             this.BlockCollection.push(new bespoke.sph.domain.Block(system.guid()));
         },
         removeBlock = function (block) {
             var self = this;
-            return function() {
+            return function () {
                 self.BlockCollection.remove(block);
+            };
+        },
+        editBlockMap = function (block) {
+            var building = this;
+            return function () {
+                console.log("show map ", building);
+                console.log(" on block ", block);
+
+                require(['viewmodels/block.map', 'durandal/app'], function (dialog, app) {
+                    dialog.spatialStoreId(block.FloorPlanStoreId());
+                    app.showModal(dialog)
+                        .done(function (result) {
+                            if (result == "OK") {
+                                block.FloorPlanStoreId(dialog.spatialStoreId());
+                            }
+                        });
+
+                });
+
+            };
+        },
+        editBlockFloor = function (block) {
+            var building = this;
+            return function () {
+                console.log("edit floors ", building);
+                console.log(" on block ", block);
+                if (block.FloorCollection().length === 0) {
+                    block.FloorCollection.push(new bespoke.sph.domain.Floor(system.guid()));
+                }
+                
+                require(['viewmodels/block.floors', 'durandal/app'], function (dialog, app) {
+                    dialog.block(block);
+                    app.showModal(dialog)
+                        .done(function (result) {
+                            if (result == "OK") {
+                                
+                            }
+                        });
+
+                });
             };
         };
     return {
@@ -73,6 +115,8 @@ bespoke.sph.domain.BuildingPartial = function () {
         addCustomListItem: addCustomListItem,
         removeCustomListItem: removeCustomListItem,
         addBlock: addBlock,
-        removeBlock : removeBlock
+        editBlockMap: editBlockMap,
+        editBlockFloor: editBlockFloor,
+        removeBlock: removeBlock
     };
 };
---
+++ source/web/web.sph/App/viewmodels/block.map.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="../../Scripts/jquery-2.0.3.intellisense.js" />
+/// <reference path="../../Scripts/knockout-2.3.0.debug.js" />
+/// <reference path="../../Scripts/knockout-2.3.0.debug.js" />
+/// <reference path="../../Scripts/require.js" />
+/// <reference path="../../Scripts/underscore.js" />
+/// <reference path="../../Scripts/moment.js" />
+/// <reference path="../services/datacontext.js" />
+/// <reference path="../schemas/sph.domain.g.js" />
+
+
+define(['services/datacontext', 'services/logger', 'durandal/plugins/router','viewmodels/map'],
+    function(context, logger, router, map) {
+
+        var isBusy = ko.observable(false),
+            activate = function(storeId) {
+                vm.spatialStoreId(storeId);
+                
+            },
+            viewAttached = function(view) {
+                map.init({
+                    panel: 'block-map-panel',
+                    draw: true,
+                    zoom: 18
+                });
+            },
+            okClick = function() {
+                this.modal.close("OK");
+            },
+            cancelClick = function() {
+                this.modal.close("Cancel");
+            };
+
+        var vm = {
+            isBusy: isBusy,
+            activate: activate,
+            viewAttached: viewAttached,
+            spatialStoreId: ko.observable(),
+            okClick: okClick,
+            cancelClick: cancelClick
+        };
+
+        return vm;
+
+    });
