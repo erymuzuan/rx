493271bf50e5cd42a673a539f6ffd16e6863dd3b
erymuzuan <erymuzuan@hotmail.com>
2013-09-24 11:26:21 +0800

    adding map

--- source/web/web.sph/App/viewmodels/space.detailpublic.js
+++ source/web/web.sph/App/viewmodels/space.detailpublic.js
@@ -6,36 +6,75 @@
 /// <reference path="../../Scripts/require.js" />
 /// <reference path="../../Scripts/underscore.js" />
 /// <reference path="../../Scripts/moment.js" />
+/// <reference path="../../Scripts/google-maps-3-vs-1-0-vsdoc.js" />
 /// <reference path="../services/datacontext.js" />
 /// <reference path="../services/cultures.my.js" />
 /// <reference path="../schemas/sph.domain.g.js" />
 /// <reference path="../objectbuilders.js" />
 
 define(['services/datacontext', 'services/logger', './_space.contract', 'durandal/system', 'config', objectbuilders.cultures, objectbuilders.map],
-    function (context, logger, contractlistvm, system, config, cultures,map) {
+    function (context, logger, contractlistvm, system, config, cultures, map) {
 
         var title = ko.observable(),
             activate = function (routeData) {
                 var id = parseInt(routeData.id),
                     tcs = new $.Deferred();
                 context.loadOneAsync("Space", "SpaceId eq " + id).done(
-                    function(space) {
+                    function (space) {
                         vm.space(space);
-                        tcs.resolve(true);
-                        
-                        // load map
-                        map.init({
-                            panel: 'map-space'
-                            
-                        });
+                        // get the building
+                        var buildingId = space.BuildingId();
+                        if (buildingId) {
+                            context.loadOneAsync("Building", "BuildingId eq " + buildingId)
+                                .done(function (b) {
+                                    vm.building(b);
+                                    tcs.resolve(true);
+                                });
+                        } else {
+
+                            tcs.resolve(true);
+                        }
+
+
                     });
 
-              
+
 
                 return tcs.promise();
             },
             viewAttached = function (view) {
                 $(view).tooltip({ 'placement': 'right' });
+                // load map
+
+                var buildingId = vm.building().BuildingId(),
+                    point = new google.maps.LatLng(3.1282, 101.6441),
+                    pathTask = $.get("/Building/GetEncodedPath/" + buildingId),
+                    centerTask = $.get("/Building/GetCenter/" + buildingId);
+                
+                $.when(pathTask, centerTask)
+                .then(function (path, center) {
+                    if (center[0]) {
+                        point = new google.maps.LatLng(center[0].Lat, center[0].Lng);
+                    } else {
+                        $('#map-space').hide();
+                        return;
+                    }
+                    map.init({
+                        panel: 'map-space',
+                        zoom: center[0] ? 18 : 12,
+                        center: point
+                    });
+                    if (path[0]) {
+                        var shape = map.add({
+                            encoded: path[0],
+                            draggable: true,
+                            editable: true,
+                            zoom: 18,
+                            icon : '/images/maps/office-building.png'
+                        });
+                    }
+
+                });
             };
 
         var vm = {
@@ -43,10 +82,10 @@ define(['services/datacontext', 'services/logger', './_space.contract', 'duranda
             title: title,
             viewAttached: viewAttached,
             space: ko.observable(new bespoke.sph.domain.Space()),
-            building : ko.observable(new bespoke.sph.domain.Building())
+            building: ko.observable(new bespoke.sph.domain.Building())
         };
 
-    
+
 
         return vm;
     });
