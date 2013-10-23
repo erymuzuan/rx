afb179b318904b8fe984a790bc8931b1671e60e9
erymuzuan <erymuzuan@hotmail.com>
2013-10-23 10:11:50 +0800

    offers and application

--- source/tools/permissions.editor/Properties/Resources.Designer.cs
+++ source/tools/permissions.editor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace routes.editor.Properties {
+namespace permissions.editor.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace routes.editor.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("routes.editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("permissions.editor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- source/web/web.sph/App/objectbuilders.js
+++ source/web/web.sph/App/objectbuilders.js
@@ -6,5 +6,6 @@
     router: 'durandal/plugins/router',
     cultures: 'services/cultures.my',
     validation: 'services/validation',
-    map: 'viewmodels/map'
+    map: 'viewmodels/map',
+    eximp: 'services/jsonimportexport'
 };
