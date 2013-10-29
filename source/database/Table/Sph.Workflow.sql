226bbae590de8daaacf2bb619e21d52bf85d9a85
ruzzaima <ruzzaima@hotmail.com>
2013-10-29 14:41:26 +0800

    workflow sql file

---
+++ source/database/Table/Sph.Workflow.sql
@@ -0,0 +1,19 @@
+USE [Sph]
+IF OBJECT_ID('Sph.Workflow', 'U') IS NOT NULL
+  DROP TABLE Sph.Workflow
+GO
+
+CREATE TABLE Sph.Workflow
+(
+	 [WorkflowId] INT PRIMARY KEY IDENTITY(1,1)	
+	,[WorkflowDefinitionId] VARCHAR(255) NULL
+	,[Name] VARCHAR(255) NULL
+	,[State] VARCHAR(255) NULL
+	,[IsActive] BIT NOT NULL
+	,[Data] XML NOT NULL
+	,[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[CreatedBy] VARCHAR(255) NULL
+	,[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[ChangedBy] VARCHAR(255) NULL
+)
+GO 
---
+++ source/database/Table/Sph.WorkflowDefinition.sql
@@ -0,0 +1,18 @@
+USE [Sph]
+IF OBJECT_ID('Sph.WorkflowDefinition', 'U') IS NOT NULL
+  DROP TABLE Sph.WorkflowDefinition
+GO
+
+CREATE TABLE Sph.WorkflowDefinition
+(
+	 [WorkflowDefinitionId] INT PRIMARY KEY IDENTITY(1,1)	
+	,[Name] VARCHAR(255) NULL
+	,[Note] VARCHAR(255) NULL
+	,[Data] XML NOT NULL
+	,[IsActive] BIT NOT NULL
+	,[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[CreatedBy] VARCHAR(255) NULL
+	,[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[ChangedBy] VARCHAR(255) NULL
+)
+GO 
