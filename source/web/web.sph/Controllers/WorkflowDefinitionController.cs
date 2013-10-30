9c7e8c4d2450c733821a8a68444d73e482df1a95
ruzzaima <ruzzaima@hotmail.com>
2013-10-30 14:06:41 +0800

    workflow definition controller

---
+++ source/web/web.sph/Controllers/WorkflowDefinitionController.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.Web.Helpers;
+using Bespoke.Sph.Web.ViewModels;
+
+namespace Bespoke.Sph.Web.Controllers
+{
+    public class WorkflowDefinitionController : Controller
+    {
+        public const string APPLICATION_JAVASCRIPT = "application/javascript";
+        public const string TEXT_HTML = "text/html";
+
+        public async Task<ActionResult> Save()
+        {
+              var wd = this.GetRequestJson<WorkflowDefinition>();
+            var context = new SphDataContext();
+            using (var session = context.OpenSession())
+            {
+                session.Attach(wd);
+                await session.SubmitChanges();
+            }
+            return Json(wd.WorkflowDefinitionId);
+        }
+    
+    }
+}
