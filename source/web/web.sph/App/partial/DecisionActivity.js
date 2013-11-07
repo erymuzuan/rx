ac61be9980cc4cc56f92a4c5d0a95d1e5d55af74
erymuzuan <erymuzuan@hotmail.com>
2013-11-07 11:27:16 +0800

    #1034 screen for Decision activity

---
+++ source/web/web.sph/App/partial/DecisionActivity.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="../schemas/sph.domain.g.js" />
+/// <reference path="../durandal/system.js" />
+/// <reference path="../durandal/amd/require.js" />
+/// <reference path="/Scripts/jquery-2.0.3.intellisense.js" />
+/// <reference path="/Scripts/knockout-3.0.0.debug.js" />
+/// <reference path="/Scripts/knockout.mapping-latest.debug.js" />
+
+var bespoke = bespoke || {};
+ 
+bespoke.sph = bespoke.sph || {};
+bespoke.sph.domain = bespoke.sph.domain || {};
+
+
+bespoke.sph.domain.DecisionActivityPartial = function () {
+    var system = require('durandal/system'),
+        addBranch = function() {
+            this.DecisionBranchCollection.push(new bespoke.sph.domain.DecisionBranch(system.guid()));
+        },
+        removeBranch = function(branch) {
+            var self = this;
+            return function() {
+                self.DecisionBranchCollection.remove(branch);
+            };
+        };
+    return {
+        addBranch: addBranch,
+        removeBranch: removeBranch
+    };
+};
