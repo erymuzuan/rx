058f59c9b72b631f2f61931804141e633614fc8d
izzati <izzati@bespoke.com.my>
2013-11-11 14:42:34 +0800

    Add my own theme..

---
+++ source/web/web.sph/Content/theme.zaza/theme.js
@@ -0,0 +1,21 @@
+﻿bespoke.sph.Theme = function() {
+    return {
+        "rentalapplication.selectspace-spaceitem": function (element, space) {
+            $(element).find("div.space-nav a").addClass("btn");
+            if (space.Category() === "Kedai Runcit") {
+                
+            }
+        },
+        "public-index":function(element) {
+            $(element).find("div.col-lg-6>div").first().after('<div><div><a class="box-label" href="/workflow_1_21/ApplyScreen">Permohonan Wakaf Baru</a></div></div>');
+        }
+        //,
+        //"admindashboard": function (element) {
+        //    var div = $('<div class="row"></div>'),
+        //        startWakaf = $('<a class="btn btn-warning" href="/workflow_1_21/ApplyScreen">Permohonan Wakaf Baru</a>');
+
+        //    startWakaf.appendTo(div);
+        //    div.appendTo($(element));
+        //}
+    };
+};
