4133235d884a31663dcfdfd971fb24b660c40df1
erymuzuan <erymuzuan@hotmail.com>
2013-11-13 16:05:31 +0800

    File upload element

---
+++ source/domain/domain.sph/FormTemplates/FileUploadElement.cs
@@ -0,0 +1,10 @@
+﻿namespace Bespoke.Sph.Domain
+{
+    public partial class FileUploadElement : FormElement
+    {
+        public override CustomField GenerateCustomField()
+        {
+            return null;
+        }
+    }
+}
