091f1a0c9ee14e35161190a439eae72d4a2bdf5d
erymuzuan <erymuzuan@hotmail.com>
2013-11-15 12:11:54 +0800

    pop over and complex type elements #1036

--- source/web/web.sph/Scripts/_ko.workflow.js
+++ source/web/web.sph/Scripts/_ko.workflow.js
@@ -1,5 +1,5 @@
 ﻿ko.bindingHandlers.activityClass = {
-    init: function (element,valueAccessor) {
+    init: function (element, valueAccessor) {
 
         var div = $(element),
             act = ko.unwrap(valueAccessor()),
@@ -11,27 +11,45 @@
 };
 
 ko.bindingHandlers.activityPopover = {
-    init: function (element,valueAccessor) {
+    init: function (element, valueAccessor) {
 
         var div = $(element),
             act = ko.unwrap(valueAccessor()),
             fullName = typeof act.$type === "function" ? act.$type() : act.$type,
             name = /Bespoke\.Sph\.Domain\.(.*?),/.exec(fullName)[1],
-            wd = ko.dataFor(div.parent()[0]).wd();
+            wd = ko.dataFor(div.parent()[0]).wd(),
+            pop = div.popover({
+                title: name + ' : ' + act.Name(),
+                html: true,
+                content: function () {
+                    $('a.edit-activity').popover('hide');
+                    div.find("a.edit-activity").addClass(act.WebId());
+                    div.find("a.delete-activity").addClass(act.WebId());
+                    return div.find("div.context-menu").html();
+                }
+            });
 
-
-        div.popover({
-            title: name + ' : ' + act.Name(),
-            html: true,
-            content: function () {
-                $('a.edit-activity').popover('hide');
-                div.find("a.edit-activity").addClass(act.WebId());
-                return div.find("div.context-menu").html();
-            } 
+        // just display it for 5 seconds
+        pop.on('shown.bs.popover', function () {
+            setTimeout(function () { pop.popover('hide'); }, 5000);
         });
+
         $(document).on('click', 'a.' + act.WebId(), function (e) {
             e.preventDefault();
-            wd.editActivity(act)();
+            var app = require(objectbuilders.app),
+                link = $(this);
+            if (link.hasClass("delete-activity")) {
+                app.showMessage("Are you sure you want to remove this Activity", "Remove activity", ["Yes", "No"])
+                    .done(function (result) {
+                        if (result === "Yes") {
+                            wd.removeActivity(act)();
+                        }
+                    });
+            }
+            if (link.hasClass("edit-activity")) {
+
+                wd.editActivity(act)();
+            }
         });
     }
 };
