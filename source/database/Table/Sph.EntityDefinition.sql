0638f288a9c24a11b9df7bf167879a9db7b07863
erymuzuan <erymuzuan@hotmail.com>
2013-12-31 14:35:45 +0800

    the table
    Related Work Items: #1424

---
+++ source/database/Table/Sph.EntityDefinition.sql
@@ -0,0 +1,17 @@
+USE [Sph]
+IF OBJECT_ID('Sph.EntityDefinition', 'U') IS NOT NULL
+  DROP TABLE Sph.EntityDefinition
+GO
+
+CREATE TABLE Sph.EntityDefinition
+(
+	 [EntityDefinitionId] INT PRIMARY KEY IDENTITY(1,1)
+	,[Data] XML NOT NULL
+	,[Name] VARCHAR(255) NOT NULL
+	,[Plural] VARCHAR(255) NOT NULL
+	,[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[CreatedBy] VARCHAR(255) NULL
+	,[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+	,[ChangedBy] VARCHAR(255) NULL
+)
+GO 
