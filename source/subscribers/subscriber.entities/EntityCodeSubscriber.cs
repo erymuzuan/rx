452f5ccaa4eab80a718d5b06e5e558890ef1938e
erymuzuan <erymuzuan@hotmail.com>
2014-01-07 16:36:31 +0800

    entity subscriber

---
+++ source/subscribers/subscriber.entities/EntityCodeSubscriber.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.SubscribersInfrastructure;
+
+namespace subscriber.entities
+{
+    public class EntityCodeSubscriber : Subscriber<EntityDefinition>
+    {
+        public override string QueueName
+        {
+            get { return "ed_code_gen"; }
+        }
+
+        public override string[] RoutingKeys
+        {
+            get { return new[] { typeof(EntityDefinition).Name + ".#.Save" }; }
+        }
+
+        protected  override Task ProcessMessage(EntityDefinition item, MessageHeaders header)
+        {  var applicationName = ConfigurationManager.ApplicationName;
+
+            var code = new StringBuilder("");
+            code.AppendLine("using System;");
+            code.AppendLinf("namespace Bespoke.{0}.Domain", applicationName);
+            code.AppendLine("{");
+            code.AppendLinf("   public class {0} : Entity", item.Name);
+            code.AppendLine("   {");
+
+
+
+            code.AppendLine("   }");
+            code.AppendLine("}");
+            
+            return Task.FromResult(0);
+        }
+
+
+
+    }
+}
---
+++ source/subscribers/subscriber.entities/SqlTableSubscriber.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.SubscribersInfrastructure;
+
+namespace subscriber.entities
+{
+    public class SqlTableSubscriber : Subscriber<EntityDefinition>
+    {
+        public override string QueueName
+        {
+            get { return "ed_sql_table"; }
+        }
+
+        public override string[] RoutingKeys
+        {
+            get { return new[] { typeof(EntityDefinition).Name + ".added.#" }; }
+        }
+
+        protected async override Task ProcessMessage(EntityDefinition item, MessageHeaders header)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["sph"].ConnectionString;
+            var applicationName = ConfigurationManager.ApplicationName;
+
+            var sql = new StringBuilder();
+            sql.AppendFormat("CREATE TABLE [{0}].[{1}]", applicationName,item.Name);
+            sql.AppendLine("(");
+            sql.AppendLine("  [Id] NVARCHAR(50) NOT NULL PRIMARY KEY");
+            var members = item.MemberCollection.Where(m => m.IsFilterable);
+            foreach (var member in members)
+            {
+                sql.AppendFormat(",[{0}] {1} {2} NULL", member.Name, member.TypeName, member.IsRequired ? "NOT" : "");
+                sql.AppendLine("");
+            }
+            sql.AppendLine(")");
+
+            using (var conn = new SqlConnection(connectionString))
+            using(var cmd = new SqlCommand(sql.ToString(),conn))
+            {
+                await conn.OpenAsync();
+                await cmd.ExecuteNonQueryAsync();
+
+            }
+
+        }
+    }
+}
