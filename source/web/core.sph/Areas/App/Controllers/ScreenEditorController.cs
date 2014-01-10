ee4fa1c45d5fd736f50bf44306d56fbc4ae1bc20
erymuzuan <erymuzuan@hotmail.com>
2014-01-10 08:29:09 +0800

    move the screen editor
    Related Work Items: #1438

---
+++ source/web/core.sph/Areas/App/Controllers/ScreenEditorController.cs
@@ -0,0 +1,38 @@
+using System.Web.Mvc;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.Web.ViewModels;
+
+namespace Bespoke.Sph.Web.Areas.App.Controllers
+{
+    public class ScreenEditorController : BaseAppController
+    {
+        public ActionResult Js()
+        {
+            var vm = new TemplateFormViewModel();
+            vm.FormElements.RemoveAll(
+                f => f.GetType() == typeof(FormElement));
+            this.Response.ContentType = APPLICATION_JAVASCRIPT;
+            var script = this.RenderRazorViewToJs("Script", vm);
+            return Content(script);
+
+
+        }
+
+        public ActionResult Script()
+        {
+            var vm = new TemplateFormViewModel();
+            vm.FormElements.RemoveAll(
+                f => f.GetType() == typeof(FormElement));
+            return View(vm);
+        }
+
+        public ActionResult Html()
+        {
+            var vm = new TemplateFormViewModel
+            {
+                IsImportVisible = true
+            };
+            return View(vm);
+        }
+    }
+}
