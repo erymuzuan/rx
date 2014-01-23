4d487beeb5cc80e844d29bf5133018ff887a35ee
erymuzuan <erymuzuan@hotmail.com>
2014-01-24 06:02:05 +0800

    remove some old properties management classes

--- source/web/core.sph/SphApp/partial/SetterAction.min - Copy.js
+++
@@ -1,4 +0,0 @@
-﻿var bespoke=bespoke||{};bespoke.sph=bespoke.sph||{};bespoke.sph.domain=bespoke.sph.domain||{};bespoke.sph.domain.SetterActionPartial=function(){var n=require("durandal/system"),t=function(n){var t=this;return function(){t.SetterActionChildCollection.remove(n)}},i=function(){var t=new bespoke.sph.domain.SetterActionChild(n.guid());t.Field({Name:ko.observable("+ Field")});this.SetterActionChildCollection.push(t)};return{addChildAction:i,removeChildAction:t}};
-/*
-//# sourceMappingURL=SetterAction.min.js.map
-*/
