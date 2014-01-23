2503ee660e1d59b4ac60daacf58c8bd262a9347f
erymuzuan <erymuzuan@hotmail.com>
2014-01-23 08:45:52 +0800

    EntityView controller
    Related Work Items: #1433

---
+++ source/web/core.sph/Areas/Sph/Controllers/EntityViewController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.Web.Helpers;
+
+namespace Bespoke.Sph.Web.Areas.Sph.Controllers
+{
+    public class EntityViewController : Controller
+    {
+        public async Task<ActionResult> Save()
+        {
+            var ef = this.GetRequestJson<EntityView>();
+            var context = new SphDataContext();
+            using (var session = context.OpenSession())
+            {
+                session.Attach(ef);
+                await session.SubmitChanges("Save");
+            }
+            return Json(new { success = true, status = "OK", id = ef.EntityViewId });
+        }
+        public async Task<ActionResult> Publish()
+        {
+            var ef = this.GetRequestJson<EntityView>();
+            var context = new SphDataContext();
+            using (var session = context.OpenSession())
+            {
+                session.Attach(ef);
+                await session.SubmitChanges("Publish");
+            }
+            return Json(new { success = true, status = "OK", id = ef.EntityViewId });
+        }
+    }
+}
