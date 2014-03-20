ea16d754a9c698392c58ef76b8634a8a0d80d968
erymuzuan <erymuzuan@hotmail.com>
2014-03-21 07:11:28 +0800

    trailing slash added to the pattern
    Related Work Items: #1591

--- source/subscribers/subscriber.entities/IisRewriteRuleSubscriber.cs
+++ source/subscribers/subscriber.entities/IisRewriteRuleSubscriber.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Bespoke.Sph.Domain;
@@ -31,10 +30,10 @@ namespace subscriber.entities
             this.WriteMessage("IIS rewrite into {0}",path);
             var config = XDocument.Load( path);
             var api = config.Descendants("rule").Where(c => c.Attribute("name").Value == "entity.api");
-            api.Descendants("match").Single().Attribute("url").Value = string.Format("api/({0})", entities);
+            api.Descendants("match").Single().Attribute("url").Value = string.Format("api/({0})/", entities);
 
             var search = config.Descendants("rule").Where(c => c.Attribute("name").Value == "entity.search");
-            search.Descendants("match").Single().Attribute("url").Value = string.Format("search/({0})", entities);
+            search.Descendants("match").Single().Attribute("url").Value = string.Format("search/({0})/", entities);
             
             config.Save(path);
 
