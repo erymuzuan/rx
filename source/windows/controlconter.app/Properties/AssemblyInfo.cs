c431c10071fca63b367eb77776c2dc010e10c3d5
erymuzuan <erymuzuan@hotmail.com>
2014-04-03 08:25:20 +0800

    copy new one

--- source/windows/controlconter.app/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Rx Developer control center")]
-[assembly: AssemblyDescription("Rx Developer control center")]
-[assembly: AssemblyConfiguration("")]
