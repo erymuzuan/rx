86d83bcc42a354f6280e27e1a6155c509add3092
erymuzuan <erymuzuan@hotmail.com>
2014-04-12 08:01:47 +0800

    SignalR

---
+++ source/web/core.sph/Hubs/AuditTrailHub.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using Microsoft.AspNet.SignalR;
+
+namespace Bespoke.Sph.Web.Hubs
+{
+    public class AuditTrailHub : Hub
+    {
+        private IEntityChangedListener<AuditTrail> m_listener;
+        public override Task OnConnected()
+        {
+          
+            if (null != m_listener)
+            {
+                m_listener.Stop();
+            }
+            m_listener = ObjectBuilder.GetObject<IEntityChangedListener<AuditTrail>>();
+            m_listener.Changed += ListenerChanged;
+            m_listener.Run();
+
+            return base.OnConnected();
+            //return Connection.Send(connectionId, (new Message { Subject = "Welcome" }).ToJsonString());
+        }
+
+        void ListenerChanged(object sender, EntityChangedEventArgs<AuditTrail> e)
+        {
+            var json = e.Item.ToJsonString();
+            Clients.All.publishChanges(json, e.AuditTrail.Operation);
+        }
+
+        public void Register(string entity, int id)
+        {
+            
+        }
+      
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
