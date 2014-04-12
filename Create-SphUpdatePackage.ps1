8db1654e01d928323ca78b37eb267680e51bfe5a
erymuzuan <erymuzuan@hotmail.com>
2014-04-12 08:02:10 +0800

    Update package

---
+++ Create-SphUpdatePackage.ps1
@@ -0,0 +1,192 @@
+﻿Param(
+       [int]$Build ,
+       [switch]$PreRelease = $false
+     )
+
+
+if($Build -eq 0)
+{
+    Write-Host "Please specify the Build Number"
+    exit;
+}
+
+Write-Host "Have you compiled your solution and published web.sph ? (y/n)"
+$published = Read-Host
+if($published -ne "y")
+{
+    Write-Warning "Too bad .. bye"
+    exit;
+}
+
+#remove all the configs from subscribers
+ls -Path .\bin\subscribers -Filter *.config | Remove-Item
+
+
+# copy some dll into schedulers and subscribers
+copy .\source\web\web.sph\bin\Common.Logging.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.Mvc.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.Razor.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.WebPages.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.WebPages.Razor.dll .\bin\subscribers
+
+
+copy .\source\web\web.sph\bin\Common.Logging.dll .\bin\schedulers
+copy .\source\web\web.sph\bin\System.Web.Mvc.dll .\bin\schedulers
+copy .\source\web\web.sph\bin\System.Web.Razor.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.WebPages.dll .\bin\subscribers
+copy .\source\web\web.sph\bin\System.Web.WebPages.Razor.dll .\bin\subscribers
+
+
+copy source\web\web.sph\bin\System.Web.WebPages.Razor.dll bin\schedulers
+copy source\web\web.sph\bin\System.Web.WebPages.dll bin\schedulers
+copy source\web\web.sph\bin\System.Web.Mvc.dll bin\schedulers
+
+copy source\web\web.sph\bin\System.Web.WebPages.Razor.dll bin\subscribers
+copy source\web\web.sph\bin\System.Web.WebPages.dll bin\subscribers
+copy source\web\web.sph\bin\System.Web.Mvc.dll bin\subscribers
+
+$output = ".\sph.packages\output"
+#creates directory
+if(Test-Path($output))
+{
+    Remove-Item $output -Force -Recurse
+    Get-ChildItem -Path $output -Recurse |  Remove-Item $output -Force -Recurse
+}
+
+mkdir $output
+Write-Host "Creating $output directory"
+
+
+
+if((Test-Path("$output\schedulers")) -eq $false)
+{
+    mkdir "$output\schedulers"
+}
+if((Test-Path("$output\subscribers")) -eq $false)
+{
+    mkdir "$output\subscribers"
+}
+if((Test-Path("$output\subscribers.host")) -eq $false)
+{
+    mkdir "$output\subscribers.host"
+}
+if((Test-Path("$output\web")) -eq $false)
+{
+    mkdir "$output\web"
+}
+
+if((Test-Path("$output\tools")) -eq $false)
+{
+    mkdir "$output\tools"
+}
+
+
+#schedulers
+Get-ChildItem -Filter *.* -Path ".\bin\schedulers" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\schedulers" -Force
+
+
+#subscribers
+Get-ChildItem -Filter *.* -Path ".\bin\subscribers" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\subscribers" -Force
+
+
+
+#subscribers.host
+Get-ChildItem -Filter *.* -Path ".\bin\subscribers.host" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\subscribers.host" -Force
+
+
+
+#tools
+Get-ChildItem -Filter *.* -Path ".\bin\tools" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\tools" -Force -Recurse
+
+#web
+Get-ChildItem -Filter *.* -Path ".\bin\web" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\web" -Force -Recurse
+
+
+#web.bin -- for dependencies
+Get-ChildItem -Filter *.* -Path ".\source\web\web.sph\bin" `
+| ? { $_.Name.StartsWith("workflows.") -eq $false} `
+| ? { $_.Name.StartsWith("Dev.") -eq $false} `
+| ? { $_.Name.EndsWith(".config") -eq $false} `
+| ? { $_.Name.EndsWith(".xml") -eq $false} `
+| Copy-Item -Destination "$output\web\bin" -Force
+
+
+
+
+
+
+
+#delete all accidentally added config
+$rubbishConfigs = @("$output\subscribers\subscriber.workflow.dll.config"
+,"$output\schedulers\scheduler.delayactivity.config"
+,"$output\schedulers\razor.template.dll.config"
+,"$output\schedulers\scheduler.workflow.trigger.config"
+,"$output\schedulers\sql.repository.dll.config"
+,"$output\subscribers\razor.template.dll.config"
+,"$output\subscribers\sql.repository.dll.config"
+)
+foreach($ucon in $rubbishConfigs)
+{
+    if((Test-Path $ucon) -eq $true){
+        Remove-Item $ucon
+    }
+}
+
+
+#control.center
+mkdir "$output\control.center"
+Get-ChildItem -Filter *.* -Path ".\sph.packages\control.center" `
+| Copy-Item -Destination "$output\control.center" -Force -Recurse
+
+
+#databases and mapping
+mkdir $output\database
+Get-ChildItem -Filter *.* -Path ".\source\database" `
+| Copy-Item -Destination "$output\database" -Force -Recurse
+mkdir $output\database\mapping
+Get-ChildItem -Filter *.* -Path ".\source\elasticsearch\mapping" `
+| Copy-Item -Destination "$output\database\mapping" -Force -Recurse
+
+
+#remove the custom triggers
+Get-Item -Path .\sph.packages\output\subscribers\subscriber.trigger.* `
+| ? { $_.Name.EndsWith("trigger.dll") -eq $false} `
+| ? { $_.Name.EndsWith("trigger.pdb") -eq $false} `
+| Remove-Item
+
+# remove workers service runner - just run the workser console runner
+ls -Path $output\subscribers.host -Filter workers.windowsservice.runner.* | Remove-Item
+
+Write-Host ""
+
+Write-Host ""
+
+Write-Host "Please check for any errors, Press [Enter] to continue packaging into 7z or q to exit"
+$compressed = Read-Host
+if($compressed -eq 'q')
+{
+    exit;
+}
+
+#compress
+& 7za a -t7z ".\rx.update.$Build.7z" ".\sph.packages\output\*"
