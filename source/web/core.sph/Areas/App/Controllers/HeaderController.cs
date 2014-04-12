743c3ddd12dd5a0cbda57483986c468c4a54f747
erymuzuan <erymuzuan@hotmail.com>
2014-04-12 08:00:04 +0800

    Nocache attribute

--- source/web/core.sph/Areas/App/Controllers/HeaderController.cs
+++ source/web/core.sph/Areas/App/Controllers/HeaderController.cs
@@ -1,17 +1,19 @@
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Bespoke.Sph.Domain;
+using Bespoke.Sph.Web.Filters;
 
 namespace Bespoke.Sph.Web.Areas.App.Controllers
 {
     public  class HeaderController : BaseAppController
     {
+        [NoCache]
         public async  Task<ActionResult> Html()
         {
             var context = new SphDataContext();
             var profile = await context.LoadOneAsync<UserProfile>(ua => ua.UserName == User.Identity.Name);
             if (null != profile)
-                ViewBag.StartModule = "#/"+profile.StartModule;
+                ViewBag.StartModule = "#"+profile.StartModule;
             return View();
         }
     }
