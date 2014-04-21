e0a0f7b58effb3513a2bc4e54dc77a555b43f6a6
erymuzuan <erymuzuan@hotmail.com>
2014-04-21 19:18:08 +0800

    template builder, untested
    Related Work Items: #1657

---
+++ source/tools/sph.builder/DocumentTemplateBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+
+namespace sph.builder
+{
+    public class DocumentTemplateBuilder : Builder<DocumentTemplate>
+    {
+
+        public override async Task Restore()
+        {
+            await base.Restore();
+
+            var folder = ConfigurationManager.WorkflowSourceDirectory + @"\DocumentTemplate";
+            foreach (var file in Directory.GetFiles(folder, "*.json"))
+            {
+                var json = File.ReadAllText(file);
+                var view = json.DeserializeFromJson<DocumentTemplate>();
+                await InsertTemplateAsync(view);
+            }
+            Console.WriteLine("Done DocumentTemplate");
+
+        }
+
+        private async Task InsertTemplateAsync(DocumentTemplate ed)
+        {
+            const string extension = ".docx";
+            var wc = ConfigurationManager.WorkflowSourceDirectory;
+            var folder = Path.Combine(wc, typeof(DocumentTemplate).Name);
+            var icon = Path.Combine(folder, ed.Name + extension);
+            if (!File.Exists(icon)) return;
+            
+            var store = ObjectBuilder.GetObject<IBinaryStore>();
+            var schema = new BinaryStore
+            {
+                Content = File.ReadAllBytes(icon),
+                Extension = extension,
+                StoreId = ed.WordTemplateStoreId,
+                FileName = ed.Name + extension,
+                WebId = ed.WordTemplateStoreId
+            };
+            await store.DeleteAsync(ed.WordTemplateStoreId);
+            await store.AddAsync(schema);
+        }
+
+    }
+}
