f36c13e375bffa6705145cda12ff8ef1bbe89b97
erymuzuan <erymuzuan@hotmail.com>
2014-04-30 08:48:20 +0800

    add the custom entity dahsboard as options to startpage
    Related Work Items: #1799

--- source/web/core.sph/Areas/App/Controllers/RoleSettingsController.cs
+++ source/web/core.sph/Areas/App/Controllers/RoleSettingsController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Bespoke.Sph.Domain;
 using Bespoke.Sph.Web.Areas.Sph.Controllers;
 using Bespoke.Sph.Web.Models;
 using Bespoke.Sph.Web.ViewModels;
+using Humanizer;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -11,19 +13,44 @@ namespace Bespoke.Sph.Web.Areas.App.Controllers
 {
     public class RoleSettingsController : BaseController
     {
-        public ActionResult Html()
+        public async Task<ActionResult> Html()
         {
             var rolesConfig = Server.MapPath("~/roles.config.js");
             var routeConfig = Server.MapPath("~/routes.config.js");
             var json = System.IO.File.ReadAllText(rolesConfig);
             var routeJson = System.IO.File.ReadAllText(routeConfig);
+            var context = new SphDataContext();
+
+            var edQuery = context.EntityDefinitions.Where(e => e.IsPublished == true);
+            var edLo = await context.LoadAsync(edQuery, includeTotalRows: true);
+            var entityDefinitions = new ObjectCollection<EntityDefinition>(edLo.ItemCollection);
+
+
+            while (edLo.HasNextPage)
+            {
+                edLo = await context.LoadAsync(edQuery, edLo.CurrentPage + 1, includeTotalRows: true);
+                entityDefinitions.AddRange(edLo.ItemCollection);
+            }
+            var edRoutes = from t in entityDefinitions
+                           select new JsRoute
+                           {
+                               Title = t.Plural,
+                               Route = string.Format("{0}", t.Name.ToLowerInvariant()),
+                               Caption = t.Plural,
+                               Icon = t.IconClass,
+                               ModuleId = string.Format("viewmodels/{0}", t.Name.ToLowerInvariant()),
+                               Nav = t.IsShowOnNavigationBar
+                           };
 
             var vm = new RoleSettingViewModel();
             var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
             var roles = JsonConvert.DeserializeObject<RoleModel[]>(json, settings);
             var routes = JsonConvert.DeserializeObject<JsRoute[]>(routeJson, settings);
+
+
             vm.Roles.ClearAndAddRange(roles);
             vm.Routes.ClearAndAddRange(routes);
+            vm.Routes.AddRange(edRoutes);
             return View(vm);
         }
         public ActionResult Js()
@@ -50,5 +77,5 @@ namespace Bespoke.Sph.Web.Areas.App.Controllers
 
             return View(roles.AsEnumerable());
         }
-	}
+    }
 }
