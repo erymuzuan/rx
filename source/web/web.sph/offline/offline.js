b8e7543f1d6d2f41b899c0235ddaa2895cb792a1
erymuzuan <erymuzuan@hotmail.com>
2014-05-05 12:21:17 +0800

    offline

--- source/web/web.sph/offline/offline.js
+++ source/web/web.sph/offline/offline.js
@@ -1,14 +1,25 @@
-  $(function(){
-        $('header#header').load('header.html');
+$(function () {
+    $('header#header').load('header.html');
 
-        window.applicationCache.addEventListener('updateready', function(e){
-        	if(window.applicationCache.status === window.applicationCache.UPDATEREADY){
-        		window.applicationCache.swapCache();
-        		if(window.confirm('New version has been downloaded, Do you want to reload the app?')){
-        			window.location.reload();
-        		}
-        	}
+    console.log("indexDB", typeof indexedDB);
+    if (!indexedDB) {
+        alert("Your browser is not compatible with this offline app");
+        return;
+    }
 
-        }, false);
+    console.log("Promise", typeof Promise);
+    if (typeof Promise === "undefined") {
+        alert("Your browser is not compatible with this offline app");
+        return;
+    }
+    window.applicationCache.addEventListener('updateready', function (e) {
+        if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
+            window.applicationCache.swapCache();
+            if (window.confirm('New version has been downloaded, Do you want to reload the app?')) {
+                window.location.reload();
+            }
+        }
 
- });
+    }, false);
+
+});
--- source/web/web.sph/offline/patient.js
+++ source/web/web.sph/offline/patient.js
@@ -11,87 +11,83 @@ require.config({
         'plugins': '/Scripts/durandal/plugins'
     }
 });
-define('jquery', function () { return jQuery; });
+define('jquery', function () {
+    return jQuery;
+});
 define('knockout', ko);
 
 
-require(['services/datacontext', 'jquery', 'services/app', 'services/system', objectbuilders.logger], 
-    function (context, jquery, app, system, logger) {
+require(['services/offline-datacontext', 'jquery', 'services/app', 'services/system', objectbuilders.logger],
+    function (context, jquery, app, system, logger, db) {
         var list = ko.observableArray(),
             online = ko.observable(window.navigator.onLine),
             errors = ko.observableArray(),
-            removeFromOffline = function(item){
-                var tcs = new $.Deferred();
+            removeFromOffline = function (item) {
                 list.remove(item);
-
-                var json = ko.mapping.toJSON(list);    
-
-                localforage.setItem('patients', json).then( function(e){
-                    tcs.resolve(true);
-                });
-
-                return tcs.promise();
-                // 019-9607794
+                return context.deleteAsync(ko.unwrap(item.WebId));
             },
-            send = function(operation){
+            send = function (operation) {
                 var item = this;
-                return function(){
+                return function () {
                     var tcs = new $.Deferred();
 
-                    context.post(ko.mapping.toJSON(item), "/Patient/" + operation)
-                    .then(function (result) {
-                        if (result.success) {
-                            errors.removeAll();
+                    context.sendAsync({body: ko.mapping.toJSON(item),
+                        method: "POST",
+                        url: "/Patient/" + operation})
+                        .then(function (result) {
+                            if (result.success) {
+                                errors.removeAll();
 
-                            app.showMessage("Ok done", "SPH Platform showcase", ["OK"])
-                                .done(function (dialogResult) {
-                                    removeFromOffline(item).then(tcs.resolve);
-                                    logger.info("Your data has been successfully sent to the server with this id " + result.id);
-                            
+                                app.showMessage("Ok done", "SPH Platform showcase", ["OK"])
+                                    .done(function () {
+                                        removeFromOffline(item).then(tcs.resolve);
+                                        logger.info("Your data has been successfully sent to the server with this id " + result.id);
+
+                                    });
+
+                            } else {
+                                errors.removeAll();
+                                _(result.rules).each(function (v) {
+                                    errors(v.ValidationErrors);
                                 });
-
-                        } else {
-                            errors.removeAll();
-                            _(result.rules).each(function (v) {
-                                errors(v.ValidationErrors);
-                            });
-                            logger.error("There are errors in your entity, !!!");
-                        }
-                        tcs.resolve(result);
-                    });
+                                logger.error("There are errors in your entity, !!!");
+                            }
+                            tcs.resolve(result);
+                        });
 
                     return tcs.promise();
 
                 };
             },
             vm = {
-                    errors: errors,
-                    online : online,
-                    list: list,
-                    entity: ko.observable(new bespoke.dev_2002.domain.Patient({ WebId: system.guid() })),
-                    sendOperation: send
-                };
+                errors: errors,
+                online: online,
+                list: list,
+                entity: ko.observable(new bespoke.dev_2002.domain.Patient({ WebId: system.guid() })),
+                sendOperation: send
+            };
 
         ko.applyBindings(vm);
 
-         localforage.getItem('patients').then(function(r){
-            var sr = r || "[]",
-                patients = ko.mapping.fromJSON(sr);
-            list(patients());
-        });
 
-      function updateOnlineStatus(event) {
-        var condition = navigator.onLine ? "online" : "offline",
-            status = {};
+        context.openAsync({database: 'dev', store: 'Patient'})
+            .then(function () {
+                console.log("Successfully open the database");
+                return context.loadAsync();
+            })
+            .then(function (results) {
+                console.log("list", results);
+                list(results);
+            });
 
-        status.className = condition;
-        status.innerHTML = condition.toUpperCase();
+        function updateOnlineStatus(event) {
+            var condition = navigator.onLine ? "online" : "offline";
 
-        logger.info("Event: " + event.type + "; Status: " + condition);
-        online(window.navigator.onLine);
-      }
+            logger.info("Event: " + event.type + "; Status: " + condition);
+            online(window.navigator.onLine);
+        }
 
-      window.addEventListener('online',  updateOnlineStatus);
-      window.addEventListener('offline', updateOnlineStatus);
-});
+        window.addEventListener('online', updateOnlineStatus);
+        window.addEventListener('offline', updateOnlineStatus);
+    });
       
