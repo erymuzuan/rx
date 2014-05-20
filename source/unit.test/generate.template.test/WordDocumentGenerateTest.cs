912727795ea0a00f4b7f6b658a30b4179e941380
erymuzuan <erymuzuan@hotmail.com>
2014-05-20 12:42:11 +0800

    rename

--- source/unit.test/generate.template.test/WordDocumentGenerateTest.cs
+++ source/unit.test/generate.template.test/WordDocumentGenerateTest.cs
@@ -18,8 +18,8 @@ namespace generate.template.test
                 Name = "1234",
                 StartModule = "test"
             };
-            IDocumentGenerator gen = new WordGenerator{DefaultNamespace = typeof(Designation).Namespace};
-            Console.WriteLine(((WordGenerator) gen).DefaultNamespace);
+            IDocumentGenerator gen = new WordGenerator { DefaultNamespace = typeof(Designation).Namespace };
+            Console.WriteLine(((WordGenerator)gen).DefaultNamespace);
             var output = Path.GetTempFileName() + ".docx";
             File.Copy(@"\project\work\sph\source\unit.test\generate.template.test\DATETIME.docx", output);
 
