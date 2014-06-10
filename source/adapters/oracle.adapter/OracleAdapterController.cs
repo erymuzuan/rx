d240e57665b38b351e4282814e4ff5eaa65425f1
erymuzuan <erymuzuan@hotmail.com>
2014-06-10 19:46:07 +0800

    parent and child foreign key

--- source/adapters/oracle.adapter/OracleAdapterController.cs
+++ source/adapters/oracle.adapter/OracleAdapterController.cs
@@ -52,6 +52,51 @@ namespace Bespoke.Sph.Integrations.Adapters
             }
         }
 
+
+        public async Task<ActionResult> Relationships()
+        {
+            var json = this.GetRequestBody();
+            var o = JObject.Parse(json);
+            var table = o.SelectToken("$.table").Value<string>();
+            var schema = o.SelectToken("$.schema").Value<string>();
+            var cs = o.SelectToken("$.connectionString").Value<string>();
+            var sql =
+                string.Format(
+                    @"
+SELECT FK.TABLE_NAME AS CHILD_TABLE
+     , SRC.TABLE_NAME AS PARENT_TABLE
+     , FK.CONSTRAINT_NAME AS FK_CONSTRAINT
+     , SRC.CONSTRAINT_NAME AS REFERENCED_CONSTRAINT
+FROM ALL_CONSTRAINTS FK
+JOIN ALL_CONSTRAINTS SRC ON FK.R_CONSTRAINT_NAME = SRC.CONSTRAINT_NAME
+WHERE FK.CONSTRAINT_TYPE = 'R'
+  AND SRC.OWNER = '{1}'
+  AND (SRC.TABLE_NAME = '{0}' OR FK.TABLE_NAME = '{0}')",
+                    table, schema);
+            using (var conn = new OracleConnection(cs))
+            using (var cmd = new OracleCommand(sql, conn))
+            {
+                await conn.OpenAsync();
+                using (var reader = (OracleDataReader)await cmd.ExecuteReaderAsync())
+                {
+                    var list = new List<object>();
+                    while (await reader.ReadAsync())
+                    {
+                        var r = new
+                        {
+                            src_table_name = reader.GetOracleString(0).Value,
+                            fk_table_name = reader.GetOracleString(1).Value,
+                            src_constraint_name = reader.GetOracleString(2).Value,
+                            fk_constraint_name = reader.GetOracleString(3).Value,
+                        };
+                        list.Add(r);
+                    }
+                    return Json(list.ToArray(), JsonRequestBehavior.AllowGet);
+                }
+
+            }
+        }
+
         public string GetRequestBody()
         {
             using (var reader = new StreamReader(this.Request.InputStream))
--- source/web/web.sph/SphApp/viewmodels/adapter.oracle.js
+++ source/web/web.sph/SphApp/viewmodels/adapter.oracle.js
@@ -9,7 +9,7 @@
 
 
 define(['services/datacontext', 'services/logger', 'plugins/router'],
-    function (context, logger, router) {
+    function (context) {
 
         var adapter = ko.observable(
             {
@@ -25,7 +25,7 @@ define(['services/datacontext', 'services/logger', 'plugins/router'],
             activate = function () {
 
             },
-            attached = function () {
+            attached = function (view) {
                 adapter().connectionString.subscribe(function (cs) {
                     isBusy(true);
                     $.get("/oracleadapter/schemas?cs=" + cs)
@@ -40,8 +40,45 @@ define(['services/datacontext', 'services/logger', 'plugins/router'],
                     $.get("/oracleadapter/tables/" + schema + "?cs=" + cs)
                         .done(function (result) {
                             isBusy(false);
-                            tableOptions(result);
+                            var tables = _(result).map(function (v) {
+                                return {
+                                    name: v,
+                                    parents: ko.observableArray(),
+                                    children: ko.observableArray()
+                                };
+                            });
+                            tableOptions(tables);
                         });
+                });
+
+                $(view).on('click', 'input[type=checkbox]', function () {
+                    var table = ko.dataFor(this);
+                    console.log(table.name);
+                    var o = {
+                        connectionString: adapter().connectionString(),
+                        schema: adapter().schema(),
+                        table: table.name
+                    };
+                    var tcs = new $.Deferred();
+                    var data = JSON.stringify(o);
+                    isBusy(true);
+
+                    context.post(data, "/oracleadapter/relationships")
+                        .then(function (result) {
+                            isBusy(false);
+                            var children = _(result).filter(function (v) {
+                                return v.fk_table_name === table.name;
+                            });
+                            table.children(children);
+                            var parents = _(result).filter(function (v) {
+                                return v.src_table_name === table.name;
+                            });
+                            table.parents(parents);
+
+                            tcs.resolve(result);
+                        });
+                    return tcs.promise();
+
                 });
             },
             generate = function () {
