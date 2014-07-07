241d3f9134fe7aa144317da1b50e9fda5f9facb3
erymuzuan <erymuzuan@hotmail.com>
2014-07-08 05:37:43 +0800

    adapter ui

---
+++ source/web/web.sph/SphApp/viewmodels/adapter.ftp.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../../Scripts/jquery-2.1.1.intellisense.js" />
+/// <reference path="../../Scripts/knockout-3.1.0.debug.js" />
+/// <reference path="../../Scripts/knockout.mapping-latest.debug.js" />
+/// <reference path="../../Scripts/require.js" />
+/// <reference path="../../Scripts/underscore.js" />
+/// <reference path="../../Scripts/moment.js" />
+/// <reference path="../services/datacontext.js" />
+/// <reference path="../schemas/sph.domain.g.js" />
+
+
+define(['services/datacontext', 'services/logger', 'plugins/router'],
+    function(context, logger, router) {
+
+        var
+            isBusy = ko.observable(false),
+            activate = function() {
+
+            },
+            attached = function(view) {
+
+            };
+
+        var vm = {
+            isBusy: isBusy,
+            activate: activate,
+            attached: attached
+        };
+
+        return vm;
+
+    });
