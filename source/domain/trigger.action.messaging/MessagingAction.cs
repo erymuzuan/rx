fa7f22bef88857fcc3a993625c7e60fafb630482
erymuzuan <erymuzuan@hotmail.com>
2014-08-08 09:18:50 +0800

    Creates a new CustomAction in different dll
    Related Work Items: #2007

---
+++ source/domain/trigger.action.messaging/MessagingAction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using Bespoke.Sph.Domain;
+
+namespace Bespoke.Sph.Messaging
+{
+    [Export(typeof(CustomAction))]
+    [DesignerMetadata(Name = "Messaging", Description = "Re route the message to the specified adapter send port", FontAwesomeIcon = "chevron-circle-right")]
+    public class MessagingAction : CustomAction
+    {
+        public override bool UseAsync
+        {
+            get { return true; }
+        }
+    }
+}
