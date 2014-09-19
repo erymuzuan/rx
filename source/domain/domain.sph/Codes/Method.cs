c5954faa71c06ef3f8e824ce298d1f610c2dfe7d
erymuzuan <erymuzuan@hotmail.com>
2014-09-19 08:00:27 +0800

    wrongly ignored

---
+++ source/domain/domain.sph/Codes/Method.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Bespoke.Sph.Domain.Codes
+{
+    public class Method
+    {
+        public Modifier Modifier { get; set; }
+        public Type ReturnType { get; set; }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+        public string Code { get; set; }
+
+        private readonly ObjectCollection<string> m_attributeCollection = new ObjectCollection<string>();
+
+        public ObjectCollection<string> AttributeCollection
+        {
+            get { return m_attributeCollection; }
+        }
+    }
+
+    public enum Modifier
+    {
+        Public,
+        Private,
+        Protected
+    }
+}
