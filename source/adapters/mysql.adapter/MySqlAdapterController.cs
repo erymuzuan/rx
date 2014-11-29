b4a70382002d8396b37fabbd0e6598ce69a973d1
erymuzuan <erymuzuan@hotmail.com>
2014-11-29 16:55:36 +0800

    working on mysql metada procedures
    Related Work Items: #2183

--- source/adapters/mysql.adapter/MySqlAdapterController.cs
+++ source/adapters/mysql.adapter/MySqlAdapterController.cs
@@ -81,6 +81,33 @@ namespace Bespoke.Sph.Integrations.Adapters
 
             }
         }
+        
+        
+        
+        //http://stackoverflow.com/questions/733349/list-of-stored-procedures-functions-mysql-command-line
+        [HttpPost]
+        [Route("procedures")]
+        public async Task<HttpResponseMessage> GetProceduresAsync([FromBody]MySqlAdapter adapter)
+        {
+            using (var conn = new MySqlConnection(adapter.ConnectionString))
+
+            using (var cmd = new MySqlCommand("show procedure status", conn))
+            {
+                await conn.OpenAsync();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    var list = new List<string>();
+                    while (await reader.ReadAsync())
+                    {
+                        list.Add(reader.GetString(0));
+                    }
+                    var json = JsonConvert.SerializeObject(new { tables = list.ToArray(), success = true, status = "OK" });
+                    var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new JsonContent(json) };
+                    return response;
+                }
+
+            }
+        }
 
 
     }
