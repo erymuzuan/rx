0ff8dfb0994abf95d9f6efce5ec093387e5d7c4e
erymuzuan <erymuzuan@hotmail.com>
2014-12-02 08:09:30 +0800

    Started working on the MySQL procedure
    Related Work Items: #2191

---
+++ source/adapters/mysql.adapter/SprocResultMember.cs
@@ -0,0 +1,24 @@
+using System.Data;
+using Bespoke.Sph.Domain;
+
+namespace Bespoke.Sph.Integrations.Adapters
+{
+    public class SprocResultMember : Member
+    {
+        private SqlDbType m_sqlDbType;
+
+        public SqlDbType SqlDbType
+        {
+            get
+            {
+
+                if ((int)m_sqlDbType != 0)
+                    return m_sqlDbType;
+
+
+                return SqlDbType.VarChar;
+            }
+            set { m_sqlDbType = value; }
+        }
+    }
+}
