91da4d8a81a480222a5667b4d8450a7f9ab28e09
erymuzuan <erymuzuan@hotmail.com>
2014-12-18 09:46:16 +0800

    Using the Rosyln compiler to get diagnostics

--- source/unit.test/domain.test/triggers/AssemblyActionTextFixture.cs
+++ source/unit.test/domain.test/triggers/AssemblyActionTextFixture.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using NUnit.Framework;
 
 namespace domain.test.triggers
@@ -39,8 +45,78 @@ namespace domain.test.triggers
                     Script = "item"
                 }
             });
-            Console.WriteLine(action.GeneratorCode());
+            var code = @"
+using Bespoke.Sph.Domain;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 
+namespace Dev.SampleTriggers
+{
+  public class SampleActionTrilgger 
+  {
+    public async Task<object> Validate()
+    {
+        var item = new Bespoke.Dev_patient.Domain.Patient
+            {
+                FullName = ""Michael Scumacher"", 
+                Dob = new DateTime(1965,4,6)
+            };
+"
+                + (action.GeneratorCode())
+                +
+                @"
+     }
+  }
+}";
+            var tree = CSharpSyntaxTree.ParseText(code)
+                ;
+            var root = (CompilationUnitSyntax)tree.GetRoot().NormalizeWhitespace(indentation:"  ", elasticTrivia:true);
+            StringAssert.Contains("Validate", root.ToString());
+
+            //  root.AddUsings(new UsingDirectiveSyntax(new CSharpSyntaxNode(), root, 0));
+
+            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+                .WithOptimizationLevel(OptimizationLevel.Debug)
+                .WithUsings()
+                .WithScriptClassName("TestX");
+
+            var compiler = CSharpCompilation.Create("mine")
+                .WithOptions(options)
+                .AddReferences(MetadataReference.CreateFromFile("Dev.Patient.dll"),
+                this.CreateMetadataReference<object>(),
+                this.CreateMetadataReference<EnumerableQuery>(),
+                this.CreateMetadataReference<Task>(),
+                this.CreateMetadataReference<DateTime>(),
+                this.CreateMetadataReference<Uri>(),
+                this.CreateMetadataReference<Trigger>(),
+                typeof(System.Web.Mvc.Controller).CreateMetadataReference())
+                .AddSyntaxTrees(tree);
+            using (var stream = new FileStream("mine.dll", FileMode.Create))
+            {
+                var result = compiler.Emit(stream);
+                result.Diagnostics.AsEnumerable().ToList()
+                    .ForEach(d =>
+                    {
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine("{0} : {1}\r\n{2}", d.Location, d.GetMessage(), d.Category);
+
+                    });
+                Assert.IsTrue(result.Success);
+            }
+
+        }
+    }
+
+    public static class CompilerHelper
+    {
+        public static MetadataReference CreateMetadataReference(this Type type)
+        {
+            return MetadataReference.CreateFromAssembly(type.Assembly);
+        }
+        public static MetadataReference CreateMetadataReference<T>(this object type)
+        {
+            return MetadataReference.CreateFromAssembly((typeof(T)).Assembly);
         }
     }
 }
