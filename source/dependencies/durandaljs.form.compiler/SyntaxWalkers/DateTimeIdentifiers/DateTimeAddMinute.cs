ca508a53e7bcd7f349dad9175f743fb4a4441a05
erymuzuan <erymuzuan@hotmail.com>
2015-01-08 13:45:48 +0800

    budok2 wat kijo

---
+++ source/dependencies/durandaljs.form.compiler/SyntaxWalkers/DateTimeIdentifiers/DateTimeAddMinute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bespoke.Sph.FormCompilers.DurandalJs.SyntaxWalkers.DateTimeIdentifiers
+{
+    [Export("DateTime", typeof(IdentifierCompiler))]
+    [IdentifierCompilerMetadata(TypeName = "DateTime", Text = "AddMinutes")]
+    public class DateTimeAddMinute : IdentifierCompiler
+    {
+        public override string Compile(SyntaxNode node, IEnumerable<ExpressionSyntax> arguments)
+        {
+            var args = arguments.ToArray();
+            return "add(" + this.EvaluateExpressionCode(args[0]) + ", 'm')";
+        }
+    }
+}
---
+++ source/dependencies/durandaljs.form.compiler/SyntaxWalkers/DateTimeIdentifiers/DateTimeAddSecond.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bespoke.Sph.FormCompilers.DurandalJs.SyntaxWalkers.DateTimeIdentifiers
+{
+    [Export("DateTime", typeof(IdentifierCompiler))]
+    [IdentifierCompilerMetadata(TypeName = "DateTime", Text = "AddSeconds")]
+    public class DateTimeAddSecond : IdentifierCompiler
+    {
+        public override string Compile(SyntaxNode node, IEnumerable<ExpressionSyntax> arguments)
+        {
+            var args = arguments.ToArray();
+            return "add(" + this.EvaluateExpressionCode(args[0]) + ", 's')";
+        }
+    }
+}
