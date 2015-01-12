05e7ea5e775ca78394d4688882223bb64bee3fd7
erymuzuan <erymuzuan@hotmail.com>
2015-01-12 10:36:18 +0800

    Refactor form desginer to to use the interface
    Related Work Items: #2292

---
+++ source/web/core.sph/SphApp/services/form.designer.js
@@ -0,0 +1,230 @@
+﻿
+
+define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.system],
+    function (context, logger, system) {
+
+        var selectedFormElement = ko.observable(),
+            formElements = ko.observableArray(),
+            form = ko.observable({
+                Id: ko.observable(),
+                Name: ko.observable(),
+                Route: ko.observable(),
+                WebId: ko.observable(),
+                FormDesign: ko.observable({
+                    FormElementCollection: ko.observableArray()
+                })
+            }),
+            createElement = function (el) {
+                var fe = context.clone(el);
+                fe.isSelected = ko.observable(true);
+                fe.hasError = ko.observable(false);
+                fe.Label("Label " + (form().FormDesign().FormElementCollection().length + 1));
+                fe.CssClass("");
+                fe.Visible("true");
+                fe.Enable("true");
+                fe.Size("input-large");
+                fe.ElementId(ko.unwrap(fe.TypeName) + "_" + (form().FormDesign().FormElementCollection().length + 1));
+                fe.WebId(system.guid());
+
+                return fe;
+            },
+            attached = function (view, form1) {
+
+                form = form1;
+                var dropDown = function (e) {
+                    e.preventDefault();
+                    e.stopPropagation();
+
+                    var button = $(this);
+                    button.parent().addClass("open")
+                        .find("input:first").focus()
+                        .select();
+                };
+
+                // Fix input element click problem
+                $(view).on("click mouseup mousedown", ".dropdown-menu input, .dropdown-menu label",
+                    function (e) {
+                        e.stopPropagation();
+                    });
+                $("#template-form-designer").on("click", "button.dropdown-toggle", dropDown);
+
+
+                //toolbox item clicked
+                $("#add-field").on("click", "a", function (e) {
+                    e.preventDefault();
+                    _(form().FormDesign().FormElementCollection()).each(function (f) {
+                        f.isSelected(false);
+                    });
+
+                    var el = ko.dataFor(this).element;
+                    var fe = createElement(el);
+
+                    form().FormDesign().FormElementCollection.push(fe);
+                    selectedFormElement(fe);
+
+
+                });
+
+                // kendoEditor
+                $("#template-form-designer").on("click", "textarea", function () {
+                    var $editor = $(this),
+                        kendoEditor = $editor.data("kendoEditor");
+                    if (!kendoEditor) {
+                        var htmlElement = ko.dataFor(this),
+                            editor = $editor.kendoEditor({
+                                change: function () {
+                                    htmlElement.Text(this.value());
+                                }
+                            }).data("kendoEditor");
+
+                        htmlElement.Text.subscribe(function (t) {
+                            editor.value(ko.unwrap(t));
+                        });
+
+                    }
+                }
+                );
+
+
+                var receive = {},
+                    initDesigner = function () {
+                        $("#template-form-designer>form").sortable({
+                            items: ">div",
+                            placeholder: "ph",
+                            helper: "original",
+                            dropOnEmpty: true,
+                            forcePlaceholderSize: true,
+                            forceHelperSize: false,
+                            receive: receive
+                        });
+                    };
+
+                receive = function (evt, ui) {
+                    $(".selected-form-element").each(function () {
+                        var kd = ko.dataFor(this);
+                        if (typeof kd.isSelected === "function")
+                            kd.isSelected(false);
+                    });
+
+                    var elements = _($("#template-form-designer>form>div")).map(function (div) {
+                        return ko.dataFor(div);
+                    }),
+                    fe = createElement(ko.dataFor(ui.item[0]).element),
+                    sortable = $(this),
+                    position = 0,
+                    children = sortable.children();
+
+                    for (var i = 0; i < children.length; i++) {
+                        if ($(children[i]).position().top > ui.position.top) {
+                            position = i;
+                            break;
+                        }
+                    }
+                    elements.splice(position, 0, fe);
+                    $("#template-form-designer>form").sortable("destroy");
+                    //rebuild
+                    form().FormDesign().FormElementCollection(elements);
+                    initDesigner();
+                    $("#template-form-designer>form li.ui-draggable").remove();
+                    selectedFormElement(fe);
+                };
+
+                initDesigner();
+
+                $.get("form-designer/toolbox-items", function (elements) {
+                    formElements(elements);
+                    $("#add-field>ul>li").draggable({
+                        helper: "clone",
+                        connectToSortable: "#template-form-designer>form"
+                    });
+                });
+
+
+                $("div.context-action-panel").on("click", "buton.close", function () {
+                    $(this).parents("div.context-action").hide();
+                });
+
+                selectedFormElement.subscribe(function (model) {
+                    model.Path.subscribe(function (p) {
+                        if (ko.unwrap(model.Label).indexOf("Label ") > -1) {
+                            model.Label(p.replace(".", " ")
+                                .replace(/([A-Z])/g, " $1").trim());
+                        }
+                    });
+                });
+
+
+            },
+            supportsHtml5Storage = function () {
+                try {
+                    return "localStorage" in window && window["localStorage"] !== null;
+                } catch (e) {
+                    return false;
+                }
+            },
+            getOrderedElements = function () {
+
+                // get the sorted element
+                var elements = _($("#template-form-designer>form>div")).map(function (div) {
+                    return ko.dataFor(div);
+                });
+                return elements();
+
+            },
+
+            selectFormElement = function (fe) {
+
+                $(".selected-form-element").each(function () {
+                    var kd = ko.dataFor(this);
+                    if (typeof kd.isSelected === "function")
+                        kd.isSelected(false);
+                });
+
+                if (typeof fe.isSelected === "undefined") {
+                    fe.isSelected = ko.observable(true);
+                }
+                if (typeof fe.hasError === "undefined") {
+                    fe.hasError = ko.observable(true);
+                }
+                fe.isSelected(true);
+                selectedFormElement(fe);
+                if (supportsHtml5Storage()) {
+                    localStorage.setItem(form().WebId(), ko.mapping.toJSON(form));
+                }
+            },
+            removeFormElement = function (fe) {
+                form().FormDesign().FormElementCollection.remove(fe);
+            },
+            showError = function (error) {
+                var fe = _(form().FormDesign().FormElementCollection())
+                    .find(function (f) {
+                        return ko.unwrap(f.ElementId) === error.ItemWebId;
+                    });
+                if (fe) {
+                    $(".selected-form-element").each(function () {
+                        var kd = ko.dataFor(this);
+                        if (typeof kd.isSelected === "function")
+                            kd.isSelected(false);
+                    });
+                    fe.isSelected(true);
+                    selectedFormElement(fe);
+                }
+            };
+
+        var vm = {
+            showError: showError,
+            attached: attached,
+            formElements: formElements,
+            selectedFormElement: selectedFormElement,
+            selectFormElement: selectFormElement,
+            removeFormElement: removeFormElement,
+            form: form,
+            getOrderedElements: getOrderedElements,
+            supportsHtml5Storage: supportsHtml5Storage
+        };
+
+        return vm;
+
+    });
+
+
--- source/web/core.sph/SphApp/viewmodels/entity.form.designer.js
+++ source/web/core.sph/SphApp/viewmodels/entity.form.designer.js
@@ -1,13 +1,12 @@
 ﻿
 
-define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router, objectbuilders.system, objectbuilders.app, objectbuilders.eximp, objectbuilders.dialog,"form.elements"],
-    function (context, logger, router, system, app, eximp, dialog) {
+define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router, objectbuilders.system, objectbuilders.app, objectbuilders.eximp, objectbuilders.dialog,
+     "services/form.designer"],
+    function (context, logger, router, system, app, eximp, dialog, designer) {
 
         var errors = ko.observableArray(),
             operationsOption = ko.observableArray(),
-            selectedFormElement = ko.observable(),
             layoutOptions = ko.observableArray(),
-            formElements = ko.observableArray(),
             entityOptions = ko.observableArray(),
             entity = ko.observable(new bespoke.sph.domain.EntityDefinition()),
             form = ko.observable(new bespoke.sph.domain.EntityForm({ WebId: system.guid() })),
@@ -77,202 +76,24 @@ define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router
 
             },
             attached = function (view) {
-
-                var fd = ko.unwrap(form().FormDesign);
-
-                var dropDown = function (e) {
-                    e.preventDefault();
-                    e.stopPropagation();
-
-                    var button = $(this);
-                    button.parent().addClass("open")
-                        .find("input:first").focus()
-                        .select();
-                };
-
-                // Fix input element click problem
-                $(view).on("click mouseup mousedown", ".dropdown-menu input, .dropdown-menu label",
-                    function (e) {
-                        e.stopPropagation();
-                    });
-                $("#template-form-designer").on("click", "button.dropdown-toggle", dropDown);
-
-
-                //toolbox item clicked
-                $("#add-field").on("click", "a", function (e) {
-                    e.preventDefault();
-                    _(fd.FormElementCollection()).each(function (f) {
-                        f.isSelected(false);
-                    });
-
-                    // clone
-                    var el = ko.dataFor(this).element;
-                    var fe = context.clone(el);
-                    fe.isSelected = ko.observable(true);
-                    fe.hasError = ko.observable(false);
-                    fe.Label("Label " + fd.FormElementCollection().length);
-                    fe.CssClass("");
-                    fe.Visible("true");
-                    fe.Enable("true");
-                    fe.Size("input-large");
-                    fe.ElementId(ko.unwrap(fe.TypeName) + "_" + (fd.FormElementCollection().length + 1));
-                    fe.WebId(system.guid());
-
-                    fd.FormElementCollection.push(fe);
-                    selectedFormElement(fe);
-
-
-                });
-
-                // kendoEditor
-                $("#template-form-designer").on("click", "textarea", function () {
-                    var $editor = $(this),
-                        kendoEditor = $editor.data("kendoEditor");
-                    if (!kendoEditor) {
-                        var htmlElement = ko.dataFor(this),
-                            editor = $editor.kendoEditor({
-                                change: function () {
-                                    htmlElement.Text(this.value());
-                                }
-                            }).data("kendoEditor");
-
-                        htmlElement.Text.subscribe(function (t) {
-                            editor.value(ko.unwrap(t));
-                        });
-
-                    }
-                }
-                );
-
-
-                var receive = {},
-                    initDesigner = function () {
-                        $("#template-form-designer>form").sortable({
-                            items: ">div",
-                            placeholder: "ph",
-                            helper: "original",
-                            dropOnEmpty: true,
-                            forcePlaceholderSize: true,
-                            forceHelperSize: false,
-                            receive: receive
-                        });
-                    };
-
-                receive = function (evt, ui) {
-                    $(".selected-form-element").each(function () {
-                        var kd = ko.dataFor(this);
-                        if (typeof kd.isSelected === "function")
-                            kd.isSelected(false);
-                    });
-
-                    var elements = _($("#template-form-designer>form>div")).map(function (div) {
-                        return ko.dataFor(div);
-                    }),
-                    fe = context.clone(ko.dataFor(ui.item[0]).element),
-                    sortable = $(this),
-                    position = 0,
-                    children = sortable.children();
-
-                    fe.isSelected = ko.observable(true);
-                    fe.hasError = ko.observable(false);
-                    fe.Enable("true");
-                    fe.Visible("true");
-                    fe.Label("Label " + (fd.FormElementCollection().length + 1));
-                    fe.ElementId(ko.unwrap(fe.TypeName) + "_" + (fd.FormElementCollection().length + 1));
-                    fe.WebId(system.guid());
-
-                    for (var i = 0; i < children.length; i++) {
-                        if ($(children[i]).position().top > ui.position.top) {
-                            position = i;
-                            break;
-                        }
-                    }
-                    elements.splice(position, 0, fe);
-                    $("#template-form-designer>form").sortable("destroy");
-                    //rebuild
-                    fd.FormElementCollection(elements);
-                    initDesigner();
-                    $("#template-form-designer>form li.ui-draggable").remove();
-                    selectedFormElement(fe);
-                };
-
-                initDesigner();
-
-                $.get("form-designer/toolbox-items", function (elements) {
-                    formElements(elements);
-                    $("#add-field>ul>li").draggable({
-                        helper: "clone",
-                        connectToSortable: "#template-form-designer>form"
-                    });
-                });
-
-
-                $("div.context-action-panel").on("click", "buton.close", function () {
-                    $(this).parents("div.context-action").hide();
-                });
-
-                selectedFormElement.subscribe(function (model) {
-                    model.Path.subscribe(function (p) {
-                        if (ko.unwrap(model.Label).indexOf("Label ") > -1) {
-                            model.Label(p.replace(".", " ")
-                                .replace(/([A-Z])/g, " $1").trim());
-                        }
-                    });
-                });
-
-
-            },
-            supportsHtml5Storage = function () {
-                try {
-                    return "localStorage" in window && window["localStorage"] !== null;
-                } catch (e) {
-                    return false;
-                }
+                designer.attached(view, form);
             },
             okClick = function (data, ev) {
                 if (bespoke.utils.form.checkValidity(ev.target)) {
 
                     var fd = ko.unwrap(form().FormDesign);
-                    // get the sorted element
-                    var elements = _($("#template-form-designer>form>div")).map(function (div) {
-                        return ko.dataFor(div);
-                    });
-                    fd.FormElementCollection(elements);
+                    fd.FormElementCollection(designer.getOrderedElements());
                     dialog.close(this, "OK");
-                    if (supportsHtml5Storage()) {
+                    if (designer.supportsHtml5Storage()) {
                         localStorage.removeItem(form().WebId());
                     }
                 }
             },
             cancelClick = function () {
-                if (supportsHtml5Storage()) {
+                if (designer.supportsHtml5Storage()) {
                     localStorage.removeItem(form().WebId());
                 }
                 dialog.close(this, "Cancel");
-            },
-            selectFormElement = function (fe) {
-
-                $(".selected-form-element").each(function () {
-                    var kd = ko.dataFor(this);
-                    if (typeof kd.isSelected === "function")
-                        kd.isSelected(false);
-                });
-
-                if (typeof fe.isSelected === "undefined") {
-                    fe.isSelected = ko.observable(true);
-                }
-                if (typeof fe.hasError === "undefined") {
-                    fe.hasError = ko.observable(true);
-                }
-                fe.isSelected(true);
-                selectedFormElement(fe);
-                if (supportsHtml5Storage()) {
-                    localStorage.setItem(form().WebId(), ko.mapping.toJSON(form));
-                }
-            },
-            removeFormElement = function (fe) {
-                var fd = ko.unwrap(form().FormDesign);
-                fd.FormElementCollection.remove(fe);
             },
             importCommand = function () {
                 return eximp.importJson()
@@ -291,12 +112,7 @@ define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router
             },
             publish = function () {
                 var fd = ko.unwrap(form().FormDesign);
-                // get the sorted element
-                var elements = _($("#template-form-designer>form>div")).map(function (div) {
-                    return ko.dataFor(div);
-                });
-                fd.FormElementCollection(elements);
-
+                fd.FormElementCollection(designer.getOrderedElements());
 
                 var tcs = new $.Deferred(),
                     data = ko.mapping.toJSON(form);
@@ -326,22 +142,6 @@ define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router
                     });
                 return tcs.promise();
 
-            },
-
-            showError = function (error) {
-                var fe = _(form().FormDesign().FormElementCollection())
-                    .find(function (f) {
-                        return ko.unwrap(f.ElementId) === error.ItemWebId;
-                    });
-                if (fe) {
-                    $(".selected-form-element").each(function () {
-                        var kd = ko.dataFor(this);
-                        if (typeof kd.isSelected === "function")
-                            kd.isSelected(false);
-                    });
-                    fe.isSelected(true);
-                    selectedFormElement(fe);
-                }
             },
             save = function () {
                 var fd = ko.unwrap(form().FormDesign);
@@ -420,18 +220,18 @@ define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router
             return tcs.promise();
         },
         partialEditor = null,
-        editCode = function () {
-            if (null == partialEditor || partialEditor.closed) {
-                var partial = "partial/" + form().Route();
-                partialEditor = window.open("/sph/editor/file?id=/sphapp/" + partial + ".js", '_blank', 'height=600px,width=800px,toolbar=0,location=0');
-                form().Partial(partial);
-            } else {
-                partialEditor.focus();
-            }
+        editCode = function() {
+                if (null == partialEditor || partialEditor.closed) {
+                    var partial = "partial/" + form().Route();
+                    partialEditor = window.open("/sph/editor/file?id=/sphapp/" + partial + ".js", '_blank', 'height=600px,width=800px,toolbar=0,location=0');
+                    form().Partial(partial);
+                } else {
+                    partialEditor.focus();
+                }
 
-            return Task.fromResult(true);
+                return Task.fromResult(true);
 
-        },
+            },
         layoutEditor = null,
         editLayout = function () {
             if (null == layoutEditor || layoutEditor.closed) {
@@ -446,15 +246,15 @@ define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router
 
         var vm = {
             errors: errors,
-            showError: showError,
+            showError: designer.showError,
             layoutOptions: layoutOptions,
             operationsOption: operationsOption,
             attached: attached,
             activate: activate,
-            formElements: formElements,
-            selectedFormElement: selectedFormElement,
-            selectFormElement: selectFormElement,
-            removeFormElement: removeFormElement,
+            formElements: designer.formElements,
+            selectedFormElement: designer.selectedFormElement,
+            selectFormElement: designer.selectFormElement,
+            removeFormElement: designer.removeFormElement,
             form: form,
             entity: entity,
             entityOptions: entityOptions,
