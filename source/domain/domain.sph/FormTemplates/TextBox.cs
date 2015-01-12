4bff695ba94bea15b61bf4bc868fcab86651efb7
Hassan Abu Bakar <hassan@bespoke.com.my>
2015-01-12 13:27:25 +0800

    bug 2299

--- source/domain/domain.sph/FormTemplates/TextBox.cs
+++ source/domain/domain.sph/FormTemplates/TextBox.cs
@@ -4,7 +4,7 @@ using System.Text;
 namespace Bespoke.Sph.Domain
 {
     [Export("FormDesigner", typeof(FormElement))]
-    [DesignerMetadata(Name = "Single Line Text", TypeName = "TextBox", FontAwesomeIcon = "text-width",Order = 1d, Description = "Creates na input for single line text")]
+    [DesignerMetadata(Name = "Single Line Text", TypeName = "TextBox", FontAwesomeIcon = "text-width",Order = 1d, Description = "Creates an input for single line text")]
     public partial class TextBox : FormElement
     {
 
