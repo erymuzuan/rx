be2089f18abffacf1a85ff6568d3678ba2abb164
erymuzuan <erymuzuan@hotmail.com>
2015-01-14 08:48:22 +0800

    screen activity form autosuggest use completely
    Related Work Items: #2292

--- source/domain/domain.sph/Triggers/ComplexVariable.cs
+++ source/domain/domain.sph/Triggers/ComplexVariable.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Linq;
+using System.Text;
 
 namespace Bespoke.Sph.Domain
 {
@@ -17,12 +18,32 @@ namespace Bespoke.Sph.Domain
             if (string.IsNullOrWhiteSpace(this.TypeName))
             {
                 result.Result = false;
-                result.Errors.Add(new BuildError(this.WebId, string.Format("[Variable] \"{0}\" does not have a valid type", this.Name) ));
+                result.Errors.Add(new BuildError(this.WebId, string.Format("[Variable] \"{0}\" does not have a valid type", this.Name)));
             }
 
             return result;
         }
 
-        
+        public override string GetJsonIntance(WorkflowDefinition wd)
+        {
+
+            var schema = wd.GetCustomSchema();
+
+            var xsd = new XsdMetadata(schema);
+            var members = xsd.GetMembersPath(this.TypeName);
+
+            var json = new StringBuilder();
+            json.AppendFormat("\"{0}\":", this.Name);
+            json.AppendLine("{");
+
+            var childs = members.Select(x => string.Format("\"{0}\":null", x));
+            json.AppendLine(string.Join(",\r\n", childs));
+
+            // TODO : split the child aggregate into it's own object
+
+            json.AppendLine("}");
+
+            return json.ToString();
+        }
     }
 }
