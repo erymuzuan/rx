50b78431168e6482f3cf8161515d323e61fa2b03
erymuzuan <erymuzuan@hotmail.com>
2015-01-14 17:37:30 +0800

    dead letter view password box

--- source/tools/dead.letter.viewer/ConnectionDialog.xaml.cs
+++ source/tools/dead.letter.viewer/ConnectionDialog.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Bespoke.Station.Windows.RabbitMqDeadLetter.ViewModels;
 
 namespace Bespoke.Station.Windows.RabbitMqDeadLetter
 {
@@ -7,10 +8,25 @@ namespace Bespoke.Station.Windows.RabbitMqDeadLetter
         public ConnectionDialog()
         {
             InitializeComponent();
+            this.Loaded += ConnectionDialog_Loaded;
+        }
+
+        void ConnectionDialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            var context = this.DataContext as ConnectionViewModel;
+            if (null != context)
+            {
+                passwordBox.Password = context.SelectedConnection.Password;
+            }
         }
 
         private void OkButtonClick(object sender, RoutedEventArgs e)
         {
+            var context = this.DataContext as ConnectionViewModel;
+            if (null != context)
+            {
+                context.SelectedConnection.Password = passwordBox.Password;
+            }
             this.DialogResult = true;
             this.Close();
         }
