0fe120f8818aac76c7dd6665740d52275cd7d16f
erymuzuan <erymuzuan@hotmail.com>
2015-01-20 12:12:54 +0800

    Splite the webs

---
+++ source/unit.test/durandaljs.compiler.test/SolutionCompilerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using NUnit.Framework;
+
+namespace durandaljs.compiler.test
+{
+    [TestFixture]
+    public class SolutionCompilerTestFixture
+    {
+        [Test]
+        [ExpectedException(typeof(NotImplementedException))]
+        public async Task CompileSimpleSolution()
+        {
+            var sol = new Solution { Id = "dev" };
+            ObjectBuilder.ComposeMefCatalog(sol);
+            var results = await sol.CompileAsync("DurandalJs");
+            Console.WriteLine(results);
+        }
+    }
+}
