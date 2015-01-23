66997f0749bb12951c7a67b8eadbabe6c2b879c1
erymuzuan <erymuzuan@hotmail.com>
2015-01-24 07:52:03 +0800

    Set up environment for V1

--- SetUpV1Environment.ps1
+++ SetUpV1Environment.ps1
@@ -1,7 +1,106 @@
 #REM Create V1 environment
 
 $applicationName = "DevV1"
-#create database
+$outputLog = ".\checkdatabase.log"
+
+#prerequisites
+
+Write-Host "Checking sqllocaldb" -ForegroundColor Cyan
+Start-Process sqllocaldb -ArgumentList @("i") -Wait -RedirectStandardOutput $outputLog -NoNewWindow
+if(Test-Path $outputLog){
+    $selectDatabase =  (Get-Content $outputLog) | Out-String
+    if($selectDatabase.Contains("Projects") -eq $false)
+    {
+        Start-Process sqllocaldb -ArgumentList @("c", "Projects") -Wait -NoNewWindow
+    }
+   
+    Remove-Item $outputLog
+}
+
+Write-Host "....done"
+Write-Host ""
+
+## database
+
+Write-Host "Checking database" -ForegroundColor Cyan
+$checkDatabase = Start-Process sqlcmd -ArgumentList @("-E","-S `"(localdb)\Projects`"", "-d master", "-Q `"SELECT [Name] FROM sysdatabases WHERE [Name]=`'$applicationName`'`"") -Wait -RedirectStandardOutput $outputLog -NoNewWindow
+if(Test-Path $outputLog){
+    $selectDatabase =  (Get-Content $outputLog) | Out-String
+    if($selectDatabase.Contains($applicationName))
+    {
+        Write-Host " $applicationName database already exist, Do you want to delete it [y/n]"
+        $deleteDatabase = Read-Host
+        if($deleteDatabase -eq "y"){            
+            & sqlcmd -E -S "(localdb)\Projects" -Q "DROP DATABASE [$applicationName]"
+        }
+    }
+   
+    Remove-Item $outputLog
+}
+Write-Host "....done"
+Write-Host ""
+
+
+#check rabbitmq
+Write-Host "Checking RabbitMQ" -ForegroundColor Cyan
+$erl = Get-Process "erl*" | measure
+if($erl.Count -eq 0){
+    Start-Process .\sph.packages\rabbitmq_server\sbin\rabbitmq-server.bat
+    Write-Host "Starting rabbit mq server, waiting for 5 seconds"
+    Start-Sleep -Seconds 5
+}
+
+& .\sph.packages\rabbitmq_server\sbin\rabbitmqctl.bat list_vhosts > $outputLog
+if(Test-Path $outputLog){
+    $vhosts =  (Get-Content $outputLog) | Out-String
+
+    if($vhosts.Contains($applicationName) -eq $false){        
+        & .\sph.packages\rabbitmq_server\sbin\rabbitmqctl.bat add_vhost $applicationName
+        Write-Host "setting permission for guest"
+        & Start-Process -FilePath ".\sph.packages\rabbitmq_server\sbin\rabbitmqctl.bat" -ArgumentList @("set_permissions", "-p", $applicationName,"guest", "`".*`" `".*`" `".*`"") -NoNewWindow
+    }
+   
+    Remove-Item $outputLog
+}
+Write-Host "....done"
+Write-Host ""
+
+#verify that elastic search 
+Write-Host "Checking RabbitMQ" -ForegroundColor Cyan
+$es = "http://localhost:9200/"
+$esExist = $false;
+$indexName = $applicationName.ToLowerInvariant();
+Try
+{
+    $cat_indices = curl -Method GET "$es`_cat/indices" | Out-String
+    $esExist = ($cat_indices.Contains("yellow $indexName"))
+}
+Catch{
+    [System.Net.WebException]
+    Start-Process .\StartElasticSearch.bat
+    Write-Host "Starting elasticsearch, waiting for 5 seconds"
+    Start-Sleep -Seconds 6
+    
+    $cat_indices = curl -Method GET "$es`_cat/indices" | Out-String
+    $esExist = ($cat_indices.Contains("yellow $indexName"))
+}
+
+if($esExist){
+    Write-Host " The index already exist, Do you want to recreate it [y/n]"
+    $createIndex = Read-Host
+    if($createIndex -eq "y"){
+        curl -Method Delete "$es$indexName"
+    }
+    #curl -Method PUT "$es$indexName"
+}
+
+Write-Host "....done"
+Write-Host ""
+
+
+
+Write-Host "Creating database" -ForegroundColor Cyan
+
 & sqlcmd -E -S "(localdb)\Projects" -Q "CREATE DATABASE [$applicationName]"
 & sqlcmd -E -S "(localdb)\Projects" -d "$applicationName" -Q "CREATE SCHEMA [$applicationName] AUTHORIZATION [dbo]"
 & sqlcmd -E -S "(localdb)\Projects" -d "$applicationName" -Q "CREATE SCHEMA [Sph] AUTHORIZATION [dbo]"
@@ -11,10 +110,13 @@ ls -Filter *.sql -Path .\source\database\Table `
     $sqlFileName = $_.FullName    
     & sqlcmd -E -S "(localdb)\Projects" -d "$applicationName" -i "$sqlFileName"
 }
+Write-Host "....done"
+Write-Host ""
+
 
 
 #asp.net memberships
-Write-Host "Executing Aspnet membership provider"
+Write-Host "Executing Aspnet membership provider" -ForegroundColor Cyan
 Start-Process -RedirectStandardOutput "v1.log" -WindowStyle Hidden -FilePath "C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regsql.exe" `
 -ArgumentList  @("-E","-S",'"(localdb)\Projects"',"-d " + $applicationName,"-A mr")
 
@@ -25,29 +127,30 @@ Write-Host "Please wait....."
 & mru -r administrators -r developers -r can_edit_entity -r can_edit_workflow -c ".\source\web\web.sph\web.config"
 & mru -u admin -p 123456 -e admin@bespoke.com.my -r administrators -r developers -c ".\source\web\web.sph\web.config"
 
+Write-Host "....done"
+Write-Host ""
 
+
+
+
+Write-Host "Populating elastic search" -ForegroundColor Cyan
 $applicationNameLower = $applicationName.ToLower()
 #elastic search indices
-curl -Method PUT ("http://localhost:9200/" + $applicationNameLower)
-curl -Method PUT ("http://localhost:9200/" + $applicationNameLower + "_sys")
+$response = curl -Method PUT ("http://localhost:9200/" + $applicationNameLower)
+Write-Host "Create $applicationNameLower : " + $response.StatusCode
+$response = curl -Method PUT ("http://localhost:9200/" + $applicationNameLower + "_sys")
+Write-Host "Create $applicationNameLower`_sys : " + $response.StatusCode
 
 Get-ChildItem -Filter *.json -Path .\source\elasticsearch\mapping `
 | %{
     $mappingUri = "http://localhost:9200/" + $applicationNameLower + "_sys/" + $_.Name.ToLowerInvariant().Replace(".json", "") + "/_mapping"
-    Write-Host "Creating elastic search mapping for $mappingUri"
-    Invoke-WebRequest -Method PUT -Uri $mappingUri -InFile $_.FullName -ContentType "application/javascript"
+    #Write-Host "Creating elastic search mapping for $mappingUri"
+    $response = Invoke-WebRequest -Method PUT -Uri $mappingUri -InFile $_.FullName -ContentType "application/javascript"
+    Write-Host  $_.Name.ToLowerInvariant().Replace(".json","") " : " $response.StatusCode
 }
 
+Write-Host "....done"
+Write-Host ""
 
-
-#rabbitmq
-.\sph.packages\rabbitmq_server\sbin\rabbitmqctl.bat add_vhost $applicationName
-.\sph.packages\rabbitmq_server\sbin\rabbitmqctl.bat set_permissions -p $applicationName guest ".*" ".*" ".*"
- 
-
-# run restore Nuget packages
-.\restore-package.ps1
-
-
-Write-Host "Now manually create a new IISExpress site to add web.sph.v1 and run on port 4436" -ForegroundColor Yellow
-Write-Host "Done....."
+Write-Host "Now run Build.Version.1.ps1 and create a new IISExpress site to add web.sph.v1 and run on port 4436" -ForegroundColor Yellow
+Write-Host "Finish"
--- restore-package.ps1
+++ restore-package.ps1
@@ -1,12 +1,16 @@
+if((Test-Path .\packages) -eq $false){
+        mkdir .\packages
+}
+$pwd = pwd
 $packagesFiles = ls -Path .\source -Recurse -Filter packages.config 
-$packagesFiles | foreach {
-    Write-Host "Getting content from "  $_.FullName -ForegroundColor Cyan
+$packagesFiles | foreach{
+    Write-Host "Reading "  $_.FullName.Replace($pwd,"").Replace("packages.config","") -ForegroundColor Cyan
     [xml]$packagesXml = Get-Content $_.FullName
     $packagesXml.packages.package | foreach    {
         $folder = ".\packages\" + $_.id + "." + $_.version
        
         if((Test-Path $folder) -eq $false){
-            Write-Host ($folder.Replace(".\packages\","") + " ....")
+            Write-Host ($folder.Replace(".\packages\","") + "....")
             $arg = "install " + $_.id + " -Version " + $_.version
            # Write-Host $arg
            Start-Process -FilePath ".\.nuget\NuGet.exe" -WorkingDirectory ".\packages" -WindowStyle Hidden -ArgumentList $arg > output.txt
@@ -15,16 +19,10 @@ $packagesFiles | foreach {
     }    
 }
 
-$packagesFiles | foreach {
-    Write-Host "Getting content from "  $_.FullName -ForegroundColor Cyan
-    [xml]$packagesXml = Get-Content $_.FullName
-    $packagesXml.packages.package | foreach    {
-        $folder = ".\packages\" + $_.id + "." + $_.version
-       
-        if((Test-Path $folder) -eq $false){
-            
-            
-        }
-    }    
+$nuget = gps nuget* | measure
+while($nuget.Count -gt 0){
+    Start-Sleep -Milliseconds 500
+    Write-Host "." -NoNewline    
+    $nuget = gps nuget* | measure
 }
-
+Write-Host "Done downloading nuget. packages"
