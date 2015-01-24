bfd2afca01cf762a124ef6da3b9f0544e20a4cae
erymuzuan <erymuzuan@hotmail.com>
2015-01-24 14:24:40 +0800

    doing some optimization for the script

--- Build.Version.1.ps1
+++ Build.Version.1.ps1
@@ -1,5 +1,5 @@
 Param(
-       [switch]$RemovePackages = $false
+       [switch]$KeepPackages = $false
      )
 #switch to v1
 $pwd = pwd
@@ -20,11 +20,12 @@ ls -Path .\source -Filter *.dll -Recurse | Remove-Item
 & git checkout source/unit.test/mapping.transformation.test/rsc.Driver.dll
 & git checkout source/unit.test/mapping.transformation.test/rsc.RilekWeb.dll
 
-Write-Host "  Removing packages folder is recommended for truely clean build,"
-Write-Host "  but it might take long time to restore if your internet connection is slow "
 
-if($RemovePackages)
+if($KeepPackages -eq $false )
 {
+    Write-Host ""
+    Write-Host "  Removing packages folder is recommended for truely clean build,"
+    Write-Host "  but it might take long time to restore if your internet connection is slow "
     # run restore Nuget packages
     if(Test-Path .\source\web\web.sph\obj){
         rmdir .\source\web\web.sph\obj -Recurse -Force
@@ -72,16 +73,15 @@ function Parallel-Build {
     Write-Host "Building $Name" -ForegroundColor Magenta
     Write-Host "---------------------------------------"
 
-    $projects | `
-    %{
-        Write-Host "$_  ..." -ForegroundColor DarkGray
+  #  $projects | `
+  #  %{
+  #      Write-Host "$_  ..." -ForegroundColor DarkGray
         $project = ".\source\$folder\$_\$_.csproj"
-
-        $arg = @("$project" ,"/p:SolutionDir=$pwd","/p:Configuration=Debug", "/nologo", "/noconsolelogger", "/fileLogger", "/flp2:errorsonly;logfile=$_.err")
+        # $arg = @("$project" ,"/p:SolutionDir=$pwd","/p:Configuration=Debug", "/nologo", "/noconsolelogger", "/fileLogger", "/flp2:errorsonly;logfile=$_.err")
         #Start-Process msbuild -WorkingDirectory "." -WindowStyle Hidden -ArgumentList $arg > output.txt
   
 
-    }
+  #  }
 
   #  $msbuilds = gps msbuild* | measure
   #  while($msbuilds.Count -gt 0){
@@ -93,15 +93,18 @@ function Parallel-Build {
     if($CopyToOutput){
         $projects | `
         %{
+            Write-Host "$_" -NoNewline
             $output = ".\source\$folder\" + $_ + "\bin\Debug"
             if(Test-Path $output){       
                 $outputs | %{
+                    Write-Host "." -NoNewline
                     ls -Path $output -Filter *.dll | Copy-Item -Force -Destination $_
                     ls -Path $output -Filter *.pdb | Copy-Item -Force -Destination $_
                 }
             }else{
                 Write-Host "Cannot find output folder for $_" -ForegroundColor Red
             }
+            Write-Host ""
         }
     }
     Write-Host "...done" -NoNewline
@@ -114,7 +117,7 @@ msbuild .\sph.all.sln /m
 $domains = @("domain.sph", "trigger.action.messaging")
 Parallel-Build -Name "domain" -folder "domain" -projects $domains
 
-$dependencies = @("elasticsearch.logger","email.service", "rabbitmq.changepublisher","rabbitmq.persistence","razor.template"`
+$dependencies = @("elasticsearch.logger","email.service", "rabbitmq.changepublisher","razor.template"`
 ,"report.sqldatasource","roslyn.scriptengine","sql.repository","sqlmembership.directoryservices","windows.taskschedulers","word.document.generator")
 
 Parallel-Build -Name "dependencies" -folder "dependencies" -projects $dependencies -CopyToOutput
@@ -162,6 +165,7 @@ Parallel-Build -Name "web" -folder "web" -projects @("core.sph")
 
 Write-Host "Removing dll.config and xml files from output folders"
 $outputs | % {
+    ls -Path $_ -Filter *.appcache | Remove-Item -Recurse
     ls -Path $_ -Filter *.dll.config | Remove-Item -Recurse
     ls -Path $_ -Filter *.xml | Remove-Item -Recurse
     ls -Path $_ -Filter *.manifest | Remove-Item -Recurse
@@ -169,6 +173,7 @@ $outputs | % {
     ls -Path $_ -Filter *.vshost.exe.config | Remove-Item -Recurse
     ls -Path $_ -Filter *.dll.config | Remove-Item -Recurse
     ls -Path $_ -Filter Common.Logging.pdb | Remove-Item -Recurse
+    ls -Path $_ | ?{$_.PSIsContainer} | Remove-Item -Recurse -WarningAction Continue
     
 }
 
@@ -191,7 +196,8 @@ $RabbitmMq_Client = ls .\packages\RabbitMQ.Client.3.4.0\lib\net35\RabbitMQ.Clien
 $Microsoft_Compostition = ls .\packages\Microsoft.Composition.1.0.27\lib\portable-net45+win8+wp8+wpa81\*.dll
 $Microsoft_Code_Analysis = ls .\packages -Filter Microsoft.CodeAnalysis.*.dll -Recurse
 $odp_managed_data_provider = ls .\packages\odp.net.managed.121.1.2\lib\net40\Oracle.ManagedDataAccess.dll
-$user_dll = ls .\bin\output\$applicationName.*.dll
+$user_dll = ls -Path .\bin\output -Filter DevV1.*
+$user_dll
 
 $Microsoft_Owin_Security | Copy-Item -Destination .\source\web\web.sph\bin
 $RabbitmMq_Client | Copy-Item -Destination .\source\web\web.sph\bin
