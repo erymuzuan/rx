06c8bd687d64e1d2bdad7c4a67eb144fa259f750
erymuzuan <erymuzuan@hotmail.com>
2015-01-26 11:47:14 +0800

    ExprssionCode
    Related Work Items: #2372

---
+++ source/dependencies/durandaljs.form.compiler/SyntaxWalkers/ExpressionStatementWalker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bespoke.Sph.FormCompilers.DurandalJs.SyntaxWalkers
+{
+    [Export(typeof(CustomObjectSyntaxWalker))]
+    public class ExpressionStatementWalker : CustomObjectSyntaxWalker
+    {
+        protected override string[] ObjectNames
+        {
+            get { return new string[] { }; }
+        }
+
+        protected override SyntaxKind[] Kinds
+        {
+            get { return new[] { SyntaxKind.ExpressionStatement }; }
+        }
+
+        public override bool Filter(SyntaxNode node, SemanticModel model)
+        {
+            return node is ExpressionStatementSyntax;
+        }
+
+        public override string Walk(SyntaxNode node, SemanticModel model)
+        {
+            var ess = (ExpressionStatementSyntax)node;
+            var code = this.Walkers
+                   .Where(x => x.Filter(ess.Expression, model))
+                   .Select(x => x.Walk(ess.Expression, model))
+                   .FirstOrDefault(x => !string.IsNullOrWhiteSpace(x));
+
+            return code;
+        }
+    }
+}
