07555d059268b0be6e44a25fa18d46911165bb00
Hassan Abu Bakar <hassan@bespoke.com.my>
2015-01-27 11:44:09 +0800

    add less task inside watchList

--- tasks/less.js
+++ tasks/less.js
@@ -10,10 +10,12 @@ var markdown = require('gulp-markdown')
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyCss = require('gulp-minify-css');
-
+var sources =['./source/web/core.sph/Content/theme.matyie/*.less'];
+      
+watchList.push({name:'less',sources: sources, tasks : ['less']});
 
 gulp.task('less', function () {
-  gulp.src('./source/web/core.sph/Content/theme.matyie/*.less')
+  gulp.src(sources)
   	.pipe(sourcemaps.init())
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
