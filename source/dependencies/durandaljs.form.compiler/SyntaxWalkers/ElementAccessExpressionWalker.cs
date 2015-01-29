c087b118f219ca30da50ee19057f515cf51e6965
erymuzuan <erymuzuan@hotmail.com>
2015-01-30 07:31:26 +0800

    Missing files from DurandalJs compiler

---
+++ source/dependencies/durandaljs.form.compiler/SyntaxWalkers/ElementAccessExpressionWalker.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Bespoke.Sph.FormCompilers.DurandalJs
+{
+    [Export(typeof(CustomObjectSyntaxWalker))]
+    class ElementAccessExpressionWalker : CustomObjectSyntaxWalker
+    {
+        protected override string[] ObjectNames
+        {
+            get { return null; }
+        }
+
+        protected override SyntaxKind[] Kinds
+        {
+            get { return new[] { SyntaxKind.ElementAccessExpression }; }
+        }
+
+
+        public override string Walk(SyntaxNode node, SemanticModel model)
+        {
+            var elementAccessor = (ElementAccessExpressionSyntax) node;
+            return string.Format("{0}[{1}]",
+                this.EvaluateExpressionCode(elementAccessor.Expression),
+                this.EvaluateExpressionCode(elementAccessor.ArgumentList.Arguments[0].Expression));
+        }
+    }
+}
