543ceb06c246e8e61799fa583bec1586a48f23b4
Hassan Abu Bakar <hassan@bespoke.com.my>
2015-01-29 14:25:00 +0800

    #2022 StartWeb.bat was not configured properly

--- StartWeb.bat
+++ StartWeb.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\IIS Express\iisexpress.exe" /config:%USERPROFILE%\Documents\IISExpress\config\applicationhost.config /site:web.sph.v1 /trace:verbose
+"C:\Program Files (x86)\IIS Express\iisexpress.exe" /config:applicationhost.config /site:web.sph.v1 /trace:verbose
