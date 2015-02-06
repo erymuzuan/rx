e0f50a06103efc74c0cf402d142e0d8d96885f57
erymuzuan <erymuzuan@hotmail.com>
2015-02-06 12:14:31 +0800

    Workflow build validation test
    Related Work Items: #2207

--- source/unit.test/domain.test/workflows/BuildValidationTest.cs
+++ source/unit.test/domain.test/workflows/BuildValidationTest.cs
@@ -10,30 +10,30 @@ namespace domain.test.workflows
         [Test]
         public void BuildValidation()
         {
-            var wd = new WorkflowDefinition { Name = "3 Is Three" ,SchemaStoreId = Guid.NewGuid().ToString()};
-            var screen = new ScreenActivity { Name = "Pohon", IsInitiator = true, WebId = Guid.NewGuid().ToString(), Performer = new Performer{IsPublic = true}};
-            
-           // screen.FormDesign.FormElementCollection.Add(new TextBox { Label = "Nama", Path = string.Empty });
+            var wd = new WorkflowDefinition { Name = "3 Is Three", SchemaStoreId = Guid.NewGuid().ToString() };
+            var screen = new ScreenActivity { Name = "Pohon", IsInitiator = true, NextActivityWebId = "end", WebId = Strings.GenerateId(), Performer = new Performer { IsPublic = true } };
+            var end = new EndActivity { WebId = "end" };
             wd.ActivityCollection.Add(screen);
+            wd.ActivityCollection.Add(end);
 
 
             var result = wd.ValidateBuild();
             Console.WriteLine(result.ToJsonString(Formatting.Indented));
             Assert.IsFalse(result.Result);
-            Assert.AreEqual(3, result.Errors.Count);
+            Assert.AreEqual(2, result.Errors.Count);
             Assert.AreEqual("Name must be started with letter.You cannot use symbol or number as first character", result.Errors[0].Message);
-            Assert.AreEqual("[ScreenActivity] : Pohon => 'Nama' does not have path", result.Errors[1].Message);
+            Assert.AreEqual("[ScreenActivity] : Pohon => does not have a Form defined", result.Errors[1].Message);
 
-            Assert.Fail("Screeen Activity has no FormDesign");
         }
 
         [Test]
         public void BuildValidationMissingWebId()
         {
-            var wd = new WorkflowDefinition { Name = "Test Workflow", SchemaStoreId = "123"};
-            var screen = new ScreenActivity { Name = "Pohon", IsInitiator = true , Performer = new Performer{IsPublic = true}};
-            //screen.FormDesign.FormElementCollection.Add(new TextBox { Label = "Nama", Path = "Nama" });
+            var wd = new WorkflowDefinition { Name = "Test Workflow", SchemaStoreId = "123" };
+            var end = new EndActivity { WebId = "end" };
+            var screen = new ScreenActivity { Name = "Pohon", NextActivityWebId = end.WebId, IsInitiator = true, Performer = new Performer { IsPublic = true } };
             wd.ActivityCollection.Add(screen);
+            wd.ActivityCollection.Add(end);
 
 
             var result = wd.ValidateBuild();
@@ -42,7 +42,6 @@ namespace domain.test.workflows
             Assert.AreEqual(2, result.Errors.Count);
             StringAssert.Contains("Missing webid", result.Errors[0].ToString());
 
-            Assert.Fail("Screeen Activity has no FormDesign");
         }
 
 
@@ -50,19 +49,20 @@ namespace domain.test.workflows
         [Test]
         public void BuildValidationDuplicateWebId()
         {
-            var wd = new WorkflowDefinition { Name = "Test Workflow", SchemaStoreId = "123"};
-            var screen = new ScreenActivity { Name = "Pohon", IsInitiator = true, WebId = "A", NextActivityWebId = "B", Performer = new Performer{IsPublic = true}};
-            var screen2 = new ScreenActivity { Name = "Pohon 2", IsInitiator = false, WebId = "A", NextActivityWebId = "C" , Performer = new Performer{IsPublic = true}};
-           // screen.FormDesign.FormElementCollection.Add(new TextBox { Label = "Nama", Path = "Nama" });
+            var wd = new WorkflowDefinition { Name = "Test Workflow", SchemaStoreId = "123" };
+            var end = new EndActivity { WebId = "end" };
+            var screen = new ScreenActivity { Name = "Pohon", FormId = "x", IsInitiator = true, WebId = "A", NextActivityWebId = "B", Performer = new Performer { IsPublic = true } };
+            var screen2 = new ScreenActivity { Name = "Pohon 2", FormId = "x", IsInitiator = false, WebId = "A", NextActivityWebId = end.WebId, Performer = new Performer { IsPublic = true } };
+
             wd.ActivityCollection.Add(screen);
             wd.ActivityCollection.Add(screen2);
+            wd.ActivityCollection.Add(end);
 
             var result = wd.ValidateBuild();
-            Console.WriteLine(result.ToJsonString());
+            Console.WriteLine(result.ToJsonString(true));
             Assert.IsFalse(result.Result);
             Assert.AreEqual(2, result.Errors.Count);
             StringAssert.Contains("Duplicate webid", result.Errors[0].ToString());
-            Assert.Fail("Screeen Activity has no FormDesign");
 
         }
 
