defc454a67e6a37045048a7d1a7a16e86da134ef
erymuzuan <erymuzuan@hotmail.com>
2015-02-18 08:24:38 +0800

    Minor changes in references

--- source/web/core.sph/Scripts/__core.js
+++ source/web/core.sph/Scripts/__core.js
@@ -2257,9 +2257,9 @@ ko.bindingHandlers.theme = {
 
     }
 };
+/// <reference path="jquery-2.1.3.js" />
 /// <reference path="jquery-ui-1.11.1.js" />
 /// <reference path="modernizr-2.8.3.js" />
-/// <reference path="jquery-2.1.1.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="breeze.debug.js" />
 /// <reference path="knockout-2.2.1.debug.js" />
