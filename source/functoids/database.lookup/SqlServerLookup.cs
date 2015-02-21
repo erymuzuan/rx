9197baeec3ce926cb892246f081a340415f4dcdc
erymuzuan <erymuzuan@hotmail.com>
2015-02-21 15:37:04 +0800

    new project for  sql server lookup
    Related Work Items: #2478

---
+++ source/functoids/database.lookup/SqlServerLookup.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Composition;
+
+namespace Bespoke.Sph.Domain
+{
+    [Export("FunctoidDesigner", typeof(Functoid))]
+    [DesignerMetadata(Name = "SqlServerLookup", FontAwesomeIcon = "database", Category = FunctoidCategory.DATABASE)]
+    public class SqlServerLookup : Functoid
+    {
+        public override bool Initialize()
+        {
+            this.ArgumentCollection.Add(new FunctoidArg { Name = "connection", Type = typeof(string) });
+            this.ArgumentCollection.Add(new FunctoidArg { Name = "schema", Type = typeof(string) });
+            this.ArgumentCollection.Add(new FunctoidArg { Name = "table", Type = typeof(string) });
+            this.ArgumentCollection.Add(new FunctoidArg { Name = "column", Type = typeof(string) });
+            this.ArgumentCollection.Add(new FunctoidArg { Name = "predicate", Type = typeof(string) });
+            return base.Initialize();
+
+        }
+
+        public override string GetEditorView()
+        {
+            return "";
+        }
+
+        
+    }
+}
