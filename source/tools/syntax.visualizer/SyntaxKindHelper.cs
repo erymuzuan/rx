5be6a697145a970cacb4adcddf79e465d6bac0b3
erymuzuan <erymuzuan@hotmail.com>
2015-03-04 16:34:21 +0800

    SyntaxKind change from int to enum

--- source/tools/syntax.visualizer/SyntaxKindHelper.cs
+++ source/tools/syntax.visualizer/SyntaxKindHelper.cs
@@ -14,28 +14,24 @@ namespace Roslyn.SyntaxVisualizer.Control
         public static string GetKind(this SyntaxNodeOrToken nodeOrToken)
         {
             var kind = nodeOrToken.IsNode ? nodeOrToken.AsNode().GetKind() : nodeOrToken.AsToken().GetKind();
-
             return kind;
         }
 
         public static string GetKind(this SyntaxNode node)
         {
-            var kind = node.Language == LanguageNames.CSharp ? node.RawKind.ToString() : node.Kind().ToString();
-
+            var kind = node.Language == LanguageNames.CSharp ? ((Microsoft.CodeAnalysis.CSharp.SyntaxKind)node.RawKind).ToString() : node.Kind().ToString();
             return kind;
         }
 
         public static string GetKind(this SyntaxToken token)
         {
             var kind = token.Language == LanguageNames.CSharp ? CSharpExtensions.Kind(token).ToString() : token.Kind().ToString();
-
             return kind;
         }
 
         public static string GetKind(this SyntaxTrivia trivia)
         {
             var kind = trivia.Language == LanguageNames.CSharp ? CSharpExtensions.Kind(trivia).ToString() : trivia.Kind().ToString();
-
             return kind;
         }
     }
