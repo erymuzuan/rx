bc2f19c998b5df4586b671a559c3db57389cb01e
Hassan Abu Bakar <hassan@bespoke.com.my>
2015-03-06 17:09:25 +0800

    2446 - Creates new solution dialog - Add dialog
    Related Work Items: #2446

---
+++ source/web/core.sph/SphApp/viewmodels/add.solution.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../../Scripts/jquery-2.1.1.intellisense.js" />
+/// <reference path="../../Scripts/knockout-3.2.0.debug.js" />
+/// <reference path="../../Scripts/knockout.mapping-latest.debug.js" />
+/// <reference path="../../Scripts/require.js" />
+/// <reference path="../../Scripts/underscore.js" />
+/// <reference path="../../Scripts/moment.js" />
+/// <reference path="../services/datacontext.js" />
+/// <reference path="../schema/sph.domain.g.js" />
+
+
+define(['plugins/dialog', objectbuilders.datacontext],
+    function (dialog, context) {
+
+        var okClick = function (data, ev) {
+                if (bespoke.utils.form.checkValidity(ev.target)) {
+                                    dialog.close(this, "OK");
+                }
+            },
+            cancelClick = function () {
+                dialog.close(this, "Cancel");
+            };
+
+        var vm = {
+            okClick: okClick,
+            cancelClick: cancelClick
+        };
+
+
+        return vm;
+
+    });
