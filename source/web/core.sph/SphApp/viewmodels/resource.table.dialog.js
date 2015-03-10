238405f46aea903623b3f258c35eb87ee61e4491
erymuzuan <erymuzuan@hotmail.com>
2015-03-10 14:48:25 +0800

    Fixed the bugs when subscriber to change event

--- source/web/core.sph/SphApp/viewmodels/resource.table.dialog.js
+++ source/web/core.sph/SphApp/viewmodels/resource.table.dialog.js
@@ -13,7 +13,7 @@ define(["plugins/dialog", objectbuilders.datacontext],
 
         var items = ko.observableArray(),
             resource = ko.observable(),
-            lang = ko.observable(),
+            selectedLanguage = ko.observable(),
             keys = ko.observableArray(),
             languageOptions = ko.observableArray(),
             activate = function () {
@@ -32,23 +32,7 @@ define(["plugins/dialog", objectbuilders.datacontext],
                 });
             },
             attached = function () {
-                lang.subscribe(function (l) {
-                    $.getJSON("/i18n/" + l + "/" + resource())
-                        .done(function (n) {
-                            for (var k in n) {
-                                if (n.hasOwnProperty(k)) {
-                                    var k1 = k,
-                                        key = _(items()).find(function (v) {
-                                            return ko.unwrap(v.Key) === k1;
-                                        });
-                                    if (key) {
-                                        key.Value(n[k1]);
-                                    }
-                                }
-                            }
-
-                        });
-                });
+                selectedLanguage("");
                 //scrollable tbody
                 var $table = $("#types-table"),
                     $bodyCells = $table.find("tbody tr:first").children(),
@@ -78,18 +62,41 @@ define(["plugins/dialog", objectbuilders.datacontext],
                     o[ko.unwrap(v.Key)] = v.Value;
                 });
 
-                return context.post(ko.toJSON(o), "/i18n/" + lang() + "/" + resource());
+                return context.post(ko.toJSON(o), "/i18n/" + selectedLanguage() + "/" + resource());
 
             },
             cancelClick = function () {
                 dialog.close(this, "Cancel");
             };
 
+
+        selectedLanguage.subscribe(function (lang) {
+            if (!lang) {
+                return;
+            }
+            $.getJSON("/i18n/" + lang + "/" + resource())
+                .done(function (n) {
+                    for (var k in n) {
+                        if (n.hasOwnProperty(k)) {
+                            var k1 = k,
+                                key = _(items()).find(function (v) {
+                                    return ko.unwrap(v.Key) === k1;
+                                });
+                            if (key) {
+                                key.Value(n[k1]);
+                            } else {
+                                items.push({ "Key": ko.observable(k1), "Value": ko.observable(n[k1]) });
+                            }
+                        }
+                    }
+
+                });
+        });
         var vm = {
             activate: activate,
             attached: attached,
             resource: resource,
-            lang: lang,
+            lang: selectedLanguage,
             languageOptions: languageOptions,
             items: items,
             keys: keys,
