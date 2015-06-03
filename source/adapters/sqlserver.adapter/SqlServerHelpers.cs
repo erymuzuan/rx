5da1d8911e5aca3e30a0cb2c065798ab9fd5e919
erymuzuan <erymuzuan@hotmail.com>
2015-06-03 08:19:50 +0800

    Added varbinary and image datatype as well as few more, and the table list got a filter to make it easier to browse through huge table list

--- source/adapters/sqlserver.adapter/SqlServerHelpers.cs
+++ source/adapters/sqlserver.adapter/SqlServerHelpers.cs
@@ -21,29 +21,34 @@ namespace Bespoke.Sph.Integrations.Adapters
         {
             switch (sqlType)
             {
-                case "xml":
+                case "xml": return typeof(System.Xml.XmlDocument);
                 case "char":
                 case "nchar":
                 case "ntext":
                 case "text":
                 case "nvarchar":
                 case "varchar": return typeof(string);
+                case "varbinary": 
+                case "timestamp": 
+                case "rowversion": 
+                case "binary": 
+                case "image": return typeof(byte[]);
                 case "uniqueidentifier": return typeof(Guid);
                 case "bigint": return typeof(long);
                 case "smallint": return typeof(short);
                 case "tinyint": return typeof(byte);
                 case "int": return typeof(int);
-                case "datetimeoffset":
-                case "time":
+                case "date":
                 case "datetime":
                 case "datetime2":
                 case "smalldatetime": return typeof(DateTime);
+                case "datetimeoffset": return typeof(DateTimeOffset);
+                case "time": return typeof(TimeSpan);
                 case "bit": return typeof(bool);
                 case "numeric":
                 case "smallmoney":
-                case "decimal": return typeof(decimal);
+                case "decimal": 
                 case "money": return typeof(decimal);
-                case "rowversion": return typeof(byte[]);
                 case "real":
                 case "float": return typeof(double);
                 case "sql_variant": return typeof(object);
