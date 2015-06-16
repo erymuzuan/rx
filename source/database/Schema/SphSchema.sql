e0d93bc7ca74b71b2ce3d4e690eb5fc86ea8c687
erymuzuan <erymuzuan@hotmail.com>
2015-06-16 08:52:19 +0800

    DevV1 tables

--- source/database/Schema/SphSchema.sql
+++ source/database/Schema/SphSchema.sql
@@ -1,5 +1,6 @@
- USE [Sph]
+ USE [DevV1]
  GO
-/****** Object:  Schema [Utility]    Script Date: 08/09/2007 15:10:19 ******/
-CREATE SCHEMA [Dev] AUTHORIZATION [dbo]
---CREATE SCHEMA [dev] AUTHORIZATION [dbo]
+
+CREATE SCHEMA [Sph] AUTHORIZATION [dbo]
+GO
+CREATE SCHEMA [DevV1] AUTHORIZATION [dbo]
--- source/database/database.sql
+++ source/database/database.sql
@@ -8,37 +8,37 @@ GO
 IF  EXISTS (
 	SELECT name 
 		FROM sys.databases 
-		WHERE name = N'SphV1'
+		WHERE name = N'DevV1'
 )
-DROP DATABASE Sph
+DROP DATABASE DevV1
 GO
 
-CREATE DATABASE [Sph]
+CREATE DATABASE [DevV1]
 ON
 PRIMARY  
     (NAME = Arch1,
-    FILENAME = 'G:\data\sph.mdf',
+    FILENAME = 'c:\data\devv1.mdf',
     SIZE = 100MB,
     MAXSIZE = 200,
     FILEGROWTH = 20),
     ( NAME = Arch2,
-    FILENAME = 'G:\data\sph2.ndf',
+    FILENAME = 'c:\data\devv1_2.ndf',
     SIZE = 100MB,
     MAXSIZE = 200,
     FILEGROWTH = 20),
     ( NAME = Arch3,
-    FILENAME = 'G:\data\sph3.ndf',
+    FILENAME = 'c:\data\devv1_3.ndf',
     SIZE = 100MB,
     MAXSIZE = 200,
     FILEGROWTH = 20)
 LOG ON 
    (NAME = Archlog1,
-    FILENAME = 'G:\data\sphlog1.ldf',
+    FILENAME = 'c:\data\devv1_log1.ldf',
     SIZE = 100MB,
     MAXSIZE = 200,
     FILEGROWTH = 20),
    (NAME = Archlog2,
-    FILENAME = 'G:\data\sphlog2.ldf',
+    FILENAME = 'c:\data\devv1_log2.ldf',
     SIZE = 100MB,
     MAXSIZE = 200,
     FILEGROWTH = 20) ;
