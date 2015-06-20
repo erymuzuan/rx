638e3b80125e7ddbd5bc16f4a9c0d5e77b4dfdb7
erymuzuan <erymuzuan@hotmail.com>
2015-06-20 11:33:57 +0800

    Controller for solution diagnostics

---
+++ source/web/core.sph/Controllers/SolutionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+namespace Bespoke.Sph.Web.Controllers
+{
+    [RoutePrefix("solution")]
+    [Authorize(Roles = "developers")]
+    public class SolutionController : Controller
+    {
+        [Route("diagnostics")]
+        public ActionResult StartDiagnostics()
+        {
+
+            // validate all forms
+            // - path
+            // - route
+
+            // validate all views
+            // - column path
+            // - link , is correct route
+            // - icon
+            // - some suggestion to spelling
+            // - route
+
+            // validate all entities
+            // - record name
+            // - security
+            // - icon/image
+            // - Operation, name, security, Setter Path
+
+
+
+            // triggers
+            // -> action
+            // - filter
+            // - compilation
+            // - deployment
+            // - active/inactive
+
+            // workflow
+
+            // 
+
+            return Json(true);
+        }
+    }
+}
