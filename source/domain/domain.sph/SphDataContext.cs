61a54b8b04ee00ac518aa9fd9a598b9f911ae497
erymuzuan <erymuzuan@hotmail.com>
2015-07-08 21:13:44 +0800

    Mishaps

--- source/domain/domain.sph/SphDataContext.cs
+++ source/domain/domain.sph/SphDataContext.cs
@@ -103,14 +103,9 @@ namespace Bespoke.Sph.Domain
 
         public async Task<T> LoadOneAsync<T>(Expression<Func<T, bool>> predicate) where T : Entity
         {
-            var jsonText = typeof (T) == typeof (EntityForm)
-                           || typeof (T) == typeof (Api.Adapter)
-                           || typeof (T) == typeof (TransformDefinition)
-                           || typeof (T) == typeof (Trigger)
-                           || typeof (T) == typeof (WorkflowDefinition);
             var storeAsSource = StoreAsSourceAttribute.GetAttribute<T>();
             if (null != storeAsSource)
-                return this.LoadOneFromSources(predicate, jsonText);
+                return this.LoadOneFromSources(predicate);
 
 
             var provider = ObjectBuilder.GetObject<QueryProvider>();
