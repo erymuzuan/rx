e0102db072f20e623016861429d31e0280515109
erymuzuan <erymuzuan@hotmail.com>
2015-07-12 10:01:34 +0800

    Invoke-RxCompiler cmdlet, at least now it works with a single source

---
+++ source/tools/sqlcmd/DynParamQuotedString.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Bespoke.Sph.Powershells
+{
+    internal class DynParamQuotedString
+    {
+        /*
+            This works around the PowerShell bug where ValidateSet values aren't quoted when necessary, and
+            adding the quotes breaks it. Example:
+
+            ValidateSet valid values = 'Test string'  (The quotes are part of the string)
+
+            PowerShell parameter binding would interperet that as [Test string] (no single quotes), which wouldn't match
+            the valid value (which has the quotes). If you make the parameter a DynParamQuotedString, though,
+            the parameter binder will coerce [Test string] into an instance of DynParamQuotedString, and the binder will
+            call ToString() on the object, which will add the quotes back in.
+        */
+
+        internal static string DefaultQuoteCharacter = "'";
+
+        public DynParamQuotedString(string quotedString) : this(quotedString, DefaultQuoteCharacter) { }
+        public DynParamQuotedString(string quotedString, string quoteCharacter)
+        {
+            OriginalString = quotedString;
+            m_quoteCharacter = quoteCharacter;
+        }
+
+        public string OriginalString { get; set; }
+        readonly string m_quoteCharacter;
+
+        public override string ToString()
+        {
+            // I'm sure this is missing some other characters that need to be escaped. Feel free to add more:
+            if (Regex.IsMatch(OriginalString, @"\s|\(|\)|""|'"))
+            {
+                return string.Format("{1}{0}{1}", OriginalString.Replace(m_quoteCharacter, string.Format("{0}{0}", m_quoteCharacter)), m_quoteCharacter);
+            }
+            return OriginalString;
+        }
+
+        public static string[] GetQuotedStrings(IEnumerable<string> values)
+        {
+            var returnList = new List<string>();
+            foreach (string currentValue in values)
+            {
+                returnList.Add((new DynParamQuotedString(currentValue)).ToString());
+            }
+            return returnList.ToArray();
+        }
+    }
+}
---
+++ source/tools/sqlcmd/InvokeRxCompiler.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace Bespoke.Sph.Powershells
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "RxC")]
+    public class InvokeRxCompiler : PSCmdlet, IDynamicParameters
+    {
+        readonly IDictionary<string, string[]> m_sources = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
+
+        public InvokeRxCompiler()
+        {
+            var entities = GetSources("EntityDefinition");
+            m_sources.Add("EntityDefinition", entities);
+
+            var triggers = GetSources("Trigger");
+            m_sources.Add("Trigger", triggers);
+
+        }
+
+        private string[] GetSources(string type)
+        {
+            var source = $@".\sources\{type}\";
+            var files = new[] { $"Cannot find any {type} in {Path.GetFullPath(source)}"  };
+            if (Directory.Exists(source))
+            {
+
+                files = (from f in Directory.GetFiles(source, "*.json")
+                    select Path.GetFileNameWithoutExtension(f))
+                    .ToArray();
+            }
+
+            return files;
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteObject($"Type = {m_type}");
+            var source = ((DynParamQuotedString)MyInvocation.BoundParameters["Source"]).OriginalString;
+            WriteObject($"Source = {source}");
+
+            var info = new ProcessStartInfo
+            {
+               FileName = @".\tools\sph.builder.exe",
+               Arguments = $@".\sources\{m_type}\{source}.json /s",
+               CreateNoWindow = true,
+               UseShellExecute = true
+              
+            };
+
+            var builder = Process.Start(info);
+            builder?.WaitForExit();
+        }
+
+        // Making this static means it should keep track of the last Type used
+        static string m_type;
+        public object GetDynamicParameters()
+        {
+            // Get 'Type' if found, otherwise get first unnamed value
+            string author = GetUnboundValue("Type", 0) as string;
+            if (!string.IsNullOrEmpty(author))
+            {
+                m_type = author.Trim('\'').Replace(
+                    string.Format("{0}{0}", DynParamQuotedString.DefaultQuoteCharacter),
+                    DynParamQuotedString.DefaultQuoteCharacter
+                    );
+            }
+
+            var parameters = new RuntimeDefinedParameterDictionary();
+
+            bool isTypeParameterMandatory = true;
+            if (!string.IsNullOrEmpty(m_type) && m_sources.ContainsKey(m_type))
+            {
+                isTypeParameterMandatory = false;
+                var bookParameter = new RuntimeDefinedParameter(
+                    "Source",
+                    typeof(DynParamQuotedString),
+                    new Collection<Attribute>
+                    {
+                        new ParameterAttribute {
+                            ParameterSetName = "SourceStuff",
+                            Position = 1,
+                            Mandatory = true
+                        },
+                        new ValidateSetAttribute(DynParamQuotedString.GetQuotedStrings(m_sources[m_type])),
+                        new ValidateNotNullOrEmptyAttribute()
+                    }
+                    );
+
+                parameters.Add(bookParameter.Name, bookParameter);
+            }
+
+            // Create author parameter. Parameter isn't mandatory if _author
+            // has a valid author in it
+            var authorParameter = new RuntimeDefinedParameter(
+                "Type",
+                typeof(DynParamQuotedString),
+                new Collection<Attribute>
+                {
+                    new ParameterAttribute {
+                        ParameterSetName = "SourceStuff",
+                        Position = 0,
+                        Mandatory = isTypeParameterMandatory
+                    },
+                    new ValidateSetAttribute(DynParamQuotedString.GetQuotedStrings(m_sources.Keys.ToArray())),
+                    new ValidateNotNullOrEmptyAttribute()
+                }
+                );
+            parameters.Add(authorParameter.Name, authorParameter);
+
+            return parameters;
+        }
+
+        /*
+            TryGetProperty() and GetUnboundValue() are from here: https://gist.github.com/fearthecowboy/1936f841d3a81710ae87
+            Source created a dictionary for all unbound values; I had issues getting ValidateSet on Type parameter to work
+            if I used that directly for some reason, but changing it into a function to get a specific parameter seems to work
+        */
+
+        object TryGetProperty(object instance, string fieldName)
+        {
+            var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public;
+
+            // any access of a null object returns null. 
+            if (instance == null || string.IsNullOrEmpty(fieldName))
+            {
+                return null;
+            }
+
+            var propertyInfo = instance.GetType().GetProperty(fieldName, bindingFlags);
+
+            if (propertyInfo != null)
+            {
+                try
+                {
+                    return propertyInfo.GetValue(instance, null);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            // maybe it's a field
+            var fieldInfo = instance.GetType().GetField(fieldName, bindingFlags);
+
+            if (fieldInfo != null)
+            {
+                try
+                {
+                    return fieldInfo.GetValue(instance);
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+
+            // no match, return null.
+            return null;
+        }
+
+        object GetUnboundValue(string paramName)
+        {
+            return GetUnboundValue(paramName, -1);
+        }
+
+        object GetUnboundValue(string paramName, int unnamedPosition)
+        {
+
+            // If paramName isn't found, value at unnamedPosition will be returned instead
+            var context = TryGetProperty(this, "Context");
+            var processor = TryGetProperty(context, "CurrentCommandProcessor");
+            var parameterBinder = TryGetProperty(processor, "CmdletParameterBinderController");
+            var args = TryGetProperty(parameterBinder, "UnboundArguments") as IEnumerable;
+
+            if (args != null)
+            {
+                var currentParameterName = string.Empty;
+                object unnamedValue = null;
+                int i = 0;
+                foreach (var arg in args)
+                {
+                    var isParameterName = TryGetProperty(arg, "ParameterNameSpecified");
+                    if (isParameterName != null && true.Equals(isParameterName))
+                    {
+                        string parameterName = TryGetProperty(arg, "ParameterName") as string;
+                        currentParameterName = parameterName;
+
+                        continue;
+                    }
+
+                    // Treat as a value:
+                    var parameterValue = TryGetProperty(arg, "ArgumentValue");
+
+                    if (currentParameterName != string.Empty)
+                    {
+                        // Found currentParameterName's value. If it matches paramName, return
+                        // it
+                        if (currentParameterName != null && currentParameterName.Equals(paramName, StringComparison.OrdinalIgnoreCase))
+                        {
+                            return parameterValue;
+                        }
+                    }
+                    else if (i++ == unnamedPosition)
+                    {
+                        unnamedValue = parameterValue;  // Save this for later in case paramName isn't found
+                    }
+
+                    // Found a value, so currentParameterName needs to be cleared
+                    currentParameterName = string.Empty;
+                }
+
+                return unnamedValue;
+            }
+
+            return null;
+        }
+    }
+}
