8293ae65630a6c7a21590f3d8f8307a5b4c571e2
erymuzuan <erymuzuan@hotmail.com>
2015-07-16 18:46:53 +0800

    Default button for alert and message no longer btn-primary

--- source/web/core.sph/SphApp/services/app.js
+++ source/web/core.sph/SphApp/services/app.js
@@ -1,7 +1,7 @@
 ﻿define(function () {
     var guid = function () {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c === "x" ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
     },
@@ -14,9 +14,9 @@
             if (dialog.__view__) {
                 dialog.__view__.modal();
             } else {
-                $.get(moduleId.replace('viewmodels', '/app/views') + '.html')
+                $.get(moduleId.replace("viewmodels", "/app/views") + ".html")
                     .done(function (html) {
-                        var panel = $(html).appendTo('body');
+                        var panel = $(html).appendTo("body");
                         panel.modal();
                         dialog.__view__ = panel;
                         dialog.modal = {
@@ -48,31 +48,31 @@
             var id = guid(),
                 tcs = new $.Deferred(),
                 dialog = $(
-                    '<div class="modal fade" id="' + id + '">' +
-                    '   <div class="modal-dialog">' +
-                    '       <div class="modal-content">' +
-                    '           <div class="modal-header">' +
-                    '               <button type="button" data-bind="click: cancelClick" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                    '               <h4 class="modal-title" data-bind="html: title"></h3>' +
-                    '           </div>' +
-                    '           <div class="modal-body">' +
-                    '               <p class="message" data-bind="html: message"></p>' +
-                    '           </div>' +
-                    '           <div class="modal-footer" data-bind="foreach: options">' +
-                    '               <button class="btn btn-default" data-bind="click: function () { $parent.selectOption($data); }, html: $data, css: { autofocus: $index() == 0 }"></button>' +
-                    '           </div>' +
-                    '       </div>' +
-                    '   </div>' +
-                    '</div>');
+                    "<div class=\"modal fade\" id=\"" + id + "\">" +
+                    "   <div class=\"modal-dialog\">" +
+                    "       <div class=\"modal-content\">" +
+                    "           <div class=\"modal-header\">" +
+                    "               <button type=\"button\" data-bind=\"click: cancelClick\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>" +
+                    "               <h4 class=\"modal-title\" data-bind=\"html: title\"></h3>" +
+                    "           </div>" +
+                    "           <div class=\"modal-body\">" +
+                    "               <p class=\"message\" data-bind=\"html: message\"></p>" +
+                    "           </div>" +
+                    "           <div class=\"modal-footer\" data-bind=\"foreach: options\">" +
+                    "               <button class=\"btn btn-default\" data-bind=\"click: function () { $parent.selectOption($data); }, html: $data, css: { autofocus: $index() == 0 }\"></button>" +
+                    "           </div>" +
+                    "       </div>" +
+                    "   </div>" +
+                    "</div>");
 
-            dialog.appendTo('body');
+            dialog.appendTo("body");
             var vm = {
                 message: message,
                 title: title,
                 options: options,
                 selectOption: function (result) {
                     tcs.resolve(result);
-                    dialog.modal('hide');
+                    dialog.modal("hide");
                     setTimeout(function () { dialog.remove(); }, 500);
                 },
                 cancelClick: function () {
