e3ef04cdee2056331c5fc0da6da2c02d834a5ce2
erymuzuan <erymuzuan@hotmail.com>
2015-07-21 10:28:59 +0800

    bugs fixed when a user connects with more than 1 browsers

--- source/web/core.sph/Hubs/MessageConnection.cs
+++ source/web/core.sph/Hubs/MessageConnection.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
@@ -6,29 +8,60 @@ using Microsoft.AspNet.SignalR;
 
 namespace Bespoke.Sph.Web.Hubs
 {
-    public class MessageConnection : PersistentConnection
+    public class MessageConnection : PersistentConnection, IDisposable
     {
-        private IEntityChangedListener<Message> m_listener;
-        private readonly ConcurrentDictionary<string, string> m_connections = new ConcurrentDictionary<string, string>();
-        protected override Task OnConnected(IRequest request, string connectionId)
+        private readonly IEntityChangedListener<Message> m_listener;
+        private readonly ConcurrentDictionary<string, IList<string>> m_connections = new ConcurrentDictionary<string, IList<string>>();
+
+        public MessageConnection()
         {
             m_listener?.Stop();
             m_listener = ObjectBuilder.GetObject<IEntityChangedListener<Message>>();
             m_listener.Changed += ListenerChanged;
             m_listener.Run();
 
+        }
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+
             var user = request?.User?.Identity?.Name;
-            if (!string.IsNullOrWhiteSpace(user))
-                m_connections.TryAdd(request.User.Identity.Name, connectionId);
+            if (string.IsNullOrWhiteSpace(user))
+                return Task.FromResult(0);
+
+            IList<string> connections;
+            if (m_connections.TryGetValue(user, out connections))
+            {
+                var list = new List<string>(connections.ToArray()) { connectionId };
+                m_connections.TryUpdate(user, list, connections);
+            }
+            else
+            {
+                m_connections.TryAdd(user, new List<string> { connectionId });
+            }
 
             return Connection.Send(connectionId, $"You are now connected to messaging connection");
         }
 
+        protected override Task OnDisconnected(IRequest request, string connectionId, bool stopCalled)
+        {
+            var user = request?.User?.Identity?.Name;
+            if (string.IsNullOrWhiteSpace(user))
+                return base.OnDisconnected(request, connectionId, stopCalled);
+            IList<string> connections;
+            if (m_connections.TryGetValue(user, out connections))
+            {
+                var list = new List<string>(connections);
+                list.Remove(connectionId);
+                m_connections.TryUpdate(user, list, connections);
+            }
+            return base.OnDisconnected(request, connectionId, stopCalled);
+        }
+
         async void ListenerChanged(object sender, EntityChangedEventArgs<Message> e)
         {
-            var conn = "";
-            if (!m_connections.TryGetValue(e.Item.UserName, out conn)) return;
-
+            IList<string> connections;
+            if (!m_connections.TryGetValue(e.Item.UserName, out connections)) return;
+            if (connections.Count == 0) return;
 
             var context = new SphDataContext();
             var query = context.CreateQueryable<Message>()
@@ -37,10 +70,13 @@ namespace Bespoke.Sph.Web.Hubs
             var lo = await context.LoadAsync(query, 1, 5, true);
 
             var data = new { message = e.Item, messages = lo.ItemCollection, unread = lo.TotalRows };
-            await Connection.Send(conn, data);
+            await Connection.Send(connections, data);
         }
 
-    
 
+        public void Dispose()
+        {
+            m_listener?.Stop();
+        }
     }
 }
