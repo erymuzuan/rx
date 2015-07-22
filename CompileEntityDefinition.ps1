7f2226f4ff28e4fd833b1af6605187c5ae4e8d24
erymuzuan <erymuzuan@hotmail.com>
2015-07-23 07:44:34 +0800

    Build scripts

---
+++ CompileEntityDefinition.ps1
@@ -0,0 +1,4 @@
+﻿Param(
+       [string]$Entity = 10
+     )
+csc .\bin\sources\_generated\$Entity\*.cs /r:.\source\web\web.sph\bin\System.Web.Mvc.dll /r:.\source\web\web.sph\bin\domain.sph.dll /r:.\source\web\web.sph\bin\core.sph.dll /t:library /r:System.Net.Http.dll /out:.\bin\output\DevV1.$Entity.dll /pdb:.\bin\output\DevV1.$Entity.pdb /debug
