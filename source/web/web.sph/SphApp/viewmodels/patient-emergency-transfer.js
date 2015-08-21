1825bdb596ce2e59a9b77dd74291c971d6c58834
erymuzuan <erymuzuan@hotmail.com>
2015-08-21 10:09:45 +0800

    test files

--- source/web/web.sph/SphApp/viewmodels/patient-emergency-transfer.js
+++ source/web/web.sph/SphApp/viewmodels/patient-emergency-transfer.js
@@ -1,22 +1,28 @@
 
-    define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router, objectbuilders.system, objectbuilders.validation, objectbuilders.eximp, objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config, objectbuilders.app],
-        function (context, logger, router, system, validation, eximp, dialog, watcher,config,app) {
+    define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
+        objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
+        objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
+        objectbuilders.app ],
+        function (context, logger, router, system, validation, eximp, dialog, watcher,config,app
+            ) {
 
             var entity = ko.observable(new bespoke.DevV1_patient.domain.Patient({WebId:system.guid()})),
                 errors = ko.observableArray(),
                 form = ko.observable(new bespoke.sph.domain.EntityForm()),
                 watching = ko.observable(false),
                 id = ko.observable(),
+                i18n = null,
                 activate = function (entityId) {
-                    id(parseInt(entityId));
+                    id(entityId);
 
-                    var query = String.format("PatientId eq {0}", entityId),
+                    var query = String.format("Id eq '{0}'", entityId),
                         tcs = new $.Deferred(),
                         itemTask = context.loadOneAsync("Patient", query),
                         formTask = context.loadOneAsync("EntityForm", "Route eq 'patient-emergency-transfer'"),
-                        watcherTask = watcher.getIsWatchingAsync("Patient", entityId);
+                        watcherTask = watcher.getIsWatchingAsync("Patient", entityId),
+                        i18nTask = $.getJSON("i18n/" + config.lang + "/patient-emergency-transfer");
 
-                    $.when(itemTask, formTask, watcherTask).done(function(b,f,w) {
+                    $.when(itemTask, formTask, watcherTask, i18nTask).done(function(b,f,w,n) {
                         if (b) {
                             var item = context.toObservable(b);
                             entity(item);
@@ -26,28 +32,31 @@
                         }
                         form(f);
                         watching(w);
-
-                        tcs.resolve(true);
+                        i18n = n[0];
+                            tcs.resolve(true);
+                        
                     });
 
                     return tcs.promise();
                 },
                 register = function(){
 
-                     var tcs = new $.Deferred(),
-                         data = ko.mapping.toJSON(entity);
+                     if (!validation.valid()) {
+                         return Task.fromResult(false);
+                     }
 
-                     context.post(data, "/Patient/Register" )
+                     var data = ko.mapping.toJSON(entity);
+
+                    return  context.post(data, "/Patient/Register" )
                          .then(function (result) {
                              if (result.success) {
                                  logger.info(result.message);
-                                 entity().PatientId(result.id);
+                                 entity().Id(result.id);
                                  errors.removeAll();
 
                                   
-                                    app.showMessage("Ok done", "SPH Platform showcase", ["OK"])
-	                                    .done(function (dialogResult) {
-                                            console.log();
+                                    app.showMessage("Ok done", "SPH Platform Showcase", ["OK"])
+	                                    .done(function () {
                                             window.location='#patient'
 	                                    });
                                  
@@ -58,29 +67,24 @@
                                  });
                                  logger.error("There are errors in your entity, !!!");
                              }
-                             tcs.resolve(result);
                          });
-                     return tcs.promise();
                  },
                 discharge = function(){
 
-                     var tcs = new $.Deferred(),
-                         data = ko.mapping.toJSON(entity);
+                     if (!validation.valid()) {
+                         return Task.fromResult(false);
+                     }
 
-                     context.post(data, "/Patient/Discharge" )
+                     var data = ko.mapping.toJSON(entity);
+
+                    return  context.post(data, "/Patient/Discharge" )
                          .then(function (result) {
                              if (result.success) {
                                  logger.info(result.message);
-                                 entity().PatientId(result.id);
+                                 entity().Id(result.id);
                                  errors.removeAll();
 
-                                  
-                                    app.showMessage("Done", "SPH Platform showcase", ["OK"])
-	                                    .done(function (dialogResult) {
-                                            console.log();
-                                            window.location='#patient'
-	                                    });
-                                 
+                                 window.location='#patient'
                              } else {
                                  errors.removeAll();
                                  _(result.rules).each(function(v){
@@ -88,29 +92,24 @@
                                  });
                                  logger.error("There are errors in your entity, !!!");
                              }
-                             tcs.resolve(result);
                          });
-                     return tcs.promise();
                  },
                 transfer = function(){
 
-                     var tcs = new $.Deferred(),
-                         data = ko.mapping.toJSON(entity);
+                     if (!validation.valid()) {
+                         return Task.fromResult(false);
+                     }
 
-                     context.post(data, "/Patient/Transfer" )
+                     var data = ko.mapping.toJSON(entity);
+
+                    return  context.post(data, "/Patient/Transfer" )
                          .then(function (result) {
                              if (result.success) {
                                  logger.info(result.message);
-                                 entity().PatientId(result.id);
+                                 entity().Id(result.id);
                                  errors.removeAll();
 
-                                  
-                                    app.showMessage("Done", "SPH Platform showcase", ["OK"])
-	                                    .done(function (dialogResult) {
-                                            console.log();
-                                            window.location='#patient'
-	                                    });
-                                 
+                                 window.location='#patient'
                              } else {
                                  errors.removeAll();
                                  _(result.rules).each(function(v){
@@ -118,29 +117,24 @@
                                  });
                                  logger.error("There are errors in your entity, !!!");
                              }
-                             tcs.resolve(result);
                          });
-                     return tcs.promise();
                  },
                 admit = function(){
 
-                     var tcs = new $.Deferred(),
-                         data = ko.mapping.toJSON(entity);
+                     if (!validation.valid()) {
+                         return Task.fromResult(false);
+                     }
 
-                     context.post(data, "/Patient/Admit" )
+                     var data = ko.mapping.toJSON(entity);
+
+                    return  context.post(data, "/Patient/Admit" )
                          .then(function (result) {
                              if (result.success) {
                                  logger.info(result.message);
-                                 entity().PatientId(result.id);
+                                 entity().Id(result.id);
                                  errors.removeAll();
 
-                                  
-                                    app.showMessage("OK done", "SPH Platform showcase", ["OK"])
-	                                    .done(function (dialogResult) {
-                                            console.log();
-                                            window.location='#patient'
-	                                    });
-                                 
+                                 window.location='#patient'
                              } else {
                                  errors.removeAll();
                                  _(result.rules).each(function(v){
@@ -148,98 +142,104 @@
                                  });
                                  logger.error("There are errors in your entity, !!!");
                              }
-                             tcs.resolve(result);
                          });
-                     return tcs.promise();
                  },
                 attached = function (view) {
                     // validation
                     validation.init($('#patient-emergency-transfer-form'), form());
 
+
+
+                },
+                compositionComplete = function() {
+                    $("[data-i18n]").each(function (i, v) {
+                        var $label = $(v),
+                            text = $label.data("i18n");
+                        if (typeof i18n[text] === "string") {
+                            $label.text(i18n[text]);
+                        }
+                    });
                 },
 
-                save = function() {
+                                save = function() {
                     if (!validation.valid()) {
                         return Task.fromResult(false);
                     }
 
-                    var tcs = new $.Deferred(),
-                        data = ko.mapping.toJSON(entity);
+                    var data = ko.mapping.toJSON(entity);
 
                         
 
-                    context.post(data, "/Patient/Save")
+                    return context.post(data, "/Patient/Save")
                         .then(function(result) {
-                            tcs.resolve(result);
+                            entity().Id(result.id);
+                            app.showMessage("Your Patient has been successfully saved", "SPH Platform Showcase", ["OK"]);
+
                         });
                     
 
-                    return tcs.promise();
                 },
                 remove = function() {
-                    var tcs = new $.Deferred();
-                    $.ajax({
+                    return $.ajax({
                         type: "DELETE",
-                        url: "/Patient/Remove/" + entity().PatientId(),
+                        url: "/Patient/Remove/" + entity().Id(),
                         contentType: "application/json; charset=utf-8",
                         dataType: "json",
-                        error: tcs.reject,
                         success: function() {
-                            tcs.resolve(true);
                             app.showMessage("Your item has been successfully removed", "Removed", ["OK"])
                               .done(function () {
-                                window.location = "#patient";
-                            });
-                        } 
+                                  window.location = "#patient";
+                              });
+                        }
                     });
 
 
-                    return tcs.promise();
                 };
 
             var vm = {
-        activate: activate,
-        config: config,
-        attached: attached,
-        entity: entity,
-        errors: errors,
-        save : save,
+                                    activate: activate,
+                config: config,
+                attached: attached,
+                compositionComplete:compositionComplete,
+                entity: entity,
+                errors: errors,
+                save : save,
                     register : register,
                     discharge : discharge,
                     transfer : transfer,
                     admit : admit,
-        //
+                //
 
 
-        toolbar : {
+                toolbar : {
                         emailCommand : {
-                entity : "Patient",
-                id :id
-            },
-                        printCommand :{
-                entity : 'Patient',
-                id : id
-            },
-                        
-            watchCommand: function() {
-                return watcher.watch("Patient", entity().PatientId())
-                    .done(function(){
-                        watching(true);
-                    });
-            },
-            unwatchCommand: function() {
-                return watcher.unwatch("Patient", entity().PatientId())
-                    .done(function(){
-                        watching(false);
-                    });
-            },
-            watching: watching,
-                        
-            saveCommand : transfer,
-            
-            commands : ko.observableArray([])
-        }
-    };
+                        entity : "Patient",
+                        id :id
+                    },
+                                            printCommand :{
+                        entity : 'Patient',
+                        id : id
+                    },
+                                                                
+                    watchCommand: function() {
+                        return watcher.watch("Patient", entity().Id())
+                            .done(function(){
+                                watching(true);
+                            });
+                    },
+                    unwatchCommand: function() {
+                        return watcher.unwatch("Patient", entity().Id())
+                            .done(function(){
+                                watching(false);
+                            });
+                    },
+                    watching: watching,
+                                            
+                    saveCommand : transfer,
+                    
+                    commands : ko.observableArray([])
+                }
+            };
 
-    return vm;
-    });
+            return vm;
+        });
