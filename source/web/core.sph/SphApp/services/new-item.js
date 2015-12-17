bbd45657498222860e5de7e6c3ee8e570a2f6f0c
erymuzuan <erymuzuan@hotmail.com>
2015-12-17 14:19:32 +0800

    Context menu in solution explorer and move the shared code to services
    Related Work Items: #3079, #3081, #3083, #3084, #3085

---
+++ source/web/core.sph/SphApp/services/new-item.js
@@ -0,0 +1,68 @@
+﻿define(["services/datacontext", "services/logger", objectbuilders.config, objectbuilders.router, "services/app"],
+    function (context, logger, config, router, app) {
+    var addEntityDefinitionAsync = function () {
+
+        return app.showDialog("new.entity.definition.dialog")
+              .done(function (dialog, result) {
+                  if (result === "OK") {
+                      router.navigate("#entity.details/" + ko.unwrap(dialog.id));
+                  }
+              });
+    },
+            addWorkflowDefinitionAsync = function () {
+
+                return app.showDialog("new.workflow.definition.dialog")
+                      .done(function (dialog, result) {
+                          if (result === "OK") {
+                              router.navigate("#workflow.definition.visual/" + ko.unwrap(dialog.id));
+                          }
+                      });
+            },
+            addTransformDefinitionAsync = function () {
+
+                return app.showDialog("new.transform.definition.dialog")
+                      .done(function (dialog, result) {
+                          if (result === "OK") {
+                              router.navigate("#transform.definition.edit/" + ko.unwrap(dialog.id));
+                          }
+                      });
+            },
+            addTriggerAsync = function () {
+
+                return app.showDialog("new.trigger.dialog")
+                      .done(function (dialog, result) {
+                          if (result === "OK") {
+                              router.navigate("#trigger.setup/" + ko.unwrap(dialog.id));
+                          }
+                      });
+            },
+            addAdapterAsync = function () {
+
+                return app.showDialog("new.adapter.dialog")
+                      .done(function (dialog, result) {
+                          if (result === "OK") {
+                              router.navigate("#" + ko.unwrap(dialog.url));
+                          }
+                      });
+            },
+            addReportDefinitionAsync = function () {
+
+                return app.showDialog("new.report.definition.dialog")
+                      .done(function (dialog, result) {
+                          if (result === "OK") {
+                              router.navigate("#reportdefinition.edit/" + ko.unwrap(dialog.id));
+                          }
+                      });
+            };
+
+    var vm = {
+        addEntityDefinitionAsync: addEntityDefinitionAsync,
+        addAdapterAsync: addAdapterAsync,
+        addReportDefinitionAsync: addReportDefinitionAsync,
+        addTriggerAsync: addTriggerAsync,
+        addTransformDefinitionAsync: addTransformDefinitionAsync,
+        addWorkflowDefinitionAsync: addWorkflowDefinitionAsync
+    };
+
+    return vm;
+});
--- source/web/core.sph/SphApp/viewmodels/dev.home.js
+++ source/web/core.sph/SphApp/viewmodels/dev.home.js
@@ -8,8 +8,8 @@
 /// <reference path="../services/domain.g.js" />
 /// <reference path="../../Scripts/bootstrap.js" />
 /// <reference path="~/Scripts/_task.js" />
-define(["services/datacontext", "services/logger", objectbuilders.config, objectbuilders.router, "services/app"],
-    function (context, logger, config, router, app) {
+define(["services/datacontext", "services/logger", objectbuilders.config, objectbuilders.router, "services/app", "services/new-item"],
+    function (context, logger, config, router, app, newItemService) {
 
         var isBusy = ko.observable(false),
             groups = ko.observableArray(),
@@ -37,60 +37,6 @@ define(["services/datacontext", "services/logger", objectbuilders.config, object
             },
             openSnippetsDialog = function () {
 
-            },
-            addEntityDefinitionAsync = function () {
-
-              return app.showDialog("new.entity.definition.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#entity.details/" + ko.unwrap(dialog.id));
-                        }
-                    });
-            },
-            addWorkflowDefinitionAsync = function () {
-
-              return app.showDialog("new.workflow.definition.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#workflow.definition.visual/" + ko.unwrap(dialog.id));
-                        }
-                    });
-            },
-            addTransformDefinitionAsync = function () {
-
-              return app.showDialog("new.transform.definition.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#transform.definition.edit/" + ko.unwrap(dialog.id));
-                        }
-                    });
-            },
-            addTriggerAsync = function () {
-
-              return app.showDialog("new.trigger.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#trigger.setup/" + ko.unwrap(dialog.id));
-                        }
-                    });
-            },
-            addAdapterAsync = function () {
-
-              return app.showDialog("new.adapter.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#" + ko.unwrap(dialog.url));
-                        }
-                    });
-            },
-            addReportDefinitionAsync = function () {
-
-              return app.showDialog("new.report.definition.dialog")
-                    .done(function (dialog, result) {
-                        if (result === "OK") {
-                            router.navigate("#reportdefinition.edit/" + ko.unwrap(dialog.id));
-                        }
-                    });
             };
 
         var vm = {
@@ -101,12 +47,7 @@ define(["services/datacontext", "services/logger", objectbuilders.config, object
             attached: attached,
             openSnippetsDialog: openSnippetsDialog,
             groups: groups,
-            addEntityDefinitionAsync: addEntityDefinitionAsync,
-            addAdapterAsync: addAdapterAsync,
-            addReportDefinitionAsync: addReportDefinitionAsync,
-            addTriggerAsync: addTriggerAsync,
-            addTransformDefinitionAsync: addTransformDefinitionAsync,
-            addWorkflowDefinitionAsync: addWorkflowDefinitionAsync
+            newItemService: newItemService
         };
 
         return vm;
