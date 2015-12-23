e11f5cca22cc40c26c1b3b1f0c1ef6147b6d80cd
erymuzuan <erymuzuan@hotmail.com>
2015-12-23 09:26:42 +0800

    cleaning using

--- source/web/web.sph/Global.asax.cs
+++ source/web/web.sph/Global.asax.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Security.Cryptography;
 using System.Security.Principal;
 using System.Web;
