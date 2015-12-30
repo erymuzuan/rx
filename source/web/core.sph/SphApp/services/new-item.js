b0b78ee8ac2a89a8acb7653c3a26f7ce62c92ebf
erymuzuan <erymuzuan@hotmail.com>
2015-12-30 08:09:15 +0800

    Wait for 500 ms for the adapter to be saved
    Related Work Items: #3133

--- source/web/core.sph/SphApp/services/new-item.js
+++ source/web/core.sph/SphApp/services/new-item.js
@@ -1,74 +1,78 @@
-﻿define(["services/datacontext", "services/logger", objectbuilders.config, objectbuilders.router, "services/app","viewmodels/_custom.forms.routes", "viewmodels/_custom.forms.dialogs", "viewmodels/_custom.forms.partial.views", "viewmodels/_custom.forms.scripts"],
+﻿define(["services/datacontext", "services/logger", objectbuilders.config, objectbuilders.router, "services/app", "viewmodels/_custom.forms.routes", "viewmodels/_custom.forms.dialogs", "viewmodels/_custom.forms.partial.views", "viewmodels/_custom.forms.scripts"],
     function (context, logger, config, router, app, customForm, customDialog, partialView, customScript) {
-    var addEntityDefinitionAsync = function () {
+        var addEntityDefinitionAsync = function () {
 
-        return app.showDialog("new.entity.definition.dialog")
-              .done(function (dialog, result) {
-                  if (result === "OK") {
-                      router.navigate("#entity.details/" + ko.unwrap(dialog.id));
-                  }
-              });
-    },
-            addWorkflowDefinitionAsync = function () {
+            return app.showDialog("new.entity.definition.dialog")
+                  .done(function (dialog, result) {
+                      if (result === "OK") {
+                          router.navigate("#entity.details/" + ko.unwrap(dialog.id));
+                      }
+                  });
+        },
+                addWorkflowDefinitionAsync = function () {
 
-                return app.showDialog("new.workflow.definition.dialog")
-                      .done(function (dialog, result) {
-                          if (result === "OK") {
-                              router.navigate("#workflow.definition.visual/" + ko.unwrap(dialog.id));
-                          }
-                      });
-            },
-            addTransformDefinitionAsync = function () {
+                    return app.showDialog("new.workflow.definition.dialog")
+                          .done(function (dialog, result) {
+                              if (result === "OK") {
+                                  router.navigate("#workflow.definition.visual/" + ko.unwrap(dialog.id));
+                              }
+                          });
+                },
+                addTransformDefinitionAsync = function () {
 
-                return app.showDialog("new.transform.definition.dialog")
-                      .done(function (dialog, result) {
-                          if (result === "OK") {
-                              router.navigate("#transform.definition.edit/" + ko.unwrap(dialog.id));
-                          }
-                      });
-            },
-            addTriggerAsync = function () {
+                    return app.showDialog("new.transform.definition.dialog")
+                          .done(function (dialog, result) {
+                              if (result === "OK") {
+                                  setTimeout(function () {
+                                      router.navigate("#transform.definition.edit/" + ko.unwrap(dialog.id));
+                                  }, 500);
+                              }
+                          });
+                },
+                addTriggerAsync = function () {
 
-                return app.showDialog("new.trigger.dialog", function (dialog) {
-                    dialog.entity(null);
-                })
-                      .done(function (dialog, result) {
-                          if (result === "OK") {
-                              router.navigate("#trigger.setup/" + ko.unwrap(dialog.id));
-                          }
-                      });
-            },
-            addAdapterAsync = function () {
+                    return app.showDialog("new.trigger.dialog", function (dialog) {
+                        dialog.entity(null);
+                    })
+                          .done(function (dialog, result) {
+                              if (result === "OK") {
+                                  router.navigate("#trigger.setup/" + ko.unwrap(dialog.id));
+                              }
+                          });
+                },
+                addAdapterAsync = function () {
 
-                return app.showDialog("new.adapter.dialog")
-                      .done(function (dialog, result) {
-                          if (result === "OK") {
-                              router.navigate("#" + ko.unwrap(dialog.url));
-                          }
-                      });
-            },
-            addReportDefinitionAsync = function () {
+                    return app.showDialog("new.adapter.dialog")
+                          .done(function (dialog, result) {
+                              if (result === "OK") {
+                                  setTimeout(function () {
+                                      router.navigate("#" + ko.unwrap(dialog.url));
+                                  }, 500);
+                              }
+                          });
+                },
+                addReportDefinitionAsync = function () {
 
-                return app.showDialog("new.report.definition.dialog")
-                      .done(function (dialog, result) {
-                          if (result === "OK") {
-                              router.navigate("#reportdefinition.edit/" + ko.unwrap(dialog.id));
-                          }
-                      });
-            };
+                    return app.showDialog("new.report.definition.dialog")
+                          .done(function (dialog, result) {
+                              if (result === "OK") {
+                                  router.navigate("#reportdefinition.edit/" + ko.unwrap(dialog.id));
+                              }
+                          });
+                };
 
-    var vm = {
-        addCustomFormAsync: customForm.addNew,
-        addCustomDialogAsync: customDialog.addNewDialog,
-        addPartialViewAsync: partialView.addNewPartialView,
-        addCustomScriptAsync: customScript.addNew,
-        addEntityDefinitionAsync: addEntityDefinitionAsync,
-        addAdapterAsync: addAdapterAsync,
-        addReportDefinitionAsync: addReportDefinitionAsync,
-        addTriggerAsync: addTriggerAsync,
-        addTransformDefinitionAsync: addTransformDefinitionAsync,
-        addWorkflowDefinitionAsync: addWorkflowDefinitionAsync
-    };
+        var vm = {
+            addCustomFormAsync: customForm.addNew,
+            addCustomDialogAsync: customDialog.addNewDialog,
+            addPartialViewAsync: partialView.addNewPartialView,
+            addCustomScriptAsync: customScript.addNew,
+            addEntityDefinitionAsync: addEntityDefinitionAsync,
+            addAdapterAsync: addAdapterAsync,
+            addReportDefinitionAsync: addReportDefinitionAsync,
+            addTriggerAsync: addTriggerAsync,
+            addTransformDefinitionAsync: addTransformDefinitionAsync,
+            addWorkflowDefinitionAsync: addWorkflowDefinitionAsync
+        };
 
-    return vm;
-});
+        return vm;
+    });
