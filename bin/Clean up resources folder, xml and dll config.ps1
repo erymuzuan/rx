448207d68cbc82d979120baa38fc3b013f01cfbb
erymuzuan <erymuzuan@hotmail.com>
2016-01-01 16:02:24 +0800

    Add cleaning scripts

---
+++ bin/Clean up resources folder, xml and dll config.ps1
@@ -0,0 +1,3 @@
+﻿ls -Filter Humanizer.resources.dll -Path .\ -Recurse | % { $_.FullName.Replace("Humanizer.resources.dll", "")} | Remove-Item -Force -Recurse
+ls -Filter *.dll.config -Path .\bin\ -Recurse | Remove-Item
+ls -Filter *.xml -Path .\ -Recurse | Remove-Item
