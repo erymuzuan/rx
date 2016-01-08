e6ebb888364b15b96f0584c92c375bb77b22e438
erymuzuan <erymuzuan@hotmail.com>
2016-01-08 20:33:32 +0800

    Make it  a little better to read and understand
    Related Work Items: #3180

--- bin/Clean up resources folder, xml and dll config.ps1
+++ bin/Clean up resources folder, xml and dll config.ps1
@@ -1,3 +1,4 @@
-﻿ls -Filter Humanizer.resources.dll -Path .\ -Recurse | % { $_.FullName.Replace("Humanizer.resources.dll", "")} | Remove-Item -Force -Recurse
-ls -Filter *.dll.config -Path .\bin\ -Recurse | Remove-Item
+﻿ls -Filter *.dll.config -Path .\bin -Recurse | Remove-Item
 ls -Filter *.xml -Path .\ -Recurse | Remove-Item
+ls -Filter Humanizer.resources.dll -Path .\ -Recurse | % { $_.FullName.Replace("\Humanizer.resources.dll", "")} | Remove-Item -Force -Recurse
+ls -Filter System.Spatial.resources.dll -Path .\ -Recurse | % { $_.FullName.Replace("\System.Spatial.resources.dll", "")} | Remove-Item -Force -Recurse
