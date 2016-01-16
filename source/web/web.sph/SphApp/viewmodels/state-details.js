4e4f92ff159c93d5eb223327159d150e5e5f0248
erymuzuan <erymuzuan@hotmail.com>
2016-01-16 17:15:48 +0800

    Test files

--- source/web/web.sph/SphApp/viewmodels/state-details.js
+++ source/web/web.sph/SphApp/viewmodels/state-details.js
@@ -1,135 +1,130 @@
+define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
+objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
+objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
+objectbuilders.app],
 
-    define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
-        objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
-        objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
-        objectbuilders.app ],
-        function (context, logger, router, system, validation, eximp, dialog, watcher,config,app
-            ) {
+function(context, logger, router, system, validation, eximp, dialog, watcher, config, app) {
 
-            var entity = ko.observable(new bespoke.DevV1_state.domain.State({WebId:system.guid()})),
-                errors = ko.observableArray(),
-                form = ko.observable(new bespoke.sph.domain.EntityForm()),
-                watching = ko.observable(false),
-                id = ko.observable(),
-                i18n = null,
-                activate = function (entityId) {
-                    id(entityId);
+    var entity = ko.observable(new bespoke.DevV1_state.domain.State(system.guid())),
+        errors = ko.observableArray(),
+        form = ko.observable(new bespoke.sph.domain.EntityForm()),
+        watching = ko.observable(false),
+        id = ko.observable(),
+        i18n = null,
+        activate = function(entityId) {
+            id(entityId);
 
-                    var query = String.format("Id eq '{0}'", entityId),
-                        tcs = new $.Deferred(),
-                        itemTask = context.loadOneAsync("State", query),
-                        formTask = context.loadOneAsync("EntityForm", "Route eq 'state-details'"),
-                        watcherTask = watcher.getIsWatchingAsync("State", entityId),
-                        i18nTask = $.getJSON("i18n/" + config.lang + "/state-details");
+            var query = String.format("Id eq '{0}'", entityId),
+                tcs = new $.Deferred(),
+                itemTask = context.loadOneAsync("State", query),
+                formTask = context.loadOneAsync("EntityForm", "Route eq 'state-details'"),
+                watcherTask = watcher.getIsWatchingAsync("State", entityId),
+                i18nTask = $.getJSON("i18n/" + config.lang + "/state-details");
 
-                    $.when(itemTask, formTask, watcherTask, i18nTask).done(function(b,f,w,n) {
-                        if (b) {
-                            var item = context.toObservable(b);
-                            entity(item);
-                        }
-                        else {
-                            entity(new bespoke.DevV1_state.domain.State({WebId:system.guid()}));
-                        }
-                        form(f);
-                        watching(w);
-                        i18n = n[0];
-                            tcs.resolve(true);
-                        
-                    });
-
-
-
-                    return tcs.promise();
-                },
-                attached = function (view) {
-                    // validation
-                    validation.init($('#state-details-form'), form());
-
-
-
-                },
-                compositionComplete = function() {
-                    $("[data-i18n]").each(function (i, v) {
-                        var $label = $(v),
-                            text = $label.data("i18n");
-                        if (typeof i18n[text] === "string") {
-                            $label.text(i18n[text]);
-                        }
-                    });
-                },
-
-                                save = function() {
-                    if (!validation.valid()) {
-                        return Task.fromResult(false);
-                    }
-
-                    var tcs = new $.Deferred(),
-                        data = ko.mapping.toJSON(entity);
-
-                        
-
-                    context.post(data, "/State/Save")
-                        .then(function(result) {
-                            tcs.resolve(result);
-                            entity().Id(result.id);
-                            app.showMessage("Your State has been successfully saved", "SPH Platform Showcase", ["ok"]);
-
-                        });
-                    
-
-                    return tcs.promise();
-                },
-                remove = function() {
-                    var tcs = new $.Deferred();
-                    $.ajax({
-                        type: "DELETE",
-                        url: "/State/Remove/" + entity().Id(),
-                        contentType: "application/json; charset=utf-8",
-                        dataType: "json",
-                        error: tcs.reject,
-                        success: function() {
-                            tcs.resolve(true);
-                            app.showMessage("Your item has been successfully removed", "Removed", ["OK"])
-                              .done(function () {
-                                  window.location = "#state";
-                              });
-                        }
-                    });
-
-
-                    return tcs.promise();
-                };
-
-            var vm = {
-                                    activate: activate,
-                config: config,
-                attached: attached,
-                compositionComplete:compositionComplete,
-                entity: entity,
-                errors: errors,
-                save : save,
-                //
-
-
-                toolbar : {
-                        emailCommand : {
-                        entity : "State",
-                        id :id
-                    },
-                                            printCommand :{
-                        entity : 'State',
-                        id : id
-                    },
-                                    removeCommand :remove,
-                    canExecuteRemoveCommand : ko.computed(function(){
-                        return entity().Id();
-                    }),
-                                                                
-                    saveCommand : save,
-                    
-                    commands : ko.observableArray([])
+            $.when(itemTask, formTask, watcherTask, i18nTask).done(function(b, f, w, n) {
+                if (b) {
+                    var item = context.toObservable(b);
+                    entity(item);
+                } else {
+                    entity(new bespoke.DevV1_state.domain.State(system.guid()));
                 }
-            };
+                form(f);
+                watching(w);
+                i18n = n[0];
+                tcs.resolve(true);
 
-            return vm;
-        });
+
+            });
+
+            return tcs.promise();
+        },
+
+        save = function() {
+
+            if (!validation.valid()) {
+                return Task.fromResult(false);
+            }
+
+            var data = ko.mapping.toJSON(entity),
+                tcs = new $.Deferred();
+
+            context.put(data, "/State/save")
+                .then(function(result) {
+                if (result.success) {
+                    logger.info(result.message);
+                    entity().Id(result.id);
+                    errors.removeAll();
+
+                } else {
+                    errors.removeAll();
+                    _(result.rules).each(function(v) {
+                        errors(v.ValidationErrors);
+                    });
+                    logger.error("There are errors in your entity, !!!");
+                }
+                tcs.resolve(result);
+            });
+            return tcs.promise();
+        }, remove = function {
+            return context.sendDelete("State/Save/ko.unwrap(entity().Id)");
+        },
+
+        attached = function(view) {
+            // validation
+            validation.init($('#state-details-form'), form());
+
+        },
+
+        compositionComplete = function() {
+            $("[data-i18n]").each(function(i, v) {
+                var $label = $(v),
+                    text = $label.data("i18n");
+                if (typeof i18n[text] === "string") {
+                    $label.text(i18n[text]);
+                }
+            });
+        },
+
+        saveCommand = function() {
+            return save()
+                .then(function(result) {
+                if (result.success) return app.showMessage("Done", ["OK"]);
+                else return Task.fromResult(false);
+            })
+                .then(function(result) {
+                if (result) router.navigate("#all-states");
+            });
+        };
+
+    var vm = {
+
+        activate: activate,
+        config: config,
+        attached: attached,
+        compositionComplete: compositionComplete,
+        entity: entity,
+        errors: errors,
+        save: save,
+        toolbar: {
+            emailCommand: {
+                entity: "State",
+                id: id
+            },
+
+            printCommand: {
+                entity: 'State',
+                id: id
+            },
+            removeCommand: remove,
+            canExecuteRemoveCommand: ko.computed(function() {
+                return entity().Id();
+            }),
+            saveCommand: saveCommand,
+
+        }, // end toolbar
+
+        commands: ko.observableArray([])
+    };
+
+    return vm;
+});
