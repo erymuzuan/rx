4c3780d6ddcfeedeb01320a93b24137b55ffde36
erymuzuan <erymuzuan@hotmail.com>
2016-01-23 20:57:43 +0800

    jQuery 2.2.0

--- source/web/core.sph/Scripts/_references.js
+++ source/web/core.sph/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-2.1.3.js" />
+﻿/// <reference path="jquery-2.2.0.js" />
 /// <reference path="jquery-ui-1.11.1.js" />
 /// <reference path="modernizr-2.8.3.js" />
 /// <reference path="bootstrap.js" />
