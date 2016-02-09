99153d76507685ffb355aaa85d04307968cfceb7
erymuzuan <erymuzuan@hotmail.com>
2016-02-09 14:40:10 +0800

    Search controller goes WebApi

--- source/web/core.sph/Areas/Sph/Controllers/SearchController.cs
+++ source/web/core.sph/Areas/Sph/Controllers/SearchController.cs
@@ -1,31 +1,50 @@
 ﻿using System;
 using System.Net.Http;
+using System.Web.Http;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using System.Linq;
 using Bespoke.Sph.Domain;
-using Bespoke.Sph.Web.Filters;
-using Bespoke.Sph.Web.Helpers;
+using Bespoke.Sph.WebApi;
+using Humanizer;
 
 namespace Bespoke.Sph.Web.Controllers
 {
-    [NoCache]
     [RoutePrefix("search")]
-    public class SearchController : Controller
+    public class SearchController : BaseApiController
     {
-        [HttpGet]
-        [Route("{text}")]
-        public async Task<ActionResult> Index(string text)
+        public SearchController()
         {
-            var context = new SphDataContext();
-            var en = (await context.GetListAsync<EntityDefinition, string>(e => e.IsPublished == true, e => e.RecordName))
-                .Select(a => $"\"{a}\":{{}}");
-            var entNames = (await context.GetListAsync<EntityDefinition, string>(e => e.IsPublished == true, e => e.Name))
-                .Select(a => a.ToLowerInvariant())
-                .ToArray();
-            var types = string.Join(",", entNames);
+            this.CacheManager = ObjectBuilder.GetObject<ICacheManager>();
+        }
 
-            var records = string.Join(",", en.Distinct().ToArray());
+        public ICacheManager CacheManager { get; set; }
+
+        private const string TypesKey = "search:published-types";
+        private const string RecordKey = "search:published-records";
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> Index([FromUri(Name = "q")]string text)
+        {
+            var types = this.CacheManager.Get<string>(TypesKey);
+            var records = this.CacheManager.Get<string>(RecordKey);
+            if (null == types || null == records)
+            {
+                var context = new SphDataContext();
+                var en = (await context.GetListAsync<EntityDefinition, string>(e => e.IsPublished, e => e.RecordName))
+                    .Select(a => $"\"{a}\":{{}}");
+                var entNames = (await context.GetListAsync<EntityDefinition, string>(e => e.IsPublished, e => e.Name))
+                    .Select(a => a.ToLowerInvariant())
+                    .ToArray();
+                types = string.Join(",", entNames);
+                records = string.Join(",", en.Distinct().ToArray());
+
+                CacheManager.Insert(TypesKey, types, 5.Minutes());
+                CacheManager.Insert(RecordKey, records, 5.Minutes());
+            }
+
+
+
             var json = @"
                 {
                     ""query"": {
@@ -45,7 +64,7 @@ namespace Bespoke.Sph.Web.Controllers
             ";
 
             var request = new StringContent(json);
-            var url = $"{ConfigurationManager.ApplicationName.ToLowerInvariant()}/{types}/_search";
+            var url = $"{ConfigurationManager.ElasticSearchIndex}/{types}/_search";
 
             using (var client = new HttpClient())
             {
@@ -54,91 +73,41 @@ namespace Bespoke.Sph.Web.Controllers
                 var response = await client.PostAsync(url, request);
                 var content = response.Content as StreamContent;
                 if (null == content) throw new Exception("Cannot execute query on es " + request);
-                this.Response.ContentType = "application/json; charset=utf-8";
                 var json2 = await content.ReadAsStringAsync();
-                return Content(json2);
+                return Json(json2);
 
             }
         }
 
-        public async Task<ActionResult> Es(string type, string json, bool sys = true)
+        [HttpPost]
+        [Route("{type}")]
+        public async Task<IHttpActionResult> Es(string type, [RawBody]string json, [FromUri]bool sys = true)
         {
             var request = new StringContent(json);
-            var url = $"{ConfigurationManager.ElasticSearchIndex}{(sys ? "_sys" : "")}/{type}/_search";
+            var index = sys ? ConfigurationManager.ElasticSearchSystemIndex : ConfigurationManager.ElasticSearchIndex;
+            var url = $"{index}/{type.ToLowerInvariant()}/_search";
 
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ConfigurationManager.ElasticSearchHost);
 
                 var response = await client.PostAsync(url, request);
+                response.EnsureSuccessStatusCode();
                 var content = response.Content as StreamContent;
                 if (null == content) throw new Exception("Cannot execute query on es " + request);
-                this.Response.ContentType = "application/json; charset=utf-8";
-                return Content(await content.ReadAsStringAsync());
+                return Json(await content.ReadAsStringAsync());
 
             }
         }
 
         [HttpPost]
         [Route("workflow/{id}/v{version}")]
-        public async Task<ActionResult> Workflow(string id, string version, [RawRequestBody]string json)
+        public async Task<IHttpActionResult> Workflow(string id, string version, [RawBody]string json)
         {
             var wfes = $"{id}workflow".Replace("-", "");
             return await Es(wfes, json, false);
         }
 
-        [Route("activity")]
-        public async Task<ActionResult> Activity()
-        {
-            return await Es("activity", this.GetRequestBody(), false);
-        }
-
-        [Route("log")]
-        public async Task<ActionResult> Log([RawRequestBody]string body)
-        {
-            return await Es("log", body);
-        }
-
-        public async Task<ActionResult> WorkflowDefinition()
-        {
-            return await Es(typeof(WorkflowDefinition).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> EntityDefinition()
-        {
-            return await Es(typeof(EntityDefinition).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        [HttpPost]
-        [Route("entityview")]
-        public async Task<ActionResult> EntityView()
-        {
-            return await Es(typeof(EntityView).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> EntityForm()
-        {
-            return await Es(typeof(EntityForm).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> Designation()
-        {
-            return await Es(typeof(Designation).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-
-        public async Task<ActionResult> ReportDefinition()
-        {
-            return await Es(typeof(ReportDefinition).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> ReportDelivery()
-        {
-            return await Es(typeof(ReportDelivery).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> Message()
-        {
-            return await Es(typeof(Message).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-        public async Task<ActionResult> Page()
-        {
-            return await Es(typeof(Page).Name.ToLowerInvariant(), this.GetRequestBody());
-        }
-
-
+    
     }
 }
