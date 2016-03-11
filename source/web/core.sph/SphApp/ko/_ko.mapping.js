c0e83c20684d2bb2434f6aead38ba54b3d9419b2
erymuzuan <erymuzuan@hotmail.com>
2016-03-11 08:35:34 +0800

    Moving some of the more stable scripts into core.sph

--- source/web/core.sph/SphApp/ko/_ko.mapping.js
+++ source/web/core.sph/SphApp/ko/_ko.mapping.js
@@ -1,5 +1,5 @@
-﻿define(['knockout'], function(ko) {
-    ko.bindingHandlers.responseSchemaTree = {
+﻿define(["knockout"], function (ko) {
+    ko.bindingHandlers.jsonTree = {
         init: function (element, valueAccessor) {
             var system = require(objectbuilders.system),
                 value = valueAccessor(),
@@ -17,7 +17,7 @@
                     node.children = _(node.data.MemberCollection()).map(function (v) {
                         return {
                             text: v.Name(),
-                            state: 'open',
+                            state: "open",
                             type: v.TypeName(),
                             data: v
                         };
@@ -25,17 +25,17 @@
                     _(node.children).each(recurseChildMember);
                 },
                 loadJsTree = function () {
-                    jsTreeData.children = _(entity.MemberCollection()).map(function (v) {
+                    jsTreeData.children = _(entity.properties).map(function (v) {
                         return {
                             text: v.Name(),
-                            state: 'open',
+                            state: "open",
                             type: v.TypeName(),
                             data: v
                         };
                     });
                     _(jsTreeData.children).each(recurseChildMember);
                     $(element)
-                        .on('select_node.jstree', function (node, selected) {
+                        .on("select_node.jstree", function (node, selected) {
                             if (selected.node.data && typeof selected.node.data.Name === "function") {
                                 member(selected.node.data);
 
@@ -51,10 +51,10 @@
                                 });
                             }
                         })
-                        .on('create_node.jstree', function (event, node) {
+                        .on("create_node.jstree", function (event, node) {
                             console.log(node, "node");
                         })
-                        .on('rename_node.jstree', function (ev, node) {
+                        .on("rename_node.jstree", function (ev, node) {
                             var mb = node.node.data;
                             mb.Name(node.text);
                         })
@@ -70,12 +70,12 @@
                                     {
                                         label: "Add Child",
                                         action: function () {
-                                            var child = new bespoke.sph.domain.Member({ WebId: system.guid(), TypeName: 'System.String, mscorlib', Name: 'Member_Name' }),
-                                                parent = $(element).jstree('get_selected', true),
+                                            var child = new bespoke.sph.domain.Member({ WebId: system.guid(), TypeName: "System.String, mscorlib", Name: "Member_Name" }),
+                                                parent = $(element).jstree("get_selected", true),
                                                 mb = parent[0].data,
-                                                newNode = { state: "open", type: "System.String, mscorlib", text: 'Member_Name', data: child };
+                                                newNode = { state: "open", type: "System.String, mscorlib", text: "Member_Name", data: child };
 
-                                            child.$type = ko.observable('Bespoke.Sph.Integrations.Adapters.RegexMember, http.adapter');
+                                            child.$type = ko.observable("Bespoke.Sph.Integrations.Adapters.RegexMember, http.adapter");
                                             child.Pattern = ko.observable();
                                             child.Group = ko.observable();
                                             child.DateFormat = ko.observable();
@@ -110,7 +110,7 @@
 
                                             // now delete the member
                                             var n = ref.get_selected(true)[0],
-                                                p = ref.get_node($('#' + n.parent)),
+                                                p = ref.get_node($("#" + n.parent)),
                                                 parentMember = p.data;
                                             if (parentMember && typeof parentMember.MemberCollection === "function") {
                                                 var child = _(parentMember.MemberCollection()).find(function (v) {
@@ -182,5 +182,91 @@
 
         }
     };
+
+    ko.bindingHandlers.functoidPopover = {
+        init: function (element, valueAccessor) {
+
+            var div = $(element),
+                functoid = ko.unwrap(valueAccessor()),
+                uid = ko.unwrap(functoid.WebId),
+                fullName = typeof functoid.$type === "function" ? functoid.$type() : functoid.$type,
+                name = /(.*?),/.exec(fullName)[1],
+                td = ko.dataFor(div.parent()[0]),
+                pop = div.popover({
+                    title: name + " : " + ko.unwrap(functoid.Name),
+                    html: true,
+                    trigger: "manual",
+                    content: function () {
+                        $("a.edit-functoid").popover("hide");
+                        return '   <div class="context-menu">' +
+                                '       <a class="edit-functoid ' + uid + '" title="edit functoid" href="#">' +
+                                '           <i class="fa fa-edit fa-2x"></i>' +
+                                "       </a>" +
+                                '       <a class="delete-functoid ' + uid + '" href="#" title="remove functoid">' +
+                                '           <i class="fa fa-times-circle fa-2x"></i>' +
+                                "       </a>" +
+                                "   </div>";
+                    }
+                });
+
+            div.mousedown(function (e) {
+                if (e.button === 2) {
+                    e.preventDefault();
+                    div.popover("toggle");
+
+                    return false;
+                }
+                return true;
+            }).bind("contextmenu", function () { return false; });
+            // just display it for 5 seconds
+            pop.on("shown.bs.popover", function () {
+                setTimeout(function () { pop.popover("hide"); }, 5000);
+            });
+
+            $(document).on("click", "a." + functoid.WebId(), function (e) {
+                e.preventDefault();
+                pop.popover("hide");
+                var app = require(objectbuilders.app),
+                    link = $(this);
+                if (link.hasClass("delete-functoid")) {
+                    app.showMessage("Are you sure you want to remove this functoid, please remove all the connection before proceed", "Remove functoid", ["Yes", "No"])
+                        .done(function (result) {
+                            if (result === "Yes") {
+                                td.FunctoidCollection.remove(functoid);
+                            }
+                        });
+                }
+                if (link.hasClass("edit-functoid")) {
+                    var context = require(objectbuilders.datacontext),
+                        clone = context.toObservable(ko.mapping.toJS(functoid));
+                    var type = ko.unwrap(functoid.$type).toLowerCase();
+
+                    require(["viewmodels/functoid." + type, "durandal/app"], function (dialog, app2) {
+                        dialog.functoid(clone);
+
+                        app2.showDialog(dialog)
+                            .done(function (result) {
+                                $("div.modalBlockout,div.modalHost").remove();
+                                if (!result) return;
+                                if (result === "OK") {
+                                    for (var g in functoid) {
+                                        if (functoid.hasOwnProperty(g)) {
+                                            var obj = functoid[g];
+                                            if (ko.isObservable(obj)) {
+                                                obj(ko.unwrap(clone[g]));
+                                            } else {
+                                                functoid[g] = clone[g];
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+
+                    });
+                }
+            });
+        }
+    };
+
 
 });
--- source/web/core.sph/SphApp/services/jsonimportexport.js
+++ source/web/core.sph/SphApp/services/jsonimportexport.js
@@ -10,13 +10,14 @@
 define([],
     function () {
 
-        var exportJson = function (filename, text) {
+        var
+            exportJson = function (filename, text) {
 
                 var tcs = new $.Deferred();
                 function download() {
-                    var pom = document.createElement("a");
-                    pom.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-                    pom.setAttribute("download", filename);
+                    var pom = document.createElement('a');
+                    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+                    pom.setAttribute('download', filename);
                     pom.click();
                     tcs.resolve(true);
                 }
@@ -46,11 +47,11 @@ define([],
              }
 
              var upload = function () {
-                 var pom = document.createElement("input");
-                 pom.setAttribute("type", "file");
-                 pom.setAttribute("id", "files");
-                 pom.setAttribute("name", "files[]");
-                 pom.addEventListener("change", handleFileSelect, false);
+                 var pom = document.createElement('input');
+                 pom.setAttribute('type', 'file');
+                 pom.setAttribute('id', 'files');
+                 pom.setAttribute('name', 'files[]');
+                 pom.addEventListener('change', handleFileSelect, false);
                  pom.click();
              };
 
