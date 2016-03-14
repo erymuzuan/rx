5e7c07e1f88a920c6769c94c43558076f5a3b52a
erymuzuan <erymuzuan@hotmail.com>
2016-03-14 12:32:30 +0800

    Remove the FTP scripts, and call the .\FtpUpdatePacakge.ps1

--- Create-SphUpdatePackage.ps1
+++ Create-SphUpdatePackage.ps1
@@ -6,99 +6,6 @@
        [string]$Channel = 'Alpha'
      )
 
-function Create-FtpDirectory {
-  param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $sourceuri,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $username,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $password,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $Directory
-  )
-  
-  $SecureString = ConvertTo-SecureString $password -AsPlainText -Force
-  $PSCredential = New-Object System.Management.Automation.PSCredential ($username, $SecureString)
-
-  Import-Module PSFTP 
-  Set-FTPConnection -Credentials $PSCredential -Server ftp://www.reactivedeveloper.com -Session ss01 
-  $Session = Get-FTPConnection -Session ss01
-
-  New-FTPItem -Session $Session -Path /$Channel/binaries -Name $Directory
-    
-}
-
-
-function Remove-FtpDirectory {
-  param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $sourceuri,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $username,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $password,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $Directory
-  )
-  
-  $SecureString = ConvertTo-SecureString $password -AsPlainText -Force
-  $PSCredential = New-Object System.Management.Automation.PSCredential ($username, $SecureString)
-
-  Import-Module PSFTP 
-  Set-FTPConnection -Credentials $PSCredential -Server ftp://www.reactivedeveloper.com -Session ss01 
-  $Session = Get-FTPConnection -Session ss01
-
-  Remove-FTPItem -Session $Session -Path $Directory -Recurse
-    
-}
-
-
-function Upload-FtpFile {
-  param(
-    [Parameter(Mandatory=$true)]
-    [string]
-    $LocalFile,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $username,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $password,
-    [Parameter(Mandatory=$true)]
-    [string]
-    $RemoteDirectory
-  )
-  
-  Write-Host "Uploading $path ... please wait...."
-  if ($LocalFile -match '\\$|\\\w+$') { throw 'sourceuri should end with a file name' }
-  if((Test-Path($LocalFile)) -eq $false){
-    Write-Warning "Cannot find $LocalFile"
-    exit;
-  }
-    
-
-  
-  $SecureString = ConvertTo-SecureString $password -AsPlainText -Force
-  $PSCredential = New-Object System.Management.Automation.PSCredential ($username, $SecureString)
-
-  Import-Module PSFTP 
-  Set-FTPConnection -Credentials $PSCredential -Server ftp://www.reactivedeveloper.com -Session ss01 
-  $Session = Get-FTPConnection -Session ss01
-
-  Add-FTPItem -Session $Session -Overwrite -Path $RemoteDirectory -LocalPath $LocalFile
-
-}
-
-
 if($Build -eq 0)
 {
     Write-Host "Please specify the Build Number"
@@ -428,6 +335,10 @@ if($compressed -eq 'q')
     exit;
 }
 
+if(Test-Path ".\$Build.7z")
+{
+    Remove-Item ".\$Build.7z"
+}
 #compress
 & 7za a -t7z ".\$Build.7z" ".\bin\build\*"
 
@@ -462,29 +373,11 @@ $versionBuildJson > .\deployment\version.$Build.json
 #release note - copy from existing file, should maintains the UTF8 encoding
 copy .\deployment\release-note-template.md .\deployment\$Build.md
 
-$ftpRoot = "ftp://www.reactivedeveloper.com/$Channel/binaries"
-$ftpUserName = "rxdeveloper"
-$ftpPassword = "reH2TaXd"
+
 
 Write-Host -ForegroundColor Yellow "NOW edit the .\deployment\$Build.ps1 and the Release Note($Build.md) to reflect any custom scripts needed to be run"
 Write-Host "Press [ENTER] to continue uploaded  to ftp " -NoNewline -ForegroundColor Yellow
 Read-Host
 
 
-
-Create-FtpDirectory -sourceuri "$ftpRoot/" -username $ftpUserName -password $ftpPassword -Directory $Build
-Upload-FtpFile -RemoteDirectory "$ftpRoot/" -username $ftpUserName -password $ftpPassword -LocalFile .\deployment\$previous.json
-Upload-FtpFile -RemoteDirectory "$ftpRoot/$Build/" -username $ftpUserName -password $ftpPassword -LocalFile .\deployment\version.$Build.json
-
-if((Test-Path .\deployment\$Build.html) -eq $false)
-{
-    Write-Host -ForegroundColor Yellow "Cannot find $Build.html for your release note, please use the markdown files provided to generate the Release note"
-    Read-Host
-}
-
-Upload-FtpFile -RemoteDirectory "$ftpRoot/$Build/" -username $ftpUserName -password $ftpPassword -LocalFile .\deployment\$Build.html
-
-Upload-FtpFile -RemoteDirectory "$ftpRoot/$Build/" -username $ftpUserName -password $ftpPassword -LocalFile .\$Build.7z
-#$build.ps1 to root
-Upload-FtpFile -RemoteDirectory "$ftpRoot/" -username $ftpUserName -password $ftpPassword -LocalFile .\deployment\$Build.ps1
-Upload-FtpFile -RemoteDirectory "$ftpRoot/$Build/" -username $ftpUserName -password $ftpPassword -LocalFile .\deployment\$Build.ps1
+& .\Ftp-SphUpdatePackage.ps1 -Build $Build -Channel $Channel
