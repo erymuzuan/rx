83e6f33de3da3571e387c74d13f5441214b74c0d
erymuzuan <erymuzuan@hotmail.com>
2016-04-27 00:05:21 +0800

    OperationEndpoint icon changed to fa-pencil
    Related Work Items: #3839

--- source/web/core.sph/Solutions/OperationEndpointProviders.cs
+++ source/web/core.sph/Solutions/OperationEndpointProviders.cs
@@ -6,7 +6,7 @@ namespace Bespoke.Sph.Web.Solutions
     [Export(typeof(IItemsProvider))]
     public class OperationEndpointProvider : EntityDefinitionItemsProviders<OperationEndpoint>
     {
-        protected override string Icon => "fa fa-bolt";
+        protected override string Icon => "fa fa-pencil";
         protected override string GetUrl(OperationEndpoint item) => $"operation.endpoint.designer/{item.Id}";
     }
 }
