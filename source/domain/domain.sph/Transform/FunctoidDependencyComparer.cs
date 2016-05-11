a87466fdbe63d1f03c9a4aff66350fad02eb9f78
erymuzuan <erymuzuan@hotmail.com>
2016-05-11 12:14:47 +0800

    return -1 when comparing 2 functoids with no relations to each other, 0 should only be used for the same instance

--- source/domain/domain.sph/Transform/FunctoidDependencyComparer.cs
+++ source/domain/domain.sph/Transform/FunctoidDependencyComparer.cs
@@ -12,12 +12,12 @@ namespace Bespoke.Sph.Domain
             const int ZERO = 0;
 
             if (x.WebId == y.WebId) return ZERO;
-
+            
             var xargs = x.ArgumentCollection.Select(a => a.Functoid).ToArray();
             var yargs = y.ArgumentCollection.Select(a => a.Functoid).ToArray();
             if (xargs.Contains(y.WebId)) return ONE ;
             if (yargs.Contains(x.WebId)) return MINUS_ONE;
-            return ZERO;
+            return MINUS_ONE;
         }
     }
 }
