ea3357c729560942f806c9058920c9fb5d864239
erymuzuan <erymuzuan@hotmail.com>
2016-05-17 08:29:26 +0800

    Javascript refactoring
    Related Work Items: #3942

--- source/web/core.sph/SphApp/prototypes/IntervalScheduleContainer.js
+++ source/web/core.sph/SphApp/prototypes/IntervalScheduleContainer.js
@@ -1,8 +1,9 @@
 ﻿/// <reference path="../schemas/report.builder.g.js" />
-/// <reference path="../../Scripts/knockout-3.1.0.debug.js" />
+/// <reference path="../../Scripts/knockout-3.4.0.debug.js" />
 /// <reference path="../../Scripts/knockout.mapping-latest.debug.js" />
+/// <reference path="../../Scripts/require.js" />
 /// <reference path="../../Scripts/underscore.js" />
-/// <reference path="../../Scripts/jquery-2.0.3.intellisense.js" />
+/// <reference path="../../Scripts/jquery-2.2.0.intellisense.js" />
 /// <reference path="../../App/durandal/amd/require.js" />
 /// <reference path="../../App/durandal/system.js" />
 
@@ -14,13 +15,13 @@ bespoke.sph.domain = bespoke.sph.domain || {};
 bespoke.sph.domain.IntervalScheduleContainer = function () {
 
     // IntervalSchedule
-    var system = require('durandal/system'),
+    var system = require("durandal/system"),
         addIntervalSchedule = function (type) {
-            var schedule = new bespoke.sph.domain[type + 'Schedule'](system.guid());
+            var schedule = new bespoke.sph.domain[type + "Schedule"](system.guid());
             var self = this;
 
             return function () {
-                require(['viewmodels/schedule.' + type.toLowerCase(), 'durandal/app'], function (dialog, app2) {
+                require(["viewmodels/schedule." + type.toLowerCase(), "durandal/app"], function (dialog, app2) {
                     dialog.schedule(schedule);
 
                     app2.showDialog(dialog)
@@ -41,7 +42,7 @@ bespoke.sph.domain.IntervalScheduleContainer = function () {
                     pattern = /Bespoke\.Sph\.Domain\.(.*?)Schedule,/,
                     type = pattern.exec(scheduleType)[1];
 
-                require(['viewmodels/schedule.' + type.toLowerCase(), 'durandal/app'], function(dialog, app2) {
+                require(["viewmodels/schedule." + type.toLowerCase(), "durandal/app"], function(dialog, app2) {
                     dialog.schedule(clone);
 
                     app2.showDialog(dialog)
@@ -49,10 +50,12 @@ bespoke.sph.domain.IntervalScheduleContainer = function () {
                             if (!result) return;
                             if (result === "OK") {
                                 for (var g in schedule) {
-                                    if (typeof schedule[g] === "function" && schedule[g].name === "observable") {
-                                        schedule[g](ko.unwrap(clone[g]));
-                                    } else {
-                                        schedule[g] = clone[g];
+                                    if (schedule.hasOwnProperty(g)) {
+                                        if (ko.isObservable(g)) {
+                                            schedule[g](ko.unwrap(clone[g]));
+                                        } else {
+                                            schedule[g] = clone[g];
+                                        }
                                     }
                                 }
                             }
