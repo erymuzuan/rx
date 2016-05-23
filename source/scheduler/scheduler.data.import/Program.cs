22bf4e7fbeea76798dd3b5343562ceb3d7f57546
erymuzuan <erymuzuan@hotmail.com>
2016-05-23 19:20:01 +0800

    Still working on subscriber to invoke SignalR C# client
    Related Work Items: #3942

--- source/scheduler/scheduler.data.import/Program.cs
+++ source/scheduler/scheduler.data.import/Program.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
 using Humanizer;
@@ -11,31 +14,128 @@ using Newtonsoft.Json.Linq;
 
 namespace scheduler.data.import
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             var id = args.FirstOrDefault();
+            var notificationOnError = ParseArgExist("notificationOnError");
+            var notificationOnSuccess = ParseArgExist("notificationOnSuccess");
+            var truncateData = ParseArgExist("truncateData");
+            var debug = ParseArgExist("debug");
+
+            if (debug)
+            {
+                Console.WriteLine(@"Press [ENTER] to continue");
+                Console.ReadLine();
+            }
             if (null == args) return;
-            StartAsync(id).Wait();
+            StartAsync(id, notificationOnSuccess, notificationOnError, truncateData).Wait();
         }
 
-        private static async Task StartAsync(string id)
+        private static async Task StartAsync(string id, bool notificationOnSuccess, bool notificationOnError, bool truncateData)
         {
-            var username = ConfigurationManager.AppSettings["username"];
-            var password = ConfigurationManager.AppSettings["password"];
-            
-            var file = $"{ConfigurationManager.WebPath}\\App_Data\\data-imports\\{id}.json";
+            var username = ConfigurationManager.AppSettings["username"] ?? "admin";
+            var password = ConfigurationManager.AppSettings["password"] ?? "123456";
+
+            var file = $"{ConfigurationManager.SphSourceDirectory}\\{nameof(DataTransferDefinition)}\\{id}.json";
             if (!File.Exists(file)) return;
             var json = File.ReadAllText(file);
             var container = new CookieContainer();
             GetCookie(username, password, container);
-            var connection = new HubConnection(ConfigurationManager.BaseUrl) {CookieContainer = container};
+            var connection = new HubConnection(ConfigurationManager.BaseUrl) { CookieContainer = container };
 
             var hubProxy = connection.CreateHubProxy("DataImportHub");
             await connection.Start();
 
-            await hubProxy.Invoke<ProgressData>("execute", p => { }, json);
+            if (truncateData)
+                await hubProxy.Invoke("truncateData");
+            try
+            {
+                var preview = await hubProxy.Invoke<object>("preview", json);
+                Console.WriteLine(preview);
+                await hubProxy.Invoke<ProgressData>("execute", p => { }, json);
+                if (notificationOnSuccess)
+                    await NotifySuccessAsync(id);
+            }
+            catch (Exception e)
+            {
+                if (notificationOnError)
+                {
+                    await NotifyErrorAsync(e, id);
+                }
+            }
+
+        }
+
+        private static async Task NotifySuccessAsync(string id)
+        {
+            var smtp = new SmtpClient();
+            var @from = ConfigurationManager.AppSettings["EmailFrom"] ?? "data-transfer-scheduler@bespoke.com.my";
+            var to = ConfigurationManager.AppSettings["EmailTo"] ?? "admin@bespoke.com.my";
+            var subject = $"Data transfer was successful - {id}";
+            var body = $@"
+Your scheduled data transfer was successfuly executed on {DateTime.Now}";
+
+            await smtp.SendMailAsync(@from, to, subject, body);
+        }
+
+        private static async Task NotifyErrorAsync(Exception exception, string id)
+        {
+            var smtp = new SmtpClient();
+            var @from = ConfigurationManager.AppSettings["EmailFrom"] ?? "data-transfer-scheduler@bespoke.com.my";
+            var to = ConfigurationManager.AppSettings["EmailTo"] ?? "admin@bespoke.com.my";
+            var subject = $"Data transfer Error for {id}";
+            var body = GetExceptionBody(exception);
+
+            await smtp.SendMailAsync(@from, to, subject, body);
+        }
+
+        private static string GetExceptionBody(Exception exception)
+        {
+            var details = new StringBuilder();
+
+            var exc = exception;
+            var aeg = exc as AggregateException;
+            if (null != aeg)
+            {
+                foreach (var ie in aeg.InnerExceptions)
+                {
+                    details.AppendLine(" ========================== ");
+                    details.AppendLine(ie.GetType().FullName);
+                    details.AppendLine(ie.Message);
+                    details.AppendLine(ie.StackTrace);
+                    details.AppendLine();
+                    details.AppendLine();
+                }
+
+            }
+
+            var rlex = exc as ReflectionTypeLoadException;
+            if (null != rlex)
+            {
+                foreach (var lex in rlex.LoaderExceptions)
+                {
+                    details.AppendLine(" ========================== ");
+                    details.AppendLine(lex.GetType().FullName);
+                    details.AppendLine(lex.Message);
+                    details.AppendLine();
+                    details.AppendLine();
+                }
+            }
+
+            while (null != exc)
+            {
+
+                details.AppendLine(" ========================== ");
+                details.AppendLine(exc.GetType().FullName);
+                details.AppendLine(exc.Message);
+                details.AppendLine(exc.StackTrace);
+                details.AppendLine();
+                details.AppendLine();
+                exc = exc.InnerException;
+            }
+            return details.ToString();
         }
 
         private static void GetCookie(string user, string password, CookieContainer container)
@@ -45,8 +145,8 @@ namespace scheduler.data.import
             request.ContentType = "application/x-www-form-urlencoded";
             request.CookieContainer = container;
 
-            var authCredentials = "UserName=" + user + "&Password=" + password;
-            var bytes = System.Text.Encoding.UTF8.GetBytes(authCredentials);
+            var authCredentials = $"UserName={user}&Password={password}&ReturnUrl=&submit=";
+            var bytes = Encoding.UTF8.GetBytes(authCredentials);
             request.ContentLength = bytes.Length;
             using (var requestStream = request.GetRequestStream())
             {
@@ -58,6 +158,21 @@ namespace scheduler.data.import
                 Console.WriteLine(response.StatusCode);
             }
 
+        }
+
+
+        public static string ParseArg(string name)
+        {
+            var args = Environment.CommandLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var val = args.SingleOrDefault(a => a.StartsWith("/" + name + ":"));
+            return val?.Replace("/" + name + ":", string.Empty);
+        }
+
+        private static bool ParseArgExist(string name)
+        {
+            var args = Environment.CommandLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var val = args.SingleOrDefault(a => a.StartsWith("/" + name));
+            return null != val;
         }
     }
     public class ProgressData
