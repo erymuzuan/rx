4d59ec97ce03b2a080ef1af7cae913e601d9087a
erymuzuan <erymuzuan@hotmail.com>
2016-05-25 15:53:28 +0800

    #3942 Changing icon for data transfer to fa-download

--- source/web/core.sph/Solutions/DataTransferDefinitionProviders.cs
+++ source/web/core.sph/Solutions/DataTransferDefinitionProviders.cs
@@ -6,7 +6,7 @@ namespace Bespoke.Sph.Web.Solutions
     [Export(typeof(IItemsProvider))]
     public class DataTransferDefinitionProviders : SourceAssetProviders<DataTransferDefinition>
     {
-        protected override string Icon => "fa fa-database";
+        protected override string Icon => "fa fa-download";
         protected override string GetIcon(DataTransferDefinition d) => this.Icon;
         protected override string GetEditUrl(DataTransferDefinition d) => $"data.import/{d.Id}";
         protected override string GetName(DataTransferDefinition d) => d.Name;
