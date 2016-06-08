c2789aa332d31580a84a70526fbfafe4b2d7d3de
erymuzuan <erymuzuan@hotmail.com>
2016-06-08 08:02:11 +0800

    Rowversion column is the Timestamp, and you cannot write explicitly
    Related Work Items: #4001

---
+++ source/adapters/sqlserver.adapter/Columns/RowVersionColumn.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.Composition;
+using System.Data;
+
+namespace Bespoke.Sph.Integrations.Adapters.Columns
+{
+    [Export("SqlColumn", typeof(SqlColumn))]
+    [ColumnGeneratorMetadata(IncludeTypes = new[] { SqlDbType.Timestamp }, IsNullable = ThreeWayBoolean.False)]
+    public class RowVersionColumn : SqlColumn
+    {
+        public override bool CanWrite => false;
+        public override Type ClrType => typeof(byte[]);
+        public override string GenerateUpdateParameterValue(string commandName = "cmd")
+        {
+            return null;
+        }
+    }
+}
