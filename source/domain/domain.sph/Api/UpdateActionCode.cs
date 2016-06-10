0d9c2078a2154b808a53508e98a566fee3ad0ebc
erymuzuan <erymuzuan@hotmail.com>
2016-06-10 16:12:57 +0800

    Add eTag If-Match and If-Unmodified-Since header value to detect if exisitng item is out of date
    Related Work Items: #4003

--- source/domain/domain.sph/Api/UpdateActionCode.cs
+++ source/domain/domain.sph/Api/UpdateActionCode.cs
@@ -13,9 +13,16 @@ namespace Bespoke.Sph.Domain.Api
 
             var pks = table.MemberCollection.Where(m => table.PrimaryKeyCollection.Contains(m.Name)).ToArray();
             var routeConstraint = pks.Select(m => "{" + m.Name.ToCamelCase() + this.GetRouteConstraint(m) + "}");
-            var arguments = pks.Select(m => m.GenerateParameterCode());
+            var arguments = pks.Select(m => m.GenerateParameterCode()).ToList();
             var parameters = pks.Select(m => m.Name.ToCamelCase());
 
+            var version = !string.IsNullOrWhiteSpace(table.VersionColumn);
+            var unmodifiedSince = !string.IsNullOrWhiteSpace(table.ModifiedDateColumn);
+            if (version)
+                arguments.Add("[IfMatch]ETag etag");
+
+            if (unmodifiedSince)
+                arguments.Add("[UnmodifiedSince]UnmodifiedSinceHeader unmodifiedSince");
 
             var code = new StringBuilder();
 
@@ -34,6 +41,27 @@ namespace Bespoke.Sph.Domain.Api
 
             if(null == exist)
                 return NotFound();");
+
+            if (version)
+            {
+                code.Append(
+       $@"               
+                if (!etag.IsMatch(exist.{table.VersionColumn}.TimeStampToString()))
+                {{
+                    return Invalid((HttpStatusCode)428, new {{ message =""This request is required to be conditional;try using 'If-Match', or may be your resource is out of date""}});
+                }}
+                ");
+            }
+            if (unmodifiedSince)
+            {
+                code.Append(
+       $@"               
+                if (!unmodifiedSince.IsMatch(exist.{table.ModifiedDateColumn}))
+                {{
+                    return Invalid((HttpStatusCode)428, new {{ message =""This request is required to be conditional;try using 'If-Unmodified-Since', or may be your resource is out of date""}});
+                }}
+                ");
+            }
 
             foreach (var pk in table.PrimaryKeyCollection)
             {
