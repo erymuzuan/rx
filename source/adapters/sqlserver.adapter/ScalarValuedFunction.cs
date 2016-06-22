8399176378c0b50920b8507e77a149859b099a4f
erymuzuan <erymuzuan@hotmail.com>
2016-06-22 09:55:45 +0800

    Rename Scalar func, from FuncOperationDefinition to ScalarValuedFunction
    Related Work Items: #4060

--- source/adapters/sqlserver.adapter/ScalarValuedFunction.cs
+++ source/adapters/sqlserver.adapter/ScalarValuedFunction.cs
@@ -5,7 +5,7 @@ using Bespoke.Sph.Domain.Api;
 
 namespace Bespoke.Sph.Integrations.Adapters
 {
-    public class FuncOperationDefinition : OperationDefinition
+    public class ScalarValuedFunction : OperationDefinition
     {
         protected override string GenerateAdapterActionBody(Adapter adapter)
         {
