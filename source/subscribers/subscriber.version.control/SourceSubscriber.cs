290162e7f30c84998694000976131b17bc335f7a
erymuzuan <erymuzuan@hotmail.com>
2016-06-29 14:46:41 +0800

    Files were deleted when updating this happened in SourceSubscriber
    Related Work Items: #4058

--- source/subscribers/subscriber.version.control/SourceSubscriber.cs
+++ source/subscribers/subscriber.version.control/SourceSubscriber.cs
@@ -1,9 +1,6 @@
-﻿using System.IO;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
 using Bespoke.Sph.SubscribersInfrastructure;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace subscriber.version.control
 {
@@ -17,52 +14,10 @@ namespace subscriber.version.control
             "#.changed.#",
             "#.deleted.#"
         };
-
-        private void RemoveExistingSource(Entity item)
-        {
-            if (null == item) return;
-            
-            var wc = ConfigurationManager.SphSourceDirectory;
-            var type = item.GetEntityType();
-            var folder = Path.Combine(wc, type.Name);
-            if (!Directory.Exists(folder))
-                return;
-
-            var files = Directory.GetFiles(folder, "*.json");
-            foreach (var f in files)
-            {
-                var text = File.ReadAllText(f);
-                if (string.IsNullOrWhiteSpace(text))
-                {
-                    File.Delete(f);
-                    continue;
-                }
-                try
-                {
-                    var o = JObject.Parse(text);
-                    var idToken = o.SelectToken("$.Id");
-                    if (null == idToken)
-                    {
-                        this.WriteMessage("[Id] field cannot be found in in {0}", f);
-                        continue;
-                    }
-                    var id = idToken.Value<string>();
-                    if (id != item.Id) continue;
-                    File.Delete(f);
-                    return;
-                }
-                catch (JsonReaderException e)
-                {
-                    this.NotificicationService.Write($"There is an error in {f}");
-                    this.NotificicationService.WriteError(e, $"there is an error in {f}");
-                }
-            }
-        }
-
+        
         protected override async Task ProcessMessage(Entity item, MessageHeaders header)
         {
             if (null == item) return;
-            RemoveExistingSource(item);
 
             var type = item.GetEntityType();
 
