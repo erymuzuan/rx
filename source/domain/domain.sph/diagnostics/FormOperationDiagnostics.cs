09d72367ee43a2a280242bf9e551ab5422a5f035
erymuzuan <erymuzuan@hotmail.com>
2016-08-23 06:38:03 +0800

    Add diagnostics to verify cehcek if the API operation and DELETE operation is set in EntityForm
    Related Work Items: #4632

---
+++ source/domain/domain.sph/diagnostics/FormOperationDiagnostics.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+
+namespace Bespoke.Sph.Domain.diagnostics
+{
+    [Export(typeof(IBuildDiagnostics))]
+    public class FormOperationDiagnostics : BuilDiagnostic
+    {
+        public override Task<BuildError[]> ValidateErrorsAsync(EntityForm form, EntityDefinition entity)
+        {
+            var errors = new List<BuildError>();
+            if (string.IsNullOrWhiteSpace(form.Operation))
+                errors.Add(new BuildError(form.Id, "No API operation is selected"));
+            if (string.IsNullOrWhiteSpace(form.DeleteOperationSuccessMesage) && string.IsNullOrWhiteSpace(form.OperationSuccessNavigateUrl))
+                errors.Add(new BuildError(form.Id, "API operation do not specify success message or navigate Uri"));
+            return Task.FromResult(errors.ToArray());
+        }
+    }
+}
