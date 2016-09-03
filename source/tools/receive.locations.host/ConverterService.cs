c8d98de3d89ca1985e6dbf68e32422e73a27e658
erymuzuan <erymuzuan@hotmail.com>
2016-09-03 18:22:20 +0800

    Receice location host

---
+++ source/tools/receive.locations.host/ConverterService.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+
+namespace receive.locations.host
+{
+    public interface IReceiveLocation
+    {
+        bool Start();
+        bool Stop();
+        void Pause();
+        void Resume();
+    }
+
+    public class ConverterService : IReceiveLocation
+    {
+        private FileSystemWatcher m_watcher;
+        private bool m_paused;
+
+        public bool Start()
+        {
+            m_watcher = new FileSystemWatcher(@"c:\temp\a", "*_in.txt");
+            m_watcher.Created += FileCreated;
+            m_watcher.IncludeSubdirectories = false;
+            m_watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            if (m_paused) return;
+            var content = File.ReadAllText(e.FullPath);
+            var upperContent = content.ToUpperInvariant();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertedFileName = Path.GetFileName(e.FullPath) + ".converted";
+            var convertedPath = Path.Combine(dir, convertedFileName);
+            File.WriteAllText(convertedPath, upperContent);
+        }
+
+        public bool Stop()
+        {
+            m_watcher.Dispose();
+            return true;
+        }
+
+        public void Pause()
+        {
+            m_paused = true;
+        }
+
+        public void Resume()
+        {
+            m_paused = false;
+        }
+    }
+}
---
+++ source/tools/receive.locations.host/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Topshelf;
+
+namespace receive.locations.host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(config =>
+            {
+                config.Service<IReceiveLocation>(svc =>
+                {
+                    svc.ConstructUsing(() =>
+                    {
+                        dynamic loc = new ConverterService();
+                        return (IReceiveLocation)loc;
+                    });
+
+                    svc.WhenStarted(x => x.Start());
+                    svc.WhenStopped(x => x.Stop());
+                    svc.WhenPaused(x => x.Pause());
+                    svc.WhenShutdown(x => x.Stop());
+                    svc.WhenCustomCommandReceived((x, g, i) =>
+                    {
+                        Console.WriteLine(g);
+                        Console.WriteLine(i);
+                    });
+                });
+                config.SetServiceName("RxReceiveLocationHost");
+                config.SetDisplayName("Rx Receive location host service");
+                config.SetDescription("Rx Developer receive location default host");
+
+                config.StartAutomatically();
+            });
+        }
+    }
+}
