1c26b44208cab585547900d95213b9f57633b1e4
erymuzuan <erymuzuan@hotmail.com>
2016-09-06 07:35:04 +0800

    Less gulp taks

--- gulp-tasks/less.js
+++ gulp-tasks/less.js
@@ -1,25 +1,26 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var path = require('path');
-var sourcemaps = require('gulp-sourcemaps');
 
-var concat = require('gulp-concat');
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var sourcemaps = require("gulp-sourcemaps");
 
-var useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-minify-css'),
-    sources =['./source/web/core.sph/Content/theme.matyie/*.less'];
+var concat = require("gulp-concat");
+
+var useref = require("gulp-useref"),
+    gulpif = require("gulp-if"),
+    uglify = require("gulp-uglify"),
+    minifyCss = require("gulp-minify-css"),
+    sources =["./source/web/core.sph/Content/theme.matyie/*.less"];
       
-watchList.push({name:'less',sources: sources, tasks : ['less']});
+watchList.push({name:"less",sources: sources, tasks : ["less"]});
 
-gulp.task('less', function () {
+gulp.task("less", function () {
   gulp.src(sources)
     .pipe(sourcemaps.init())
     .pipe(less({
-      //paths: [ path.join(__dirname, 'less', 'includes') ]
+     // paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
-    //.pipe(sourcemaps.write())
-    //.pipe(gulp.dest('./source/web/core.sph/Content/theme.matyie/'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./source/web/core.sph/Content/theme.matyie/"))
     ;
 });
