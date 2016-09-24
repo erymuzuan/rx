44da2b3cfb28ca0514b3402c5fd9854956fe79c5
erymuzuan <erymuzuan@hotmail.com>
2016-09-24 17:21:29 +0800

    More filtering for event logs

--- source/web/core.sph/SphApp/viewmodels/event.logs.js
+++ source/web/core.sph/SphApp/viewmodels/event.logs.js
@@ -10,10 +10,14 @@
 
 
 define(["services/datacontext", "services/logger", "plugins/router"],
-    function (context, logger, router) {
-        var list = ko.observableArray(),
+    function (context) {
+        const list = ko.observableArray(),
             severityOptions = ko.observableArray(),
             selectedSeverities = ko.observableArray(),
+            sourceOptions = ko.observableArray(),
+            selectedSources= ko.observableArray(),
+            logOptions = ko.observableArray(),
+            selectedLogs= ko.observableArray(),
             logId = ko.observable(),
             selectedComputers = ko.observableArray(),
             timeFrom = ko.observable(moment().subtract(7, "days").format()),
@@ -53,7 +57,7 @@ define(["services/datacontext", "services/logger", "plugins/router"],
                 return tcs.promise();
             },
             searchById = function () {
-                return $.getJSON("/management-api/logs/" + ko.unwrap(logId))
+                return $.getJSON(`/management-api/logs/${ko.unwrap(logId)}`)
                     .done(function (r) {
                         openDetails(r);
                     });
@@ -61,6 +65,8 @@ define(["services/datacontext", "services/logger", "plugins/router"],
             activate = function () {
 
                 getKeysAsync("severity").done(severityOptions);
+                getKeysAsync("source").done(sourceOptions);
+                getKeysAsync("log").done(logOptions);
 
                 return getKeysAsync("computer")
                     .then(computerOptions);
@@ -81,7 +87,7 @@ define(["services/datacontext", "services/logger", "plugins/router"],
                 ]
             }),
             executeQuery = function () {
-                var q = {
+                const q = {
                     "query": {
                         "bool": {
                             "must": []
@@ -94,24 +100,23 @@ define(["services/datacontext", "services/logger", "plugins/router"],
                          }
                      }
                     ]
-                };
+                },
+                    pushTerms =function(term, options) {
+                        const selecteditems = ko.unwrap(options),
+                            qt = {
+                                "terms": {
+                                }
+                            };
+                        qt.terms[term] = selecteditems;
+                        if (selecteditems.length > 0) {
+                            q.query.bool.must.push(qt);
+                        }
+                    };
+                pushTerms("computer", selectedComputers);
+                pushTerms("severity", selectedSeverities);
+                pushTerms("source", selectedSources);
+                pushTerms("log", selectedLogs);
 
-                var computers = ko.unwrap(selectedComputers);
-                if (computers.length > 0) {
-                    q.query.bool.must.push({
-                        "terms": {
-                            "computer": computers
-                        }
-                    });
-                }
-                var severities = ko.unwrap(selectedSeverities);
-                if (severities.length > 0) {
-                    q.query.bool.must.push({
-                        "terms": {
-                            "severity": severities
-                        }
-                    });
-                }
                 if (ko.unwrap(timeFrom) && ko.unwrap(timeTo)) {
                     q.query.bool.must.push({
                         "range": {
@@ -123,24 +128,29 @@ define(["services/datacontext", "services/logger", "plugins/router"],
                     });
                 }
 
-
                 query(q);
             };
 
         selectedComputers.subscribe(executeQuery, null, "arrayChange");
         selectedSeverities.subscribe(executeQuery, null, "arrayChange");
+        selectedSources.subscribe(executeQuery, null, "arrayChange");
+        selectedLogs.subscribe(executeQuery, null, "arrayChange");
         timeFrom.subscribe(executeQuery);
         timeTo.subscribe(executeQuery);
 
 
-        var vm = {
+        const vm = {
             logId: logId,
             searchById: searchById,
             openDetails: openDetails,
             computerOptions: computerOptions,
             severityOptions: severityOptions,
+            sourceOptions: sourceOptions,
             selectedComputers: selectedComputers,
             selectedSeverities: selectedSeverities,
+            selectedSources: selectedSources,
+            logOptions: logOptions,
+            selectedLogs: selectedLogs,
             timeFrom: timeFrom,
             timeTo: timeTo,
             query: query,
