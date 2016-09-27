2a67f4765cf79a74db6d797cb40a6be0badf725c
erymuzuan <erymuzuan@hotmail.com>
2016-09-27 09:41:05 +0800

    Product lookup

---
+++ source/web/web.sph/SphApp/viewmodels/new-product.js
@@ -0,0 +1,156 @@
+define([objectbuilders.datacontext, objectbuilders.logger, objectbuilders.router,
+objectbuilders.system, objectbuilders.validation, objectbuilders.eximp,
+objectbuilders.dialog, objectbuilders.watcher, objectbuilders.config,
+objectbuilders.app],
+
+function(context, logger, router, system, validation, eximp, dialog, watcher, config, app) {
+
+    var entity = ko.observable(new bespoke.DevV1_product.domain.Product(system.guid())),
+        errors = ko.observableArray(),
+        form = ko.observable(new bespoke.sph.domain.EntityForm()),
+        watching = ko.observable(false),
+        id = ko.observable(),
+        partial = partial || {},
+        i18n = null,
+        headers = {},
+        activate = function(entityId) {
+            id(entityId);
+            var tcs = new $.Deferred();
+            context.loadOneAsync("EntityForm", "Route eq 'new-product'")
+                .then(function(f) {
+                form(f);
+                return watcher.getIsWatchingAsync("Product", entityId);
+            })
+                .then(function(w) {
+                watching(w);
+                return $.getJSON("i18n/" + config.lang + "/new-product");
+            })
+                .then(function(n) {
+                i18n = n[0];
+                if (!entityId || entityId === "0") {
+                    return Task.fromResult({
+                        WebId: system.guid()
+                    });
+                }
+                return context.get("/api/products/" + entityId);
+            }).then(function(b, textStatus, xhr) {
+
+                if (xhr) {
+                    var etag = xhr.getResponseHeader("ETag"),
+                        lastModified = xhr.getResponseHeader("Last-Modified");
+                    if (etag) {
+                        headers["If-Match"] = etag;
+                    }
+                    if (lastModified) {
+                        headers["If-Modified-Since"] = lastModified;
+                    }
+                }
+                entity(new bespoke.DevV1_product.domain.Product(b[0] || b));
+            }, function(e) {
+                if (e.status == 404) {
+                    app.showMessage("Sorry, but we cannot find any Product with location : " + "/api/products/" + entityId, "Engineering Team Development", ["OK"]);
+                }
+            }).always(function() {
+                if (typeof partial.activate === "function") {
+                    partial.activate(ko.unwrap(entity))
+                        .done(tcs.resolve)
+                        .fail(tcs.reject);
+                } else {
+                    tcs.resolve(true);
+                }
+            });
+            return tcs.promise();
+
+        },
+
+        defaultCommand = function() {
+
+            if (!validation.valid()) {
+                return Task.fromResult(false);
+            }
+
+            var data = ko.mapping.toJSON(entity),
+                tcs = new $.Deferred();
+
+            context.post(data, "/api/products/", headers)
+                .fail(function(response) {
+                var result = response.responseJSON;
+                errors.removeAll();
+                if (response.status === 428) {
+                    // out of date conflict
+                    logger.error(result.message);
+                }
+                if (response.status === 422 && _(result.rules).isArray()) {
+                    _(result.rules).each(function(v) {
+                        errors(v.ValidationErrors);
+                    });
+                }
+                logger.error("There are errors in your entity, !!!");
+                tcs.resolve(false);
+            })
+                .then(function(result) {
+                logger.info(result.message);
+                entity().Id(result.id);
+                errors.removeAll();
+                tcs.resolve(result);
+            });
+            return tcs.promise();
+        },
+        attached = function(view) {
+            // validation
+            validation.init($('#new-product-form'), form());
+
+            if (typeof partial.attached === "function") {
+                partial.attached(view);
+            }
+
+        },
+
+        compositionComplete = function() {
+            $("[data-i18n]").each(function(i, v) {
+                var $label = $(v),
+                    text = $label.data("i18n");
+                if (i18n && typeof i18n[text] === "string") {
+                    $label.text(i18n[text]);
+                }
+            });
+        },
+        saveCommand = function() {
+            return defaultCommand()
+                .then(function(result) {
+                if (result.success) {
+                    return app.showMessage("Your product has been added", ["OK"]);
+                } else {
+                    return Task.fromResult(false);
+                }
+            })
+                .then(function(result) {
+                if (result) {
+                    router.navigate("product-all");
+                }
+            });
+        };
+    var vm = {
+        partial: partial,
+        activate: activate,
+        config: config,
+        attached: attached,
+        compositionComplete: compositionComplete,
+        entity: entity,
+        errors: errors,
+        toolbar: {
+            saveCommand: saveCommand,
+            canExecuteSaveCommand: ko.computed(function() {
+                if (typeof partial.canExecuteSaveCommand === "function") {
+                    return partial.canExecuteSaveCommand();
+                }
+                return true;
+            }),
+
+        }, // end toolbar
+
+        commands: ko.observableArray([])
+    };
+
+    return vm;
+});
---
+++ source/web/web.sph/SphApp/viewmodels/product-all.js
@@ -0,0 +1,43 @@
+define(["services/datacontext", "services/logger", "plugins/router", "services/chart", objectbuilders.config, "services/_ko.list"],
+
+function(context, logger, router, chart, config, koList) {
+
+    var isBusy = ko.observable(false),
+        query = "/api/products/",
+        partial = partial || {},
+        list = ko.observableArray([]),
+        map = function(v) {
+            if (typeof partial.map === "function") {
+                return partial.map(v);
+            }
+            return v;
+        },
+        activate = function() {
+            if (typeof partial.activate === "function") {
+                return partial.activate(list);
+            }
+            return true;
+        },
+        attached = function(view) {
+            if (typeof partial.attached === "function") {
+                partial.attached(view);
+            }
+        };
+
+    var vm = {
+        query: query,
+        config: config,
+        isBusy: isBusy,
+        map: map,
+        activate: activate,
+        attached: attached,
+        list: list,
+        toolbar: {
+            addNewCommand : function(){ router.navigate("new-product/0");},
+            commands: ko.observableArray([])
+        }
+    };
+
+    return vm;
+
+});
