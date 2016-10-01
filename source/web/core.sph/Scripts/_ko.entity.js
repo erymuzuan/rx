c5d1ab3b5e886f1e46b532a82dde2a07a8192534
erymuzuan <erymuzuan@hotmail.com>
2016-10-01 08:43:21 +0800

    ES6

--- source/web/core.sph/Scripts/_ko.entity.js
+++ source/web/core.sph/Scripts/_ko.entity.js
@@ -33,7 +33,7 @@ ko.bindingHandlers.tree = {
             recurseChildMember = function (node) {
                 node.children = _(node.data.MemberCollection()).map(function (v) {
 
-                    var type = getNodeMemberType(v);
+                    const type = getNodeMemberType(v);
                     return {
                         text: v.Name(),
                         state: "open",
