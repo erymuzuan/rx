6722e15cdb09a2c1276f2c66f207c883f3a77cbf
erymuzuan <erymuzuan@hotmail.com>
2016-10-14 11:25:43 +0800

    Deployment

---
+++ deploy-update.psq.ps1
@@ -0,0 +1,14 @@
+﻿param
+(
+    [int]$Build,
+    [string]$WebServer = "S302"
+    
+)
+
+$WebServerSession = New-PSSession -ComputerName $WebServer
+
+Invoke-Command -ComputerName $WebServer -ScriptBlock { mkdir "c:\apps\rx\web\binaries\$using:Build\"}
+
+Copy-Item .\$Build.7z -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
+Copy-Item  .\deployment\10325.* -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
+Copy-Item  .\deployment\version.$Build.* -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
