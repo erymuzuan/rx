63879e031d72ac1826bab53f6a8142eaba7796e0
erymuzuan <erymuzuan@hotmail.com>
2016-10-20 10:12:17 +0800

    Deploying update package on ken 2

---
+++ deploy-update-package.ken2.ps1
@@ -0,0 +1,22 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [int]$Build,
+    [string]$WebServer = "S302"
+    
+)
+
+$WebServerSession = New-PSSession -ComputerName $WebServer
+
+Invoke-Command -ComputerName $WebServer -ScriptBlock `
+{
+    if((Test-Path("c:\apps\rx\web\binaries\$using:Build\")) -eq $false)
+    {        
+        mkdir "c:\apps\rx\web\binaries\$using:Build\"
+    }
+     
+}
+
+Copy-Item .\$Build.7z -Force -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
+Copy-Item  .\deployment\10325.* -Force -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
+Copy-Item  .\deployment\version.$Build.* -Force -ToSession $WebServerSession c:\apps\rx\web\binaries\$Build\
