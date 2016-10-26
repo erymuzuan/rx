faddfc6ca7d7c50572d143932cc0b7a9bd7f5006
erymuzuan <erymuzuan@hotmail.com>
2016-10-26 10:54:32 +0800

    New deploy script to alpha.rxdeveloper.com on ken2

---
+++ Deploy.Update.Package.Ken2.ps1
@@ -0,0 +1,26 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [int]$Build,
+    [string]$WebServer = "S314",
+    [pscredential]$Admin = (Get-Credential 'ORG\Administrator')
+    
+)
+
+$WebServerSession = New-PSSession -ComputerName $WebServer -Credential $Admin
+
+Invoke-Command -Session $WebServerSession -ScriptBlock `
+{
+    if((Test-Path("c:\apps\rxwebsite\binaries\$using:Build\")) -eq $false)
+    {        
+        mkdir "c:\apps\rxwebsite\binaries\$using:Build\"
+    }
+     
+}
+$current = $Build -1 ;
+
+Copy-Item .\$Build.7z -Force -ToSession $WebServerSession c:\apps\rxwebsite\binaries\$Build\
+Copy-Item  .\deployment\$Build.* -Force -ToSession $WebServerSession c:\apps\rxwebsite\binaries\$Build\
+Copy-Item  .\deployment\version.$Build.* -Force -ToSession $WebServerSession c:\apps\rxwebsite\binaries\$Build\
+Copy-Item  .\deployment\$current.json -Force -ToSession $WebServerSession c:\apps\rxwebsite\binaries\$current.json
+Copy-Item  .\deployment\$current.ps1 -Force -ToSession $WebServerSession c:\apps\rxwebsite\binaries\$current.ps1
