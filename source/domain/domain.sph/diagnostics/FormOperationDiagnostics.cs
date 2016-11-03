eba63b801799d319d657dcb858d21d187f5d3163
erymuzuan <erymuzuan@hotmail.com>
2016-11-03 14:06:15 +0800

    Neither operation is mandatory

--- source/domain/domain.sph/diagnostics/FormOperationDiagnostics.cs
+++ source/domain/domain.sph/diagnostics/FormOperationDiagnostics.cs
@@ -14,10 +14,11 @@ namespace Bespoke.Sph.Domain.diagnostics
             if (!string.IsNullOrWhiteSpace(form.DeleteOperation))
                 return Task.FromResult(errors.ToArray());
 
-            if (string.IsNullOrWhiteSpace(form.Operation))
-                errors.Add(new BuildError(form.Id, "No API operation is selected"));
-            if (string.IsNullOrWhiteSpace(form.OperationSuccessMesage) && string.IsNullOrWhiteSpace(form.OperationSuccessNavigateUrl))
-                errors.Add(new BuildError(form.Id, "API operation do not specify success message or navigate Uri"));
+            if (!string.IsNullOrWhiteSpace(form.Operation))
+            {
+                if (string.IsNullOrWhiteSpace(form.OperationSuccessMesage) && string.IsNullOrWhiteSpace(form.OperationSuccessNavigateUrl))
+                    errors.Add(new BuildError(form.Id, "API operation do not specify success message or navigate Uri"));
+            }
             return Task.FromResult(errors.ToArray());
         }
     }
