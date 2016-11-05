e028e983b33e6924415ada6181384844bbd60e35
erymuzuan <erymuzuan@hotmail.com>
2016-11-06 07:16:01 +0800

    Read the pipeline

--- source/tools/sqlcmd/GetProcessVariable.cs
+++ source/tools/sqlcmd/GetProcessVariable.cs
@@ -7,19 +7,22 @@ namespace Bespoke.Sph.Powershells
     [Cmdlet(VerbsCommon.Get, "ProcessVariable")]
     public class GetProcessVariable : PSCmdlet
     {
-        [Parameter(HelpMessage = "Process name", Mandatory = false)]
+        [Parameter(Position = 0,ValueFromPipeline = true,ValueFromPipelineByPropertyName = true)]
         [ValidateNotNullOrEmpty]
-        public string Name { get; set; } = "controlcenter";
-
-
-
+        public string[] Name { set; get; }
+        
         [Parameter(HelpMessage = "Proces name", Mandatory = true)]
         [ValidateNotNullOrEmpty]
         public string Variable { get; set; }
 
         protected override void ProcessRecord()
         {
-            var processes = Process.GetProcessesByName(this.Name);
+            if (this.Name.Length != 1)
+            {
+                WriteError(new ErrorRecord(new Exception("You could only supply one process"), "0", ErrorCategory.FromStdErr, this.Name));
+                return;
+            }
+            var processes = Process.GetProcessesByName(this.Name[0]);
 
             if (processes.Length == 0)
                 WriteError(new ErrorRecord(new Exception("Process with a given name not found. Please modify the Name"), "0", ErrorCategory.FromStdErr, this.Name));
