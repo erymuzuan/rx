2a7eaa5852bb81efc1e1127a7d2842e7455e6706
erymuzuan <erymuzuan@hotmail.com>
2017-07-03 10:43:20 +0800

    Action exection code
    Related Work Items: #6050

--- source/domain/trigger.action.entity/EntityAction.cs
+++ source/domain/trigger.action.entity/EntityAction.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.ComponentModel.Composition;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
-using Bespoke.Sph.Domain.Api;
 
 namespace Bespoke.Sph.Messaging
 {
@@ -13,105 +11,37 @@ namespace Bespoke.Sph.Messaging
     public partial class EntityAction : CustomAction
     {
 
-        public override async Task ExecuteAsync(RuleContext context)
+        public override async Task ExecuteAsync(RuleContext rc)
         {
-            if (string.IsNullOrWhiteSpace(this.OutboundEntity) && !string.IsNullOrWhiteSpace(this.OutboundMap))
+            // map
+            dynamic map = Activator.CreateInstance(this.OutboundMapType);
+            var item = await map.TransformAsync(rc.Item);
+
+            var ctx = new SphDataContext();
+            using (var session = ctx.OpenSession())
             {
-                dynamic adapter = Activator.CreateInstance(this.AdapterType);
-                Console.WriteLine(adapter);
-
+                session.Attach(item);
+                await session.SubmitChanges(this.Title);
             }
-            if (!string.IsNullOrWhiteSpace(this.Table) && !string.IsNullOrWhiteSpace(this.Crud))
-            {
-                var dll = this.AdapterType.Assembly;
-                var ttname = this.AdapterType.Namespace + "." + this.Table + "Adapter";
-                var tt = dll.GetType(ttname, true);
-                if (null == tt) throw new InvalidOperationException(this.AdapterType.Namespace + "." + this.Table);
-                dynamic table = Activator.CreateInstance(tt);
-
-                // map
-                dynamic map = Activator.CreateInstance(this.OutboundMapType);
-                var item = await map.TransformAsync(context.Item);
-                await table.InsertAsync(item);
-
-            }
-
 
         }
 
         public override string GeneratorCode()
         {
             var code = new StringBuilder();
-
             var context = new SphDataContext();
             var map = context.LoadOne<TransformDefinition>(x => x.Id == this.OutboundMap);
-            var adapter = context.LoadOne<Adapter>(x => x.Name == this.Adapter);
-            var rt = new ErrorRetry();
-            var hasRetry = this.Retry.HasValue;
-            if (hasRetry)
-            {
-                rt = new ErrorRetry
-                {
-                    Algorithm = this.RetryAlgorithm,
-                    WebId = this.WebId,
-                    Attempt = this.Retry.Value,
-                    Wait = Convert.ToInt32(this.RetryIntervalTimeSpan * this.RetryInterval),
-                    IsEnabled = true
-                };
-            }
 
             code.AppendLine($"var map = new {map.CodeNamespace}.{map.ClassName}();");
-            if (hasRetry)
-            {
-                code.AppendLine($@" var sourceRetry = await Policy.Handle<Exception>()
-                                                    .WaitAndRetryAsync({rt.GenerateWaitCode()})
-                                                    .ExecuteAndCaptureAsync(async () => await map.TransformAsync(item));");
-                code.AppendLine("if(null != sourceRetry.FinalException)");
-                code.AppendLine("    throw sourceRetry.FinalException;");
-                code.AppendLine("var source = sourceRetry.Result;");
-            }
-            else
-                code.AppendLine("var source = await map.TransformAsync(item);");
-            var useOperation = string.IsNullOrWhiteSpace(this.Table) && !string.IsNullOrWhiteSpace(this.Operation);
-            var useTable = !string.IsNullOrWhiteSpace(this.Table) && !string.IsNullOrWhiteSpace(this.Crud);
-
-
-            if (useOperation)
-            {
-                var op = adapter.OperationDefinitionCollection.Single(x => x.Name == this.Operation);
-                code.AppendLine($"var adapter = new {AdapterType.FullName}();");
-                if (hasRetry)
-                {
-                    code.AppendLine($@" var pr = await Policy.Handle<Exception>()
-                                                    .WaitAndRetryAsync({rt.GenerateWaitCode()})
-                                                    .ExecuteAndCaptureAsync(async () => await adapter.{op.MethodName}Async(source));");
-                    code.AppendLine("if(null != pr.FinalException)");
-                    code.AppendLine("    throw pr.FinalException;");
-                    code.AppendLine("var response = pr.Result;");
-                }
-                else
-                    code.AppendLine($"var response = await adapter.{op.MethodName}Async(source);");
-
-            }
-            if (useTable)
-            {
-                var table = adapter.TableDefinitionCollection.Single(x => x.Name == this.Table);
-                code.AppendLine($"var adapter = new {adapter.CodeNamespace}.{table.ClrName}Adapter();");
-                if (hasRetry)
-                {
-                    code.AppendLine($@" var pr = await Policy.Handle<Exception>()
-                                                    .WaitAndRetryAsync({rt.GenerateWaitCode()})
-                                                    .ExecuteAndCaptureAsync(async () => await adapter.{Crud}Async(source));");
-                    code.AppendLine("if(null != pr.FinalException)");
-                    code.AppendLine("    throw pr.FinalException;");
-                    code.AppendLine("var response = pr.Result;");
-                }
-                else
-                {
-                    code.AppendLine($"var response = await adapter.{Crud}Async(source);");
-                }
-            }
-            code.AppendLine("return response;");
+            code.AppendLine("var source = await map.TransformAsync(item);");
+            code.AppendLine($@"  
+                var ctx = new SphDataContext();
+                using (var session = ctx.OpenSession())
+                {{
+                    session.Attach(source);
+                    await session.SubmitChanges(""{this.Title}"");
+                }}");
+            code.AppendLine($@"return source;");
             return code.ToString();
         }
 
--- source/domain/trigger.action.entity/EntityAction.p.cs
+++ source/domain/trigger.action.entity/EntityAction.p.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Xml.Serialization;
 using Bespoke.Sph.Domain;
+using Bespoke.Sph.Domain.Api;
 using Newtonsoft.Json;
 
 namespace Bespoke.Sph.Messaging
@@ -22,6 +23,27 @@ namespace Bespoke.Sph.Messaging
             set
             {
                 this.OutboundMap = value.GetShortAssemblyQualifiedName();
+                RaisePropertyChanged();
+            }
+        }
+        [XmlIgnore]
+        [JsonIgnore]
+        public Type EntityType
+        {
+            get
+            {
+                var type = Strings.GetType(this.OutboundEntity);
+                if (null == type)
+                {
+                    var context = new SphDataContext();
+                    var adapter = context.LoadOneFromSources<EntityDefinition>(x => x.Name == this.OutboundEntity || x.Id == this.OutboundEntity);
+                    type = Strings.GetType(adapter.FullTypeName);
+                }
+                return type;
+            }
+            set
+            {
+                this.OutboundEntity = value.GetShortAssemblyQualifiedName();
                 RaisePropertyChanged();
             }
         }
