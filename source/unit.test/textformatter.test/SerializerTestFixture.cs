2adc474bcd63e853e2a8fcc00991a204d329d3d3
erymuzuan <erymuzuan@hotmail.com>
2017-08-07 07:47:19 +0800

    missing default ctor, causing Json.net to use null for the arg in DelimitedTextSimpleMember ctor
    Related Work Items: #6192

---
+++ source/unit.test/textformatter.test/SerializerTestFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace textformatter.test
+{
+    [Trait("Serializer", "DelimitedText")]
+    public class SerializerTestFixture
+    {
+        private readonly ITestOutputHelper m_helper;
+
+        public SerializerTestFixture(ITestOutputHelper helper)
+        {
+            m_helper = helper;
+        }
+        [Fact]
+        public Task SerializeJson()
+        {
+            var json = File.ReadAllText(@".\docs\vasn.json");
+            var ed = json.DeserializeFromJson<EntityDefinition>();
+            Assert.Equal("Vasn", ed.Name);
+
+            return Task.FromResult(0);
+        }
+
+    }
+}
