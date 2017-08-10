d72474bc755e2bedb59cf3c2e4431604579bbb20
erymuzuan <erymuzuan@hotmail.com>
2017-08-10 10:33:34 +0800

    deployment GUI
    Related Work Items: #6173

---
+++ source/tools/deployment.gui/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Windows;
+using Bespoke.Sph.Mangements.ViewModels;
+
+namespace deployment.gui
+{
+    [Export("MainWindow", typeof(Window))]
+    public partial class MainWindow : IPartImportsSatisfiedNotification
+    {
+        [Import]
+        public MainViewModel MainViewModel { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnImportsSatisfied()
+        {
+            this.MainViewModel.View = this;
+            this.DataContext = this.MainViewModel;
+            this.MainViewModel.PropertyChanged += MainViewModelPropertyChanged;
+        }
+
+        private void MainViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //
+        }
+    }
+}
---
+++ source/tools/deployment.gui/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Threading;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace Bespoke.Sph.Mangements.ViewModels
+{
+    [Export]
+    public partial  class MainViewModel : ViewModelBase, IView
+    {
+        public RelayCommand LoadCommand { get; set; }
+
+        public MainViewModel()
+        {
+            this.LoadCommand = new RelayCommand(Load, ()=> true);
+        }
+
+        private void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DispatcherObject View { get; set; }
+    }
+}
