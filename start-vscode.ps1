b301a778592736117bd317520413a6022ad55f95
erymuzuan <erymuzuan@hotmail.com>
2017-08-23 06:57:22 +0800

    Visual studio code

--- start-vscode.ps1
+++ start-vscode.ps1
@@ -1,1 +1,15 @@
-& "D:\Program Files (x86)\Microsoft VS Code\Code.exe" $PWD
+Param(
+       [switch]$RxSource = $false
+       
+     )
+$Project = "$PWD\bin\sources"
+$VsCode = "D:\Program Files (x86)\Microsoft VS Code\Code.exe"
+if($RxSource.IsPresent){
+    $Project =  "$PWD\source"
+}
+
+if((Test-Path($VsCode)) -eq $false){
+    $VsCode = $env:RX_DevV1_VsCodePath
+}
+Write-Host "opening visual studio code with $Project"
+Start-Process -ArgumentList "$Project" -FilePath $VsCode  -WindowStyle Maximized
