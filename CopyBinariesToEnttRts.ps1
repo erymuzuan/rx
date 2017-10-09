e22b2a56dfaf595c875d4ba325daa862f40d6436
erymuzuan <erymuzuan@hotmail.com>
2017-10-09 08:37:22 +0800

    script update

--- CopyBinariesToEnttRts.ps1
+++ CopyBinariesToEnttRts.ps1
@@ -8,12 +8,15 @@ $schedulersFolder = '..\entt.rts\schedulers'
 
 $domain = @('.\source\domain\domain.sph\bin\Debug\domain.sph.dll', '.\source\domain\domain.sph\bin\Debug\domain.sph.pdb')
 $sphBuilder = @('.\bin\tools\sph.builder.exe', '.\bin\tools\sph.builder.pdb')
-$loggers = ls -Path .\source\web\web.sph\bin -Filter *.logger.* | sort -Descending LastWriteTime | select -First 10 
+$loggers = ls -Path .\source\web\web.sph\bin -Filter *.logger.* | sort -Descending LastWriteTime | select -First 10
+$rabbitMqChangePublisher = @('.\source\dependencies\rabbitmq.changepublisher\bin\Debug\rabbitmq.changepublisher.dll', '.\source\dependencies\rabbitmq.changepublisher\bin\Debug\rabbitmq.changepublisher.pdb')
+
 
 
 copy $domain $toolsFolder -Verbose
 copy $domain $webBin -Verbose
 copy $domain $subscriberHost -Verbose
+copy $rabbitMqChangePublisher $webBin -Verbose
 
 copy $sphBuilder $toolsFolder -Verbose
 
