ab075a8799112d0b8390f13111358a544a2b3c97
erymuzuan <erymuzuan@hotmail.com>
2017-10-31 09:33:40 +0800

    Deployment scripts

---
+++ Copy-LastModifiedFiles.ps1
@@ -0,0 +1,23 @@
+﻿Param(
+
+       [string]$Path,
+       [int]$First = 2,
+       [Switch]$WhatIf,
+       [string[]] $Destinations
+     )
+$files = ls $Path | sort -Descending LastWriteTime | select -First $First
+
+if($WhatIf.IsPresent -eq $true){
+
+    foreach($folder in $Destinations){
+        Write-Host "Copying $files to $folder"
+    }
+    
+    $files
+    return;
+}
+
+foreach($folder in $Destinations){
+    $files | Copy-Item -Destination $folder -Force -Verbose
+}
+$files
