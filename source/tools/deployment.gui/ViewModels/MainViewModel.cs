607bd1b1201b72dee68c01c6ad3ba1042a40d9c3
erymuzuan <erymuzuan@hotmail.com>
2017-11-01 11:14:40 +0800

    Intialize DeploymentMetadata table in SQL and give it ErrorWindow

--- source/tools/deployment.gui/ViewModels/MainViewModel.cs
+++ source/tools/deployment.gui/ViewModels/MainViewModel.cs
@@ -95,6 +95,39 @@ namespace Bespoke.Sph.Mangements.ViewModels
 
         }
 
+
+        public async Task InitializeAsync()
+        {
+            const string SQL = @"
+CREATE TABLE [Sph].[DeploymentMetadata](
+     [Id] INT PRIMARY KEY NOT NULL IDENTITY(1,1)
+    ,[Name] VARCHAR(255)  NULL
+    ,[EdId] VARCHAR(255) NOT NULL
+    ,[Tag] VARCHAR(255) NOT NULL
+    ,[Revision] VARCHAR(255)  NULL
+    ,[DateTime] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+    ,[Source] VARCHAR(MAX)
+)
+
+                ";
+            using (var conn = new SqlConnection(ConfigurationManager.SqlConnectionString))
+            using (var checkTableCommand = new SqlCommand(@"SELECT COUNT(*)
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'Sph' 
+                 AND  TABLE_NAME = 'DeploymentMetadata'", conn))
+            {
+                await conn.OpenAsync();
+                var exist = (int)await checkTableCommand.ExecuteScalarAsync() == 1;
+                if (!exist)
+                {
+                    using (var createTableCommand = new SqlCommand(SQL, conn))
+                    {
+                        await createTableCommand.ExecuteNonQueryAsync();
+                    }
+                }
+            }
+        }
+
         private async void LoadHelperAsync()
         {
             var context = new SphDataContext();
@@ -102,6 +135,7 @@ namespace Bespoke.Sph.Mangements.ViewModels
                 .Select(x => new EntityDeployment { EntityDefinition = x })
                 .ToList();
 
+            await this.InitializeAsync();
             using (var conn = new SqlConnection(ConfigurationManager.SqlConnectionString))
             {
                 await conn.OpenAsync();
