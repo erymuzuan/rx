ae10eedc8d449582ac1f6e0076d9cb55bfd50f56
erymuzuan <erymuzuan@hotmail.com>
2017-11-03 11:29:48 +0800

    Start and stop worker cmdlet

--- source/tools/rx.ps.module/RxWorkerCmdlet.cs
+++ source/tools/rx.ps.module/RxWorkerCmdlet.cs
@@ -1,5 +1,13 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Management.Automation;
+using System.Management.Automation.Language;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using Bespoke.Sph.RxPs.Domain;
 
 namespace Bespoke.Sph.RxPs
@@ -12,13 +20,42 @@ namespace Bespoke.Sph.RxPs
     [Cmdlet(VerbsLifecycle.Stop, "RxWorker")]
     public class StopRxWorkerCmdlet : PSCmdlet
     {
-        [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "Name", Mandatory = true)]
         public string Name { get; set; }
-        
+
+        [Parameter(Position = 0, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true, ParameterSetName = "Pid", Mandatory = true)]
+        public int Pid { get; set; }
+
+        [Parameter(Position = 0, ValueFromPipeline = true, ParameterSetName = nameof(Worker), Mandatory = true)]
+        public Worker Worker { get; set; }
+
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
         protected override void ProcessRecord()
         {
-            WriteObject("ToDO...");
+            if (this.ParameterSetName == "Pid")
+            {
+                var worker = Process.GetProcesses().SingleOrDefault(x => x.Id == Pid);
+                WriteVerbose($"Worker {(worker == null ? "not " : "")}found");
+                if (null == worker) return;
+                IntPtr h = worker.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^c");
+
+            }
+            if (this.ParameterSetName == nameof(Worker))
+            {
+                var worker = Process.GetProcesses().SingleOrDefault(x => x.Id == this.Worker.Pid);
+                WriteVerbose($"Worker {(worker == null ? "not " : "")}found");
+                if (null == worker) return;
+                IntPtr h = worker.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^c");
+
+            }
         }
+
     }
 
     [Cmdlet(VerbsLifecycle.Start, "RxWorker")]
@@ -29,10 +66,12 @@ namespace Bespoke.Sph.RxPs
         public string Name { get; set; }
 
         [Parameter(ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
-        public string Environment { get; set; }
+        [ArgumentCompleter(typeof(WorkerEnvironmentCompleter))]
+        public string Environment { get; set; } = "dev";
 
         [Parameter(ParameterSetName = RxWorkerCmdlet.PARAMETER_SET_DEFAULT, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
-        public string Configuration { get; set; }
+        [ArgumentCompleter(typeof(WorkerConfigCompleter))]
+        public string Configuration { get; set; } = "all";
 
         [Parameter(ParameterSetName = RxWorkerCmdlet.PARAMETER_SET_DEFAULT)]
         public string LogDirectory { get; set; } = ".\\logs\\workers.console.log";
@@ -47,8 +86,6 @@ namespace Bespoke.Sph.RxPs
 
         protected override void ProcessRecord()
         {
-
-
             var debug = MyInvocation.BoundParameters.ContainsKey("Debug") ? " /debug " : "";
             var verbose = MyInvocation.BoundParameters.ContainsKey("Verbose") ? " /verbose " : "";
 
@@ -56,12 +93,12 @@ namespace Bespoke.Sph.RxPs
             {
                 FileName = $"{ConfigurationManager.Home}\\subscribers.host\\workers.console.runner.exe",
                 Arguments = $"{debug}{verbose}/log:console "
-                            + $"/config:{this.Configuration} "
+                            + $"/config:{this.Configuration} /env:{this.Environment}"
                             + $" /v:{RxApplicationName} /u:{ConfigurationManager.RabbitMqUserName} /p:{ConfigurationManager.RabbitMqPassword} /h:{ConfigurationManager.RabbitMqHost} "
                             + $"/out:{this.LogDirectory} /outSize:100KB /outSwitch:{TraceSwitch}",
                 UseShellExecute = UseShellExecute
             };
-           // info.Verbs
+            // info.Verbs
 
             /*
                 Environment =
@@ -94,8 +131,57 @@ namespace Bespoke.Sph.RxPs
 
         protected override void ProcessRecord()
         {
-            WriteObject("ToDO...");
+            WriteObject("WIP: Send [Ctrl] + C");
         }
 
+    }
+
+
+    public class WorkerConfigCompleter : IArgumentCompleter
+    {
+        IEnumerable<CompletionResult> IArgumentCompleter.CompleteArgument(string commandName,
+            string parameterName,
+            string wordToComplete,
+            CommandAst commandAst,
+            IDictionary fakeBoundParameters)
+        {
+            return GetAllowedNames().
+                Where(new WildcardPattern(wordToComplete + "*", WildcardOptions.IgnoreCase).IsMatch).
+                Select(s => new CompletionResult(s));
+        }
+        private static string[] GetAllowedNames()
+        {
+            var files = Directory.GetFiles($@"{ConfigurationManager.SphSourceDirectory}\WorkersConfig", "*.json");
+            var list = files.Select(Path.GetFileNameWithoutExtension)
+                .Select(x => x.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+            Console.WriteLine(string.Join(",", list));
+            return list.ToArray();
+        }
+    }
+
+    public class WorkerEnvironmentCompleter : IArgumentCompleter
+    {
+        IEnumerable<CompletionResult> IArgumentCompleter.CompleteArgument(string commandName,
+            string parameterName,
+            string wordToComplete,
+            CommandAst commandAst,
+            IDictionary fakeBoundParameters)
+        {
+            return GetAllowedNames().
+                Where(new WildcardPattern(wordToComplete + "*", WildcardOptions.IgnoreCase).IsMatch).
+                Select(s => new CompletionResult(s));
+        }
+        private static string[] GetAllowedNames()
+        {
+            var files = Directory.GetFiles($@"{ConfigurationManager.SphSourceDirectory}\WorkersConfig", "*.json");
+            var list = files.Select(Path.GetFileNameWithoutExtension)
+                    .Select(x => x.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault())
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+            Console.WriteLine("Environments..." + string.Join(",", list));
+            return list.ToArray();
+        }
     }
 }
