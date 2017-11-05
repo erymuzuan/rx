77b5d4fa53be70be4165273add200a760736cf90
erymuzuan <erymuzuan@hotmail.com>
2017-11-05 15:06:43 +0800

    RabbitMq starts and stop WIP

--- source/tools/rx.ps.module/RxRabbitMqCmdlet.cs
+++ source/tools/rx.ps.module/RxRabbitMqCmdlet.cs
@@ -1,25 +1,120 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Management.Automation;
+using System.Threading.Tasks;
 using Bespoke.Sph.RxPs.Domain;
 
 namespace Bespoke.Sph.RxPs
 {
     [Cmdlet(VerbsLifecycle.Stop, "RxRabbitMq")]
-    public class StopRxRabbitMqCmdlet : PSCmdlet
+    public class StopRxRabbitMqCmdlet : RxCmdlet
     {
         protected override void ProcessRecord()
         {
-            WriteObject("ToDO...");
+            WriteVerbose("[STOPPING] web.console.logger");
+            WriteVerbose("RabbitMQ...[STOPPING]");
+
+            var rabbitmqctl = $@"{ConfigurationManager.Home}\rabbitmq_server\sbin\rabbitmqctl.bat";
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = rabbitmqctl,
+                Arguments = "stop",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+            using (var stop = Process.Start(startInfo))
+            {
+                stop?.WaitForExit();
+            }
+
+            WriteVerbose("RabbitMQ... [STOPPED]");
         }
     }
 
     [Cmdlet(VerbsLifecycle.Start, "RxRabbitMq")]
-    public class StartRxRabbitMqCmdlet : PSCmdlet
+    public class StartRxRabbitMqCmdlet : RxCmdlet
     {
         protected override void ProcessRecord()
         {
-            WriteObject("ToDO...");
+            var rabbitMqBase = Environment.GetEnvironmentVariable("RABBITMQ_BASE", EnvironmentVariableTarget.Process);
+            if (string.IsNullOrWhiteSpace(rabbitMqBase) || !Directory.Exists(rabbitMqBase))
+            {
+                WriteWarning("Environment variable for RABBITMQ_BASE was not properly set");
+                WriteWarning($"The value {rabbitMqBase} is not valid or does not exist");
+                return;
+            }
+            WriteVerbose("Starting RabbitMq broker ...");
+            WriteVerbose("RabbitMQ...[STARTING]");
+            try
+            {
+                var rabbitMqServerBat = $@"{ConfigurationManager.Home}\rabbitmq_server\sbin\rabbitmq-server.bat";
+                if (!File.Exists(rabbitMqServerBat))
+                {
+                    WriteWarning("CannotFind " + rabbitMqServerBat);
+                    return;
+                }
+
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = rabbitMqServerBat,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true,
+                    RedirectStandardError = true,
+                    WindowStyle = ProcessWindowStyle.Normal,
+
+                };
+
+                var rabbitMqServer = Process.Start(startInfo);
+
+                if (null == rabbitMqServer) throw new InvalidOperationException("Cannot start RabbitMQ");
+                rabbitMqServer.BeginOutputReadLine();
+                rabbitMqServer.BeginErrorReadLine();
+                rabbitMqServer.OutputDataReceived += (o, e) => WriteVerbose(e.Data);
+                rabbitMqServer.ErrorDataReceived += (o, e) => WriteWarning(e.Data);
+
+
+
+                var tcs = new TaskCompletionSource<bool>();
+                void Started(object o, DataReceivedEventArgs e)
+                {
+                    if ($"{e.Data}".Contains("Starting broker"))
+                        tcs.SetResult(true);
+                    if ($"{e.Data}".Contains("ERROR"))
+                    {
+                        tcs.SetResult(false);
+                    }
+                    if ($"{e.Data}".Contains("erl_crash.dump"))
+                    {
+                        tcs.SetResult(false);
+                    }
+                }
+
+                rabbitMqServer.OutputDataReceived += Started;
+                rabbitMqServer.ErrorDataReceived += Started;
+                tcs.Task.ContinueWith(_ =>
+                {
+                    if (_.Result)
+                    {
+                        WriteObject("RabbitMQ... [STARTED]");
+                    }
+                    else
+                    {
+                        WriteWarning("!Error starting RabbitMq, please check the log in the console");
+                    }
+                });
+                tcs.Task.Wait(TimeSpan.FromMinutes(2));
+
+
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "", ErrorCategory.FromStdErr, this));
+            }
         }
     }
 
@@ -63,20 +158,21 @@ current node details:
 
 F:\project\work\entt.snb.v2 [dev.local master ↓8 +0 ~2 -0 !]>
              */
-            var info = new ProcessStartInfo
+            var rabbitmqctl = $@"{ConfigurationManager.Home}\rabbitmq_server\sbin\rabbitmqctl.bat";
+            var info = new ProcessStartInfo(rabbitmqctl, "cluster_status -q")
             {
-                FileName = "rabbitmqctl.bat",
-                Arguments = "cluster_status",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
-                CreateNoWindow = true
+                RedirectStandardError = true,
+                WindowStyle = ProcessWindowStyle.Normal
             };
             var process = Process.Start(info);
             if (null == process)
                 throw new InvalidOperationException("Cannot start rabbitmqctl");
             var output = process.StandardOutput.ReadToEnd();
             var err = process.StandardError.ReadToEnd();
-            WriteError(new ErrorRecord(new Exception(err), "", ErrorCategory.FromStdErr, this));
+            if (!string.IsNullOrWhiteSpace(err))
+                WriteError(new ErrorRecord(new Exception(err), "", ErrorCategory.FromStdErr, this));
             process.WaitForExit();
 
             var rabbit = new RabbitMqServer
