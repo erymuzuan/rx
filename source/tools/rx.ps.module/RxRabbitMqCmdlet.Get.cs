515e7414226a7cde649c9615b72ccff7f35953cb
erymuzuan <erymuzuan@hotmail.com>
2017-11-05 15:09:45 +0800

    nested files

---
+++ source/tools/rx.ps.module/RxRabbitMqCmdlet.Get.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management.Automation;
+using Bespoke.Sph.RxPs.Domain;
+
+namespace Bespoke.Sph.RxPs
+{
+    [Cmdlet(VerbsCommon.Get, "RxRabbitMq")]
+    [OutputType(typeof(RabbitMqServer))]
+    public class GetRxRabbitMqCmdlet : RxCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            var rabbitmqctl = $@"{ConfigurationManager.Home}\rabbitmq_server\sbin\rabbitmqctl.bat";
+            var info = new ProcessStartInfo(rabbitmqctl, "cluster_status -q")
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WindowStyle = ProcessWindowStyle.Normal
+            };
+            var process = Process.Start(info);
+            if (null == process)
+                throw new InvalidOperationException("Cannot start rabbitmqctl");
+            var output = process.StandardOutput.ReadToEnd();
+            var err = process.StandardError.ReadToEnd();
+            if (!string.IsNullOrWhiteSpace(err))
+                WriteError(new ErrorRecord(new Exception(err), "", ErrorCategory.FromStdErr, this));
+            process.WaitForExit();
+
+            var rabbit = new RabbitMqServer
+            {
+                Status = output.Contains("unable to connect to node") ? "Stopped" : "Running",
+                RabbitMqBase = Environment.GetEnvironmentVariable("RABBITMQ_BASE")
+            };
+            WriteObject(rabbit);
+        }
+
+    }
+}
