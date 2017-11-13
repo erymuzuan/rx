0b84ad715de1841aa10f3f96d84cd16790f91eb6
erymuzuan <erymuzuan@hotmail.com>
2017-11-13 08:00:04 +0800

    Sorting for es query parser
    Related Work Items: #6401, #6404

---
+++ source/dependencies/es.queryparser/QueryParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Bespoke.Sph.Domain;
+using Newtonsoft.Json.Linq;
+
+namespace Bespoke.Sph.ElasticsearchQueryParsers
+{
+    public class QueryParser : IQueryParser
+    {
+        public QueryDsl Parse(string text)
+        {
+            var json = JObject.Parse(text);
+            var query = new QueryDsl();
+            var filters = json.SelectToken("$.filter");
+
+            var sorts = from jt in json.SelectToken("$.sort")
+                        let st = jt.First as JProperty
+                        where null != st
+                        let order = st.First as JObject
+                        where null != order
+                        let direction = order.First as JProperty
+                        where null != direction
+                        /* The order defaults to desc when sorting on the _score, and defaults to asc when sorting on anything else.*/
+                        let dic = direction.Value.ToString() == "desc" ? SortDirection.Desc : SortDirection.Asc
+                        select new Sort { Path = st.Name, Direction = dic };
+
+            query.Sorts.AddRange(sorts);
+            return query;
+        }
+
+        public string Provider => "elasticsearch";
+        public string Version => "1.7";
+    }
+}
---
+++ source/unit.test/es.queryparser.test/SortTest.cs
@@ -0,0 +1,157 @@
+﻿using System.Linq;
+using Bespoke.Sph.Domain;
+using Bespoke.Sph.ElasticsearchQueryParsers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace es.queryparser.test
+{
+    public class SortTest
+    {
+        private ITestOutputHelper Console { get; }
+
+        public SortTest(ITestOutputHelper console)
+        {
+            Console = console;
+        }
+
+
+        [Fact]
+        public void WithoutOrderSpecied()
+        {
+            var text = @"{
+    ""sort"" : [
+        {
+            ""_geo_distance"" : {
+                ""pin.location"" : {
+                    ""lat"" : 40,
+                    ""lon"" : -70
+                },
+                ""unit"" : ""km""
+            }
+        }
+    ],
+    ""query"" : {
+        ""term"" : { ""user"" : ""kimchy"" }
+    }
+}";
+            var parser = new QueryParser();
+            var query = parser.Parse(text);
+
+            var sort = query.Sorts.SingleOrDefault();
+            Assert.NotNull(sort);
+            Assert.Equal("_geo_distance", sort.Path);
+            Assert.Equal(SortDirection.Asc, sort.Direction);
+
+            Console.WriteLine(sort.ToString());
+        }
+
+        [Fact]
+        public void WithOtherSortOptions()
+        {
+            var text = @"{
+    ""sort"" : [
+        {
+            ""_geo_distance"" : {
+                ""pin.location"" : {
+                    ""lat"" : 40,
+                    ""lon"" : -70
+                },
+                ""order"" : ""asc"",
+                ""unit"" : ""km""
+            }
+        }
+    ],
+    ""query"" : {
+        ""term"" : { ""user"" : ""kimchy"" }
+    }
+}";
+            var parser = new QueryParser();
+            var query = parser.Parse(text);
+
+            var sort = query.Sorts.SingleOrDefault();
+            Assert.NotNull(sort);
+            Assert.Equal("_geo_distance", sort.Path);
+            Assert.Equal(SortDirection.Asc, sort.Direction);
+
+            Console.WriteLine(sort.ToString());
+        }
+
+        [Fact]
+        public void MrnOrderAsc()
+        {
+            var text = @"{
+                ""sort"": [
+                 {
+                     ""mrn"": {
+                         ""order"": ""asc""
+                     }
+                 }
+                ]
+            }";
+            var parser = new QueryParser();
+            var query = parser.Parse(text);
+
+            var sort = query.Sorts.SingleOrDefault();
+            Assert.NotNull(sort);
+            Assert.Equal("mrn", sort.Path);
+            Assert.Equal(SortDirection.Asc, sort.Direction);
+
+            Console.WriteLine(sort.ToString());
+        }
+
+        [Fact]
+        public void MrnOrderAscMykadOrderDesc()
+        {
+            var text = @"{
+                ""sort"": [
+                    {
+                       ""mrn"": {
+                             ""order"": ""asc""
+                         }
+                    },
+                    {
+                     ""mykad"": {
+                         ""order"": ""desc""
+                     }
+
+                    }
+                ]
+            }";
+            var parser = new QueryParser();
+            var query = parser.Parse(text);
+
+            Assert.Equal(2, query.Sorts.Count);
+            var mykad = query.Sorts.SingleOrDefault(x => x.Path == "mykad");
+            Assert.NotNull(mykad);
+            Assert.Equal(SortDirection.Desc, mykad.Direction);
+
+            Console.WriteLine(mykad.ToString());
+        }
+
+
+        [Fact]
+        public void TimeOrderDesc()
+        {
+            var text = @"{
+                ""sort"": [
+                 {
+                     ""time"": {
+                         ""order"": ""desc""
+                     }
+                 }
+                ]
+            }";
+            var parser = new QueryParser();
+            var query = parser.Parse(text);
+
+            var sort = query.Sorts.SingleOrDefault();
+            Assert.NotNull(sort);
+            Assert.Equal("time", sort.Path);
+            Assert.Equal(SortDirection.Desc, sort.Direction);
+
+            Console.WriteLine(sort.ToString());
+
+        }
+    }
+}
