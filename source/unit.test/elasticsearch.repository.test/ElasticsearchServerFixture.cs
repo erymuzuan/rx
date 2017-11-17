76ef5b04849eca4704daa1cba0cfd211295ae13a
erymuzuan <erymuzuan@hotmail.com>
2017-11-17 14:39:53 +0800

    Ensure the test index is created

--- source/unit.test/elasticsearch.repository.test/ElasticsearchServerFixture.cs
+++ source/unit.test/elasticsearch.repository.test/ElasticsearchServerFixture.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
@@ -22,35 +20,26 @@ namespace Bespoke.Sph.Tests.Elasticsearch
         public ElasticsearchServerFixture()
         {
             Client = new HttpClient { BaseAddress = new Uri(URL) };
-            this.Repository = new ReadOnlyRepository<Patient>(URL,INDEX);
-            // this.InitializeIndexAsync().Wait();
+            this.Repository = new ReadOnlyRepository<Patient>(URL, INDEX);
+            // TODO : RUN source\unit.test\sample-data-patients\create-index.linq
+            this.InitializeIndexAsync()
+                .ContinueWith(_ =>
+                {
+                    if (_.Exception != null)
+                        throw _.Exception;
+                }).Wait();
 
         }
 
         public async Task InitializeIndexAsync()
         {
-            var response = await this.Client.GetAsync("test_index/patient/_count");
-            if (response.IsSuccessStatusCode)
-            {
-                var json = await response.ReadContentAsJsonAsync();
-                var count = json["count"].Value<int>();
-                if (count == 100) return;
-            }
-            var delete = await this.Client.DeleteAsync(INDEX);
-            Console.WriteLine(delete.StatusCode);
-            var index = await this.Client.PostAsync(INDEX, new StringContent(""));
-            index.EnsureSuccessStatusCode();
+            var response = await this.Client.GetAsync($"{INDEX}/{TYPE}/_count");
+            response.EnsureSuccessStatusCode();
 
-            var mapping = await this.Client.PutAsync($"{INDEX}/_mapping/{TYPE}", new ByteArrayContent(Properties.Resources.Patient));
-            mapping.EnsureSuccessStatusCode();
-            /* */
-            var tasks = from file in Directory.GetFiles(
-                    $"../../../sample-data-patients/", "*.json")
-                        let id = Path.GetFileNameWithoutExtension(file)
-                        let content = new StringContent(File.ReadAllText(file))
-                        select this.Client.PostAsync($"/{INDEX}/{TYPE}/{id}", content);
+            var json = await response.ReadContentAsJsonAsync();
+            var count = json["count"].Value<int>();
+            if (count != 100) throw new InvalidOperationException("Remove the test index and create them with TODO : RUN source\\unit.test\\sample-data-patients\\create-index.linq");
 
-            await Task.WhenAll(tasks);
         }
 
         public void Dispose()
