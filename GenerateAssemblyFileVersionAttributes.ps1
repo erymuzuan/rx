8e1b0c53eb4bb73a7406ae42adf074d903d28410
erymuzuan <erymuzuan@hotmail.com>
2017-11-17 18:48:36 +0800

    assembily filversion info

--- GenerateAssemblyFileVersionAttributes.ps1
+++ GenerateAssemblyFileVersionAttributes.ps1
@@ -22,7 +22,7 @@ ls .\source -Filter *.csproj -Recurse | % {
     $logDate = git log -n 1 --pretty=%aI $project
     $date = [System.DateTime]::Parse($logDate)
     $days = [System.Convert]::ToInt32( ($date - $y2012).TotalDays)
-    Write-Host   " $days $_"
+    Write-Host   " $days $commitId $_"
 
     $cs = $_.DirectoryName.Replace("$PWD\source\","").Replace("\", "-") +  ".cs"
 
