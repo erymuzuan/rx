1e26f057d90b3304b0e4d4a7211b9477034b3e12
erymuzuan <erymuzuan@hotmail.com>
2017-12-06 17:13:04 +0800

    just adding // for touching every AssemblyInfo.cs in every projects

---
+++ BumpAssemblyInforFile.ps1
@@ -0,0 +1,6 @@
+﻿ls -Recurse -Filter AssemblyInfo.cs -Path .\source |
+%{
+
+    $cs = [System.IO.File]::ReadAllText($_.FullName) 
+    [System.IO.File]::WriteAllText($_.FullName, $cs + "`r`n//")
+}
