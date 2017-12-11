17e67adf1f70c4a242392bb6e4e82408af2b5dd2
erymuzuan <erymuzuan@hotmail.com>
2017-12-11 08:40:06 +0800

    sample properties

--- bin/sources/EntityDefinition/Customer.sql
+++ bin/sources/EntityDefinition/Customer.sql
@@ -1,20 +1,23 @@
-CREATE TABLE [DevV1].[Customer](  [Id] VARCHAR(50) PRIMARY KEY NOT NULL
-,[AccountNo] AS CAST(JSON_VALUE([Json], '$.AccountNo') AS VARCHAR(255))
-,[FirstName] AS CAST(JSON_VALUE([Json], '$.FirstName') AS VARCHAR(255))
-,[LastName] AS CAST(JSON_VALUE([Json], '$.LastName') AS VARCHAR(255))
-,[Age] AS CAST(JSON_VALUE([Json], '$.Age') AS INT)
-,[Gender] AS CAST(JSON_VALUE([Json], '$.Gender') AS VARCHAR(255))
-,[IsPriority] AS CAST(JSON_VALUE([Json], '$.IsPriority') AS BIT)
-,[RegisteredDate] AS CAST(JSON_VALUE([Json], '$.RegisteredDate') AS SMALLDATETIME)
-,[Rating] AS CAST(JSON_VALUE([Json], '$.Rating') AS INT)
-,[PrimaryContact] AS CAST(JSON_VALUE([Json], '$.PrimaryContact') AS VARCHAR(255))
-,[Department] AS CAST(JSON_VALUE([Json], '$.Department') AS VARCHAR(255))
-,[Address.State] AS CAST(JSON_VALUE([Json], '$.Address.State') AS VARCHAR(255))
-,[Address.Locality] AS CAST(JSON_VALUE([Json], '$.Address.Locality') AS VARCHAR(255))
-,[Contact.Name] AS CAST(JSON_VALUE([Json], '$.Contact.Name') AS VARCHAR(255))
-,[Json] VARCHAR(MAX)
-,[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
-,[CreatedBy] VARCHAR(255) NULL
-,[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
-,[ChangedBy] VARCHAR(255) NULL
+
+CREATE TABLE [DevV1].[Customer]
+(
+    [Id] VARCHAR(50) PRIMARY KEY NOT NULL
+    ,[AccountNo] AS CAST(JSON_VALUE([Json], '$.AccountNo') AS VARCHAR(255))
+    ,[FirstName] AS CAST(JSON_VALUE([Json], '$.FirstName') AS VARCHAR(255))
+    ,[LastName] AS CAST(JSON_VALUE([Json], '$.LastName') AS VARCHAR(255))
+    ,[Age] AS CAST(JSON_VALUE([Json], '$.Age') AS INT)
+    ,[Gender] AS CAST(JSON_VALUE([Json], '$.Gender') AS VARCHAR(255))
+    ,[IsPriority] AS CAST(JSON_VALUE([Json], '$.IsPriority') AS BIT)
+    ,[RegisteredDate] AS CAST(JSON_VALUE([Json], '$.RegisteredDate') AS DATETIME2)
+    ,[Rating] AS CAST(JSON_VALUE([Json], '$.Rating') AS INT)
+    ,[PrimaryContact] AS CAST(JSON_VALUE([Json], '$.PrimaryContact') AS VARCHAR(255))
+    ,[Department] AS CAST(JSON_VALUE([Json], '$.Department') AS VARCHAR(255))
+    ,[Address.State] AS CAST(JSON_VALUE([Json], '$.Address.State') AS VARCHAR(255))
+    ,[Address.Locality] AS CAST(JSON_VALUE([Json], '$.Address.Locality') AS VARCHAR(255))
+    ,[Contact.Name] AS CAST(JSON_VALUE([Json], '$.Contact.Name') AS VARCHAR(255))
+    ,[Json] VARCHAR(MAX)
+    ,[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+    ,[CreatedBy] VARCHAR(255) NULL
+    ,[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE()
+    ,[ChangedBy] VARCHAR(255) NULL
 )
